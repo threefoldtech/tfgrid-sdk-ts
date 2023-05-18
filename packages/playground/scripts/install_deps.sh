37280f7fc925c5617fe5c2b1dd59d7e27e35cd31
zaelgohary <zainabelgohary19@gmail.com>
2023-05-18 12:59:10 +0300

    add version, docker, build scripts

---
+++ packages/playground/scripts/install_deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# insall playground dependencies
+rm -rf ./node_modules
+yarn 
--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,25 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-  base: '/',
+  base: "/",
   plugins: [vue(), nodePolyfills()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   define: {
-    'process.env': {
-      NETWORK: process.env.NETWORK || 'dev',
+    "process.env": {
+      NETWORK: process.env.NETWORK || "dev",
+      VERSION: process.env.VERSION || "No version to show",
       INTERNAL_SOLUTION_PROVIDER_ID: process.env.INTERNAL_SOLUTION_PROVIDER_ID || 1,
     },
   },
-})
+});
