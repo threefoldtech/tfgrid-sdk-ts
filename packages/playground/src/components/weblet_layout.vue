afe6671fdecad3bd952f9759dfa567c326395eae
kassem <omarksm09@gmail.com>
2023-05-25 14:54:02 +0300

    move cost bar to the end of weblet layout

--- packages/playground/src/components/weblet_layout.vue
+++ packages/playground/src/components/weblet_layout.vue
@@ -14,18 +14,6 @@
         <v-card-subtitle v-if="$slots.subtitle" :style="{ whiteSpace: 'initial' }">
           <slot name="subtitle" />
         </v-card-subtitle>
-
-        <v-alert class="mt-2 mx-4" :style="{ fontSize: '1.2rem' }" type="info" variant="tonal" v-if="showPrice">
-          Based on your specifications and TFChain TFT balance, your deployment cost is
-          <span class="font-weight-black">{{ costLoading ? "Calculating..." : cost }}</span> USD/Month.
-          <a
-            class="d-block app-link text-decoration-underline"
-            target="_blank"
-            href="https://manual.grid.tf/cloud/cloudunits_pricing.html?highlight=pricing#cloud-unit-pricing"
-          >
-            Learn how to unlock discounts.
-          </a>
-        </v-alert>
       </div>
       <v-spacer />
       <div class="mr-4" v-if="$slots['header-actions']">
@@ -53,6 +41,24 @@
     </v-card-text>
 
     <template v-if="$slots['footer-actions'] && (profileManager.profile || disableAlerts)">
+      <v-alert
+        v-show="!status"
+        class="mx-4"
+        :style="{ fontSize: '1.2rem' }"
+        type="info"
+        variant="tonal"
+        v-if="showPrice"
+      >
+        Based on your specifications and TFChain TFT balance, your deployment cost is
+        <span class="font-weight-black">{{ costLoading ? "Calculating..." : cost }}</span> USD/Month.
+        <a
+          class="app-link text-decoration-underline"
+          target="_blank"
+          href="https://manual.grid.tf/cloud/cloudunits_pricing.html?highlight=pricing#cloud-unit-pricing"
+        >
+          Learn how to unlock discounts.
+        </a>
+      </v-alert>
       <v-divider class="mt-5" />
       <v-card-actions>
         <v-spacer />
