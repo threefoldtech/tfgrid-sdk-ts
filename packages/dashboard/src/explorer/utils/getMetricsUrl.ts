7d31d84548e1f5b656c87dbda3d76448cd3f5cf8
Thunder <mahmmoud.hassanein@gmail.com>
2023-05-31 11:30:48 +0300

    refactors the GrafanaStatistics class to implement the IGrafanaArgs interface.

--- packages/dashboard/src/explorer/utils/getMetricsUrl.ts
+++ packages/dashboard/src/explorer/utils/getMetricsUrl.ts
@@ -2,53 +2,51 @@ import config from "@/portal/config";
 
 import { INode } from "../graphql/api";
 
-export type GrafanaArgsType = {
+export interface IGrafanaArgs {
   orgID: number;
   network: string;
   farmID: number;
   twinID: number;
   accountID: string;
-};
+}
 
-export class GrafanaStatistics {
-  public args: GrafanaArgsType = {
-    orgID: 2,
-    network: config.network,
-    twinID: 0,
-    farmID: 0,
-    accountID: "",
-  };
+export class GrafanaStatistics implements IGrafanaArgs {
+  public orgID = 2;
+  public network: string = config.network;
+  public farmID = 0;
+  public twinID = 0;
+  public accountID = "";
 
   constructor(node: INode, accountID: string) {
-    this.args.twinID = node.twinId;
-    this.args.farmID = node.farmId;
-    this.args.accountID = accountID;
+    this.twinID = node.twinId;
+    this.farmID = node.farmId;
+    this.accountID = accountID;
     this.updateNetwork();
   }
 
   public getUrl(): string {
-    const orgId = `orgId=${this.args.orgID}`;
-    const network = `var-network=${this.args.network}`;
-    const farmID = `var-farm=${this.args.farmID}`;
-    const nodeID = `var-node=${this.args.accountID}`;
+    const orgId = `orgId=${this.orgID}`;
+    const network = `var-network=${this.network}`;
+    const farmID = `var-farm=${this.farmID}`;
+    const nodeID = `var-node=${this.accountID}`;
 
     const urlArgs = `${orgId}&refresh=30s&${network}&${farmID}&${nodeID}&var-diskdevices=%5Ba-z%5D%2B%7Cnvme%5B0-9%5D%2Bn%5B0-9%5D%2B%7Cmmcblk%5B0-9%5D%2B`;
     return `https://metrics.grid.tf/d/rYdddlPWkfqwf/zos-host-metrics?${urlArgs}`;
   }
 
   public updateNetwork() {
-    switch (this.args.network) {
+    switch (this.network) {
       case "dev":
-        this.args.network = "development";
+        this.network = "development";
         break;
       case "qa":
-        this.args.network = "qa";
+        this.network = "qa";
         break;
       case "test":
-        this.args.network = "testing";
+        this.network = "testing";
         break;
       case "main":
-        this.args.network = "production";
+        this.network = "production";
         break;
     }
   }
