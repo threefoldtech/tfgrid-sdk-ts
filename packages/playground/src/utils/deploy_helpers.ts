3ce7e10e7eeb96615607a546b016fe0223ac3063
MohamedElmdary <engm5081@gmail.com>
2023-06-11 12:28:13 +0300

    Add accessNodeId option while creating network

--- packages/playground/src/utils/deploy_helpers.ts
+++ packages/playground/src/utils/deploy_helpers.ts
@@ -5,6 +5,7 @@ export function createNetwork(network: Network = {}): NetworkModel {
   nw.name = network.name || "NW" + generateString(10);
   nw.ip_range = network.ipRange || "10.20.0.0/16";
   nw.addAccess = network.addAccess || false;
+  nw.accessNodeId = network.accessNodeId;
   return nw;
 }
 
@@ -12,4 +13,5 @@ export interface Network {
   name?: string;
   ipRange?: string;
   addAccess?: boolean;
+  accessNodeId?: number;
 }
