658736bc0b9f0d9cf9e3f1e2e8facfd7b7665fbc
Thunder <mahmmoud.hassanein@gmail.com>
2023-06-13 12:13:33 +0300

    Added a tooltip for each field inside Presearch solution.

--- packages/playground/src/weblets/tf_presearch.vue
+++ packages/playground/src/weblets/tf_presearch.vue
@@ -37,7 +37,9 @@
           ]"
           #="{ props }"
         >
-          <v-text-field label="Name" v-model="name" v-bind="props" />
+          <input-tooltip #="{ tooltipProps }" tooltip="Solution name.">
+            <v-text-field label="Name" v-model="name" v-bind="{ ...props, ...tooltipProps }" />
+          </input-tooltip>
         </input-validator>
 
         <input-validator
@@ -49,12 +51,34 @@
           #="{ props }"
         >
           <password-input-wrapper>
-            <v-text-field label="Presearch Registeration Code" v-bind="props" v-model="code" />
+            <input-tooltip #="{ tooltipProps }" tooltip="Presearch Registeration Code.">
+              <v-text-field
+                label="Presearch Registeration Code"
+                v-bind="{ ...props, ...tooltipProps }"
+                v-model="code"
+              />
+            </input-tooltip>
           </password-input-wrapper>
         </input-validator>
 
-        <v-switch color="primary" inset label="Public IPv4" v-model="ipv4" />
-        <v-switch color="primary" inset label="Planetary Network" v-model="planetary" />
+        <v-tooltip
+          location="top"
+          text="Public IPv4 refers to an Internet Protocol version 4 address that is globally unique and accessible over the internet."
+        >
+          <template v-slot:activator="{ props }">
+            <v-switch color="primary" inset label="Public IPv4" v-model="ipv4" v-bind="props" />
+          </template>
+        </v-tooltip>
+
+        <v-tooltip
+          location="top"
+          text="The Planetary Network is a distributed network infrastructure that spans across multiple regions and countries, providing global connectivity."
+        >
+          <template v-slot:activator="{ props }">
+            <v-switch color="primary" inset label="Planetary Network" v-model="planetary" v-bind="props" />
+          </template>
+        </v-tooltip>
+
         <v-alert v-show="networkError" class="mb-2" type="warning" variant="tonal">
           You must enable at least one of network options.
         </v-alert>
@@ -71,8 +95,31 @@
       </template>
 
       <template #restore>
-        <v-textarea label="Private Presearch Restore Key" v-model="privateRestoreKey" no-resize :spellcheck="false" />
-        <v-textarea label="Public Presearch Restore Key" v-model="publicRestoreKey" no-resize :spellcheck="false" />
+        <input-tooltip
+          #="{ tooltipProps }"
+          tooltip="The Private Presearch Restore Key is a unique cryptographic key associated with your Presearch account."
+        >
+          <v-textarea
+            label="Private Presearch Restore Key"
+            v-bind="{ ...tooltipProps }"
+            v-model="privateRestoreKey"
+            no-resize
+            :spellcheck="false"
+          />
+        </input-tooltip>
+
+        <input-tooltip
+          #="{ tooltipProps }"
+          tooltip="The Public Presearch Restore Key is a unique cryptographic key associated with your Presearch account."
+        >
+          <v-textarea
+            label="Public Presearch Restore Key"
+            v-bind="{ ...tooltipProps }"
+            v-model="publicRestoreKey"
+            no-resize
+            :spellcheck="false"
+          />
+        </input-tooltip>
       </template>
     </d-tabs>
 
