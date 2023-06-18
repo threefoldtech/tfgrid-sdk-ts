19eb3d3f97c784d0498c5a05c78f01530fa6c393
alaamahmoud.1223@gmail.com <alaamahmoud.1223@gmail.com>
2023-06-18 17:52:46 +0300

    change manual to help

--- packages/dashboard/src/Dashboard.vue
+++ packages/dashboard/src/Dashboard.vue
@@ -25,7 +25,7 @@
             <v-btn v-else @click="disconnectWallet" color="red"> Disconnect </v-btn>
 
             <a href="https://library.threefold.me/info/manual/#/manual__dashboard_readme" target="_blank">
-              <v-btn class="custom-button" color="white" style="color: black"> Manual</v-btn>
+              <v-btn class="custom-button" color="white" style="color: black"> Help</v-btn>
             </a>
 
             <v-theme-provider root>
