20ee1f05a0141ded05e5fc6d7b0f13d62bcbc736
alaamahmoud.1223@gmail.com <alaamahmoud.1223@gmail.com>
2023-06-19 16:52:40 +0300

    add 404 page

--- packages/playground/src/views/not_yet_implemented.vue
+++ packages/playground/src/views/not_yet_implemented.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>not_yet_implemented</div>
+  <img class="centered-img" src="images/404.png" />
+  <p class="text1">Oops. The page you were looking for doesn't exist.</p>
+  <p class="text2">You may have mistyped address or the page may have been moved</p>
+  <div style="text-align: center">
+    <a href="/" class="link-text"> Take me back to the home page</a>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,3 +12,32 @@ export default {
   name: "NotYetImplemented",
 };
 </script>
+
+<style>
+.centered-img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 65%;
+}
+
+.text1 {
+  font-size: 30px;
+  text-align: center;
+  font-weight: 500;
+  margin-top: 50px;
+}
+
+.text2 {
+  font-size: 20px;
+  text-align: center;
+  font-weight: 400;
+}
+
+.link-text {
+  font-size: 20px;
+  text-align: center;
+  font-weight: 400;
+  color: #3d7ad4;
+}
+</style>
