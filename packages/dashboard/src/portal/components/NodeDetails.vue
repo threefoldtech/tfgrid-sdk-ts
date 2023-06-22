af1a6588855908bbb04149476dc16647aae6ae97
kassem <omarksm09@gmail.com>
2023-06-22 20:23:11 +0300

    handle single card

--- packages/dashboard/src/portal/components/NodeDetails.vue
+++ packages/dashboard/src/portal/components/NodeDetails.vue
@@ -155,7 +155,7 @@
       </v-card>
     </v-col>
 
-    <v-col :cols="3" class="my-4">
+    <v-col :cols="3">
       <v-card flat color="transparent" tag="div">
         <!-- Title -->
         <v-list-item>
@@ -166,20 +166,96 @@
             <v-list-item-title style="font-size: 20px"> Node Resources </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-sheet class="d-flex justify-center align-center" height="180">
+        <!-- <v-sheet class="d-flex justify-center align-center pt-0" height="180">
           <div v-if="loading" class="text-center">
             <v-progress-circular indeterminate></v-progress-circular>
             <p class="pt-2">Loading GPU details</p>
           </div>
-          <div v-else-if="nodeGPU === undefined" class="text-center">
+          <div v-else-if="nodeGPUitems === undefined || nodeGPUitems.length == 0" class="text-center">
             <v-alert class="ma-2" dense outlined type="error">
               Failed to receive node GPUs information
               <template v-slot:append>
                 <v-icon @click="loadGPUitems">mdi-reload</v-icon>
               </template>
             </v-alert>
-          </div>
-        </v-sheet>
+          </div> -->
+        <v-row>
+          <v-col cols="12">
+            <v-list>
+              <v-list-item>
+                <v-list-item-content style="max-width: fit-content">
+                  <v-tooltip top nudge-bottom="30">
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-list-item-title v-bind="attrs" v-on="on"
+                        >Card ID
+                        <v-chip
+                          lose-icon="mdi-delete"
+                          :color="gpuItem.contract ? 'warning' : 'success'"
+                          x-small
+                          class="mb-1"
+                          >{{ gpuItem.contract ? "Reserved" : "Available" }}</v-chip
+                        >
+                      </v-list-item-title>
+                    </template>
+                    <span>Card id that's used in a deployment</span>
+                  </v-tooltip>
+                </v-list-item-content>
+
+                <v-col class="pa-0 ml-2 mr-n2">
+                  <v-select
+                    v-if="nodeGPUitems.length > 1"
+                    append-outer-icon="mdi-content-copy"
+                    hide-details
+                    dense
+                    v-model="gpuItem"
+                    :items="nodeGPUitems"
+                    @input.native="gpuItem = $event.srcElement.value.value"
+                    @click:append-outer="copy(gpuItem.id)"
+                  />
+                  <v-text-field
+                    v-else
+                    :value="gpuItem.id"
+                    readonly
+                    hide-details
+                    class="mt-n2"
+                    dense
+                    append-outer-icon="mdi-content-copy"
+                    @click:append-outer="copy(gpuItem.id)"
+                    solo
+                  ></v-text-field>
+                </v-col>
+              </v-list-item>
+
+              <v-divider />
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title> Vendor </v-list-item-title>
+                </v-list-item-content>
+                {{ gpuItem?.vendor }}
+              </v-list-item>
+              <v-divider />
+
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title> Device </v-list-item-title>
+                </v-list-item-content>
+                {{ gpuItem?.device }}
+              </v-list-item>
+              <v-divider />
+              <v-list-item v-if="gpuItem.contract !== undefined">
+                <v-tooltip top nudge-bottom="30">
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-list-item-content v-bind="attrs" v-on="on">
+                      <v-list-item-title> Contract ID</v-list-item-title>
+                    </v-list-item-content>
+                  </template>
+                  <span>The contract id that reserves this GPU card</span>
+                </v-tooltip>
+                {{ gpuItem?.contract }}
+              </v-list-item>
+              <v-divider v-if="gpuItem.contract !== undefined" />
+            </v-list> </v-col
+        ></v-row>
       </v-card>
     </v-col>
   </v-row>
@@ -200,16 +276,36 @@ export default class NodeDetails extends Vue {
     farm: { id: string; name: string; farmCertType: string; pubIps: string };
   };
   loading = false;
-  nodeGPU: INodeGPU[] | undefined = [];
+  nodeGPUitems:
+    | {
+        text: string;
+        value: INodeGPU;
+
+        disabled: false;
+      }[]
+    | undefined = [];
+  gpuItem: INodeGPU | undefined = undefined;
+  error = false;
   gpuError = false;
   async loadGPUitems() {
     this.loading = true;
-    this.nodeGPU = await getNodeGPUs(this.node.nodeId);
-    console.log(this.nodeGPU);
+    const nodeGPU = await getNodeGPUs(this.node.nodeId);
     this.loading = false;
+    if (!nodeGPU) return;
+    this.nodeGPUitems = nodeGPU.map((item: INodeGPU) => {
+      return {
+        text: item.id,
+        value: item,
+        disabled: false,
+      };
+    });
+    this.gpuItem = this.nodeGPUitems[0].value;
   }
   async mounted() {
     await this.loadGPUitems();
+  }
+  copy(id: string) {
+    navigator.clipboard.writeText(id);
   }
 }
 </script>
