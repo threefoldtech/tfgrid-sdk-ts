3503f28cfe3bef3a74613585a31bb54b29cf1d67
Thunder <mahmmoud.hassanein@gmail.com>
2023-06-26 16:59:48 +0300

    Created new layout component for the new dedicated nodes view, added action/mutation type, added a watcher on the search input value change to request the grid proxy with the new value.

--- packages/dashboard/src/portal/store/mutations.ts
+++ packages/dashboard/src/portal/store/mutations.ts
@@ -1,7 +1,14 @@
 import { apiInterface } from "../lib/util";
 import { accountInterface, PortalState } from "./state";
 
+import { accountInterface, PortalState } from "./state";
+
 export enum MutationTypes {
+  SET_DEDICATED_NODES = "setNodes",
+  SET_DEDICATED_NODES_FILTER = "setNodesFilter",
+}
+
+export enum PortalMutationTypes {
   SET_ACCOUNTS = "setAccounts",
   REMOVE_ACCOUNTS = "removeAccounts",
   SET_PROPOSALS = "setProposals",
@@ -69,5 +76,10 @@ export default {
 
   setDedicatedNodesCount(state: PortalState, payload: number) {
     state.dedicatedNodesCount = payload;
+  },
+
+  setNodesFilter(state: PortalState, payload: { key: string; value: any }) {
+    state.dedicatedNodesFilter[payload.key] = payload.value;
+    console.log(payload.key, state.dedicatedNodesFilter[payload.key]);
   },
 };
