890f7a90914a8abf18953dda80d3e852610f815b
Thunder <mahmmoud.hassanein@gmail.com>
2023-07-03 11:03:14 +0300

    WIP: update the INode model with the missing fields, make a bind on the inputes and request to get the nodes on change.

--- packages/dashboard/src/portal/store/state.ts
+++ packages/dashboard/src/portal/store/state.ts
@@ -13,13 +13,27 @@ export interface PortalState {
   accounts: accountInterface[];
   dedicatedNodes: INode[];
   proposals: number;
-  api: ApiPromise;
+  api: apiInterface;
+  address: string;
+  tabQuery: string;
+  tableLoad: boolean;
+  twinID: number;
   dedicatedNodesFilter: any;
+  dedicatedNodesTablePageSize: number;
+  dedicatedNodesTablePageNumber: number;
+  dedicatedNodesCount: number;
 }
 export default {
   accounts: [],
   dedicatedNodes: [],
   proposals: 0,
   api: undefined,
+  address: "",
+  tabQuery: "",
+  tableLoad: false,
+  twinID: 0,
   dedicatedNodesFilter: {},
+  dedicatedNodesTablePageSize: 10,
+  dedicatedNodesTablePageNumber: 1,
+  dedicatedNodesCount: 0,
 };
