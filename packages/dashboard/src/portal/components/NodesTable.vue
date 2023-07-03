b3437ffb8c9cf7152783e64829f507d13c07eb78
Thunder <mahmmoud.hassanein@gmail.com>
2023-07-03 15:06:53 +0300

    WIP: Added some improvements on the dedicated nodes component and portal actions/mutations files.

--- packages/dashboard/src/portal/components/NodesTable.vue
+++ packages/dashboard/src/portal/components/NodesTable.vue
@@ -175,10 +175,17 @@ export default class NodesTable extends Vue {
   // reload the nodes table
   async requestNodes() {
     if (this.$api) {
+<<<<<<< HEAD
       // this.$store.commit('portal/' + MutationTypes.SET_API, this.$api);
       this.$store.commit("portal/" + MutationTypes.SET_TWIN_ID, this.twinId);
       this.$store.commit("portal/" + MutationTypes.SET_TAB_QUERY, this.tab.query);
       await this.$store.dispatch(ActionTypes.REQUEST_DEDICATED_NODES);
+=======
+      this.$store.commit(`portal/${MutationTypes.SET_API}`, this.$api);
+      this.$store.commit(`portal/${MutationTypes.SET_TWIN_ID}`, this.twinId);
+      this.$store.commit(`portal/${MutationTypes.SET_TAB_QUERY}`, this.tab.query);
+      await this.$store.dispatch(`portal/${ActionTypes.REQUEST_DEDICATED_NODES}`);
+>>>>>>> a7059ec (WIP: Added some improvements on the dedicated nodes component and portal actions/mutations files.)
     }
   }
 
