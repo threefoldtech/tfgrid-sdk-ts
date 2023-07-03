277baf7bb187733e84614918fe18de4b97624f97
Thunder <mahmmoud.hassanein@gmail.com>
2023-07-03 11:03:14 +0300

    WIP: update the INode model with the missing fields, make a bind on the inputes and request to get the nodes on change.

--- packages/dashboard/src/portal/components/NodesTable.vue
+++ packages/dashboard/src/portal/components/NodesTable.vue
@@ -134,11 +134,6 @@ export default class NodesTable extends Vue {
     console.log("filterKeys | value: ", value);
   }
 
-  @Watch("filterKeys") async filterRequest(value: string) {
-    console.log(this.filterKeys);
-    console.log("filterKeys | value: ", value);
-  }
-
   async mounted() {
     this.address = this.$store.state.credentials.account.address;
     this.$store.commit("portal/" + MutationTypes.SET_ADDRESS, this.address);
