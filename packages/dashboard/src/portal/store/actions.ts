7fd952de15067d06d2d21385e8ba1bdb7811be84
Thunder <mahmmoud.hassanein@gmail.com>
2023-07-03 11:03:14 +0300

    WIP: update the INode model with the missing fields, make a bind on the inputes and request to get the nodes on change.

--- packages/dashboard/src/portal/store/actions.ts
+++ packages/dashboard/src/portal/store/actions.ts
@@ -21,23 +21,33 @@ export default {
   },
 
   async requestDedicatedNodes({ state, commit }: ActionContext<PortalState, PortalState>) {
-    let url = `${window.configs.APP_GRIDPROXY_URL}/nodes?ret_count=true&rentable=true`;
+    commit(MutationTypes.SET_TABLE_LOAD, true);
+
+    let url = `${window.configs.APP_GRIDPROXY_URL}/nodes?status=up&ret_count=true&rentable=true`;
     url += `&size=${state.dedicatedNodesTablePageSize}`;
     url += `&page=${state.dedicatedNodesTablePageNumber}`;
 
     for (const key in state.dedicatedNodesFilter) {
       let value = state.dedicatedNodesFilter[key];
-
       if (key == "free_hru" || key == "free_mru" || key == "free_sru" || key == "free_cru") {
         value *= 1024 * 1024 * 1024; // convert from gb to b
       }
-
       // don't break the call for the null values
-      if (value == null || value == undefined) value = "";
-
+      if (value == null || value == undefined || value == 0) value = "";
       url += `&${key}=${value}`;
     }
-    // console.log('url => ', url);
+
+    const res = await fetch(url);
+
+    const nodesCount: any = res.headers.get("count");
+    commit(MutationTypes.SET_DEDICATED_NODES_COUNT, +nodesCount);
+
+    let nodes = await res.json();
+    // Update the nodes with price and discount.
+    nodes = await updateDedicatedNodes(state.api, state.address, nodes);
+
+    commit(MutationTypes.SET_DEDICATED_NODES, { nodes });
+    commit(MutationTypes.SET_TABLE_LOAD, false);
   },
 
   async unsubscribeAccounts({ commit }: ActionContext<PortalState, PortalState>) {
@@ -50,43 +60,43 @@ export default {
 
   async getProposal({ commit, state }: ActionContext<PortalState, PortalState>, twin: number) {
     if (state.api) {
-      try {
-        const active = (await getProposals(state.api)).active;
-        if (!active.length) return;
-        const farms = await getFarm(state.api, parseFloat(`${twin}`));
-        // only users who own a farm should get the notification
-        if (!farms.length) {
-          commit(MutationTypes.SET_PROPOSALS, { proposals: 0 });
-          return;
-        }
-        const farmIds = farms.map(value => value.id);
+      console.log(state.api, "ahooooooooooooooooooooooooooooooooooo");
 
-        const voted: number[] = [];
-        active.forEach((proposal, index) => {
-          let inYes = false;
-          proposal.ayes.forEach(({ farmId }) => {
-            if (farmIds.includes(farmId)) {
-              inYes = true;
-              voted.push(index);
-              return;
-            }
-          });
-          if (inYes) return;
-          proposal.nayes.forEach(({ farmId }) => {
-            if (farmIds.includes(farmId)) {
-              voted.push(index);
-              return;
-            }
-          });
-        });
-        voted.forEach(index => {
-          active.splice(index, 1);
-        });
-        commit(MutationTypes.SET_PROPOSALS, { proposals: active.length });
-      } catch (error) {
-        console.error("Failed to get proposal:", error);
-        // Handle error and show error message to the user
+      const active = (await getProposals(state.api)).active;
+      if (!active.length) return;
+      const farms = await getFarm(state.api, parseFloat(`${twin}`));
+
+      // only users who own a farm should get the notification
+      if (!farms.length) {
+        commit("setProposals", { proposals: 0 });
+        return;
       }
+      const farmIds = farms.map(function (value) {
+        return value.id;
+      });
+
+      const voted: number[] = [];
+      active.forEach((proposal, index) => {
+        let inYes = false;
+        proposal.ayes.forEach(({ farmId }) => {
+          if (farmIds.includes(farmId)) {
+            inYes = true;
+            voted.push(index);
+            return;
+          }
+        });
+        if (inYes) return;
+        proposal.nayes.forEach(({ farmId }) => {
+          if (farmIds.includes(farmId)) {
+            voted.push(index);
+            return;
+          }
+        });
+      });
+      voted.forEach(index => {
+        active.splice(index, 1);
+      });
+      commit("setProposals", { proposals: active.length });
     }
   },
 };
