eafb08e02c0f45e994e648f507bbbf717c6c3c25
Thunder <mahmmoud.hassanein@gmail.com>
2023-07-03 15:06:53 +0300

    WIP: Added some improvements on the dedicated nodes component and portal actions/mutations files.

--- packages/dashboard/src/portal/store/mutations.ts
+++ packages/dashboard/src/portal/store/mutations.ts
@@ -1,26 +1,7 @@
 import { apiInterface } from "../lib/util";
 import { accountInterface, PortalState } from "./state";
 
-import { fillNodesFields } from "@/explorer/store/mutations";
-import { IState } from "@/explorer/store/state";
-
-import { apiInterface } from "../lib/util";
-import { accountInterface, PortalState } from "./state";
-
 export enum MutationTypes {
-  SET_DEDICATED_NODES = "setNodes",
-  SET_DEDICATED_NODES_FILTER = "setNodesFilter",
-  SET_DEDICATED_NODES_TABLE_PAGE_NUMBER = "setDedicatedNodesTablePageNumber",
-  SET_DEDICATED_NODES_TABLE_PAGE_SIZE = "setDedicatedNodesTablePageSize",
-  SET_TWIN_ID = "setTwinID",
-  SET_TAB_QUERY = "setTabQuery",
-  SET_TABLE_LOAD = "setTableLoad",
-  SET_DEDICATED_NODES_COUNT = "setDedicatedNodesCount",
-  SET_API = "setApi",
-  SET_ADDRESS = "setAddress",
-}
-
-export enum PortalMutationTypes {
   SET_ACCOUNTS = "setAccounts",
   REMOVE_ACCOUNTS = "removeAccounts",
   SET_PROPOSALS = "setProposals",
@@ -34,9 +15,6 @@ export enum PortalMutationTypes {
   SET_DEDICATED_NODES = "setDedicatedNodes",
   SET_DEDICATED_NODES_COUNT = "setDedicatedNodesCount",
   SET_ADDRESS = "setAddress",
-
-  CLEAR_DEDICATED_NODES_FILTER = "clearDedicatedNodesFilter",
-  CLEAR_DEDICATED_NODES_FILTER_KEY = "clearDedicatedNodesFilterKey",
 }
 
 export default {
@@ -51,36 +29,40 @@ export default {
   setProposals(state: PortalState, payload: { proposals: number }) {
     state.proposals = payload.proposals;
   },
+
   setApi(state: PortalState, api: apiInterface) {
     state.api = api;
   },
 
-  setNodesFilter(state: PortalState, payload: { key: string; value: any }) {
+  setDedicatedNodesFilter(state: PortalState, payload: { key: string; value: any }) {
     state.dedicatedNodesFilter[payload.key] = payload.value;
   },
 
   setDedicatedNodesTablePageNumber(state: PortalState, payload: number) {
     state.dedicatedNodesTablePageNumber = payload;
   },
+
   setDedicatedNodesTablePageSize(state: PortalState, payload: number) {
     state.dedicatedNodesTablePageSize = payload;
   },
+
   setTwinID(state: PortalState, payload: number) {
     state.twinID = payload;
   },
+
   setTabQuery(state: PortalState, payload: string) {
     state.tabQuery = payload;
   },
+
   setTableLoad(state: PortalState, payload: boolean) {
     state.tableLoad = payload;
   },
+
   setAddress(state: PortalState, address: string) {
     state.address = address;
   },
 
-  setNodes(state: PortalState, nodes: any[]): void {
-    // clear the state each time you reload. to avoid duplicated nodes
-    state.dedicatedNodes = [];
+  setDedicatedNodes(state: PortalState, nodes: any[]): void {
     state.dedicatedNodes = nodes;
   },
 
