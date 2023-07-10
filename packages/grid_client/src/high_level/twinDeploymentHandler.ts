4505ae7f7f6fb7188fc5075f45d630412ebf7a39
kassem <omarksm09@gmail.com>
2023-07-10 13:23:30 +0300

    remove addedNameContracts

--- packages/grid_client/src/high_level/twinDeploymentHandler.ts
+++ packages/grid_client/src/high_level/twinDeploymentHandler.ts
@@ -13,7 +13,6 @@ class TwinDeploymentHandler {
   rmb: RMB;
   deploymentFactory: DeploymentFactory;
   original_deployments = [];
-  addedNameContracts = [];
   nodes: Nodes;
 
   constructor(public config: GridClientConfig) {
@@ -32,9 +31,7 @@ class TwinDeploymentHandler {
       }
     }
     try {
-      const contract = await this.tfclient.contracts.createName({ name });
-      this.addedNameContracts.push(name);
-      return contract;
+      return await this.tfclient.contracts.createName({ name });
     } catch (e) {
       throw Error(`Failed to create name contract ${name} due to ${e}`);
     }
