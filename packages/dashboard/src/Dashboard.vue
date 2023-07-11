d0ce76e76406ba339c8032de2f2e7005498a6321
kassem <omarksm09@gmail.com>
2023-07-11 13:59:55 +0300

    add minting to navbar

--- packages/dashboard/src/Dashboard.vue
+++ packages/dashboard/src/Dashboard.vue
@@ -223,6 +223,7 @@ interface SidenavItem {
   icon: string;
   prefix: string;
   active?: boolean;
+  hidden?: boolean;
   hyperlink?: boolean;
   children: Array<{
     label?: string;
@@ -269,6 +270,9 @@ export default class Dashboard extends Vue {
     });
   }
   async mounted() {
+    this.routes = this.routes.filter(route => {
+      if (!route.hidden) return route;
+    });
     await this.subscribe();
     this.accounts = this.$store.state.portal.accounts;
     if (this.$route.path === "/" && !this.$api) {
@@ -460,6 +464,13 @@ export default class Dashboard extends Vue {
       icon: "earth",
       prefix: "/other/bootstrap",
       children: [],
+    },
+    {
+      label: "Minting",
+      icon: "cash-multiple",
+      prefix: "/other/minting",
+      children: [],
+      hidden: !(window.configs.APP_NETWORK === "main"),
     },
     {
       label: "Monitoring",
