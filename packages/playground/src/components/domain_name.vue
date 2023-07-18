abc18e3faedc6e48a97e0338fe1e3bc61e375729
kassem <omarksm09@gmail.com>
2023-07-18 12:48:34 +0300

    update domain name

--- packages/playground/src/components/domain_name.vue
+++ packages/playground/src/components/domain_name.vue
@@ -3,7 +3,7 @@
     <h6 class="text-h5">Domain Name</h6>
     <v-tooltip location="top" text="Use Custom domain name">
       <template #activator="{ props }">
-        <v-switch v-bind="props" v-model="customDomain" hide-details color="primary" inset label="custom domain" />
+        <v-switch v-bind="props" v-model="customDomain" hide-details color="primary" inset label="Custom domain" />
       </template>
     </v-tooltip>
     <v-expand-transition>
@@ -17,7 +17,12 @@
           #="{ props }"
         >
           <input-tooltip tooltip="Domain Name that will points to this instance">
-            <v-text-field :disabled="!customDomain" label="Domain Name" v-model="domainName" v-bind="{ ...props }" />
+            <v-text-field
+              :disabled="!customDomain"
+              label="Custom domain name"
+              v-model="domainName"
+              v-bind="{ ...props }"
+            />
           </input-tooltip>
         </input-validator>
       </div>
@@ -26,13 +31,14 @@
       <SelectGatewayNode ref="selectGateway" v-model="gatewayNode" customDomain :farmData="farmData" />
       <v-expand-transition>
         <v-alert
-          v-show="domain?.useFQDN && domain?.ip && !selectGateway?.loading"
+          v-show="domain?.useFQDN && domain?.ip && domainName && !selectGateway?.loading"
           class="mb-2"
           type="warning"
           variant="tonal"
         >
-          Please make sure to create a record with ip: <span class="font-weight-bold">{{ domain?.ip }}</span> on the
-          name provider
+          Before starting the deployment, Please make sure to create an A record on your name provider with
+          <span class="font-weight-bold">{{ domainName }}</span> pointing to
+          <span class="font-weight-bold">{{ domain?.ip }}</span>
         </v-alert>
       </v-expand-transition>
     </div>
