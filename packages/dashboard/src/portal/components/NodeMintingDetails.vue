7552a0fa435233c9060d5147593fbd7e5c6917ad
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2023-07-20 03:10:52 +0300

    wip: finish ui for node minting details - Add NodeMintingDetails component - restyle the node details - fetch cloud_units & payoutDate

--- packages/dashboard/src/portal/components/NodeMintingDetails.vue
+++ packages/dashboard/src/portal/components/NodeMintingDetails.vue
@@ -1,77 +1,135 @@
 <template>
   <v-container>
-    <v-select v-model="month" :nodes="months" label="Filter by Month"></v-select>
+    <v-row>
+      <v-col cols="3">
+        <v-menu ref="menu" v-model="showMenu" :close-on-content-click="true" transition="scale-transition">
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="selectedData"
+              label="Filter by month"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
 
-    <v-card>
-      <v-container fluid>
-        <v-row :justify="'start'">
-          <v-col cols="8">
-            <v-row>
-              <v-flex xs3 class="text-left pr-2">Payout Address</v-flex>
-              <v-flex class="text-truncate font-weight-bold">
-                <span>{{ node.nodeId }}</span>
-              </v-flex>
-            </v-row>
-            <v-row>
-              <v-flex xs3 class="text-left pr-2">Farm ID</v-flex>
-              <v-flex class="text-truncate font-weight-bold">
-                <span>{{ node.farmId }}</span>
-              </v-flex>
-            </v-row>
-            <v-row>
-              <v-flex xs3 class="text-left pr-2">Twin ID</v-flex>
-              <v-flex class="text-truncate font-weight-bold">
-                <span>{{ node.twinId }}</span>
-              </v-flex>
-            </v-row>
-            <v-row>
-              <v-flex xs3 class="text-left pr-2">Twin ID</v-flex>
-              <v-flex class="text-truncate font-weight-bold">
-                <span>{{ node.twinId }}</span>
-              </v-flex>
-            </v-row>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
-    <br />
-    <v-btn @click="downloadNodeReceipt">Download Receipts</v-btn>
+          <v-date-picker
+            v-model="selectedData"
+            type="month"
+            class="mt-4"
+            color="blue lighten-1"
+            :min="minDate"
+            :max="maxDate"
+            no-title
+            scrollable
+          ></v-date-picker>
+        </v-menu>
+      </v-col>
+      <v-col cols="9">
+        <v-card outlined>
+          <v-card-title>
+            <span class="headline">Minting Details</span>
+          </v-card-title>
+          <v-card-text>
+            <span class="font-weight-bold">Date of Payout :</span>
+            {{ getDateFromTimestamp(receipt.payoutDate) }}
+            <br />
+            <span class="font-weight-bold">Node TFT Amount :</span> {{ receipt.tft || 0 }} TFT
+            <br />
+            <span class="font-weight-bold">Cloud Units :</span>
+            <ul>
+              <li inset v-for="(val, key) in receipt.clould_units" :key="key">
+                <b style="text-transform: uppercase">{{ key }}:</b> {{ val }}
+              </li>
+            </ul>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue" @click="downloadNodeReceipt">Download Node Receipt</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
 import { jsPDF } from "jspdf";
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Prop, Vue, Watch } from "vue-property-decorator";
 
 import { nodeInterface } from "../lib/farms";
-import { generateReceipt } from "../lib/nodes";
+import { generateReceipt, receiptInterface } from "../lib/nodes";
 
 @Component({ name: "NodeMintingDetails" })
 export default class NodeMintingDetails extends Vue {
   @Prop({ required: true })
   node!: nodeInterface;
 
-  month = "";
-  months = [
-    "All",
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+  showMenu = false;
+  selectedData = "";
+  minDate = "";
+  maxDate = "";
+  receipt: receiptInterface = {
+    hash: "",
+    clould_units: {
+      cu: 0,
+      su: 0,
+      nu: 0,
+    },
+    payoutDate: 0,
+  };
+
+  @Watch("selectedData")
+  onDateChange() {
+    this.getMonthReceipt();
+  }
 
   downloadNodeReceipt() {
     let doc = new jsPDF();
     doc = generateReceipt(doc, this.node);
-    doc.save(`node_${this.node.nodeId}_receipts.pdf`);
+    // doc.save(`node_${this.node.nodeId}_receipts.pdf`);
+  }
+
+  formatDate(date: Date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+
+    return `${year}-${month}`;
+  }
+
+  getDates() {
+    const firstReceiptData = this.node.receipts[this.node.receipts.length - 1].payoutDate;
+    const lastReceiptData = this.node.receipts[0].payoutDate;
+
+    this.minDate = this.formatDate(this.getDateFromTimestamp(firstReceiptData));
+    this.maxDate = this.formatDate(this.getDateFromTimestamp(lastReceiptData));
+
+    this.selectedData = this.maxDate;
+  }
+
+  mounted() {
+    this.getDates();
+    this.getMonthReceipt();
+    for (let i = 0; i < this.node.receipts.length; i++) {
+      if (this.node.receipts[i].clould_units.cu > 0) {
+        console.log(this.node.receipts[i]);
+      }
+    }
+  }
+
+  getDateFromTimestamp(timestamp: number) {
+    return new Date(timestamp * 1000);
+  }
+
+  getMonthReceipt() {
+    const selectedDate = new Date(this.selectedData);
+    for (let i = 0; i < this.node.receipts.length; i++) {
+      const receiptDate = this.getDateFromTimestamp(this.node.receipts[i].payoutDate);
+      if (selectedDate.getMonth() === receiptDate.getMonth()) {
+        this.receipt = this.node.receipts[i];
+        break;
+      }
+    }
   }
 }
 </script>
