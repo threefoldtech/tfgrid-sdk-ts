0552ef682345000dd339eca6d79ba999d8e7a81f
mayar osama <mayarosama18@gmail.com>
2023-07-25 14:53:08 +0300

    Resolving comments

--- packages/playground/src/components/tf_notification.vue
+++ packages/playground/src/components/tf_notification.vue
@@ -8,31 +8,28 @@ import "mosha-vue-toastify/dist/style.css";
 import { ContractStates } from "@threefold/grid_client";
 import { createToast } from "mosha-vue-toastify";
 import { onMounted } from "vue";
-import { ref } from "vue";
 
 import { useProfileManager } from "../stores";
-import { getUserContracts, type NormalizedContract } from "../utils/contracts";
 import { getGrid } from "../utils/grid";
 
 const profileManager = useProfileManager();
-const contracts = ref<NormalizedContract[]>([]);
 
 onMounted(async () => {
-  contracts.value = [];
-
   while (profileManager.profile) {
     const grid = await getGrid(profileManager.profile!);
-    contracts.value = await getUserContracts(grid!);
+    const contracts: any = await grid!.contracts.listMyContracts({ state: [ContractStates.GracePeriod] });
 
-    for (const contract of contracts.value) {
-      if (contract.state == ContractStates.GracePeriod) {
-        createToast("Contract " + contract.contractId + " is in grace period", {
-          position: "top-right",
-          hideProgressBar: true,
-          toastBackgroundColor: "red",
-          timeout: 5000,
-        });
-      }
+    if (
+      contracts.nameContracts.length != 0 ||
+      contracts.nodeContracts.length != 0 ||
+      contracts.rentContracts.length != 0
+    ) {
+      createToast("Contracts are in grace period", {
+        position: "top-right",
+        hideProgressBar: true,
+        toastBackgroundColor: "red",
+        timeout: 5000,
+      });
     }
     await new Promise(resolve => setTimeout(resolve, 15 * 60 * 1000));
   }
