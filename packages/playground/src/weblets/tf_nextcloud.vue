d0702fb1a6a6f6d2252101de433022e5bba14972
mik-tf <logismos@protonmail.ch>
2023-08-08 01:48:13 -0400

    nextcloud, start with owncloud nextcloud.vue model

--- packages/playground/src/weblets/tf_nextcloud.vue
+++ packages/playground/src/weblets/tf_nextcloud.vue
@@ -7,11 +7,17 @@
     :ipv4="ipv4"
     :certified="certified"
     :dedicated="dedicated"
-    title-image="images/icons/nextcloud.png"
+    title-image="images/icons/owncloud.png"
   >
-    <template #title>Deploy a Nextcloud Instance </template>
+    <template #title>Deploy an OwnCloud Instance </template>
 
-    <d-tabs :tabs="[{ title: 'Base', value: 'base' }]" ref="tabs">
+    <d-tabs
+      :tabs="[
+        { title: 'Base', value: 'base' },
+        { title: 'SMTP Server', value: 'smtp' },
+      ]"
+      ref="tabs"
+    >
       <template #base>
         <input-validator
           :value="name"
@@ -30,13 +36,95 @@
           </input-tooltip>
         </input-validator>
 
+        <input-validator
+          :value="username"
+          :rules="[
+            validators.required('Username is required.'),
+            validators.isLowercase('Username should consist of lowercase letters only.'),
+            validators.isAlphanumeric('Username should consist of letters and numbers only.'),
+            username => validators.isAlpha('Username must start with alphabet char.')(username[0]),
+            validators.minLength('Username must be at least 2 characters.', 2),
+            validators.maxLength('Username cannot exceed 15 characters.', 15),
+          ]"
+          #="{ props }"
+        >
+          <input-tooltip tooltip="OwnCloud admin username.">
+            <v-text-field label="Username" v-model="username" v-bind="props" />
+          </input-tooltip>
+        </input-validator>
+
+        <password-input-wrapper #="{ props }">
+          <input-validator
+            :value="password"
+            :rules="[
+              validators.required('Password is required.'),
+              validators.minLength('Password must be at least 6 characters.', 6),
+              validators.maxLength('Password cannot exceed 15 characters.', 15),
+            ]"
+            #="{ props: validatorProps }"
+          >
+            <input-tooltip tooltip="OwnCloud admin password.">
+              <v-text-field label="Password" v-model="password" v-bind="{ ...props, ...validatorProps }" />
+            </input-tooltip>
+          </input-validator>
+        </password-input-wrapper>
+
         <SelectSolutionFlavor
           v-model="solution"
-          :minimum="{ cpu: 2, memory: 1024 * 4, disk: 50 }"
-          :standard="{ cpu: 2, memory: 1024 * 8, disk: 500 }"
+          :minimum="{ cpu: 2, memory: 1024 * 16, disk: 250 }"
+          :standard="{ cpu: 2, memory: 1024 * 16, disk: 500 }"
           :recommended="{ cpu: 4, memory: 1024 * 16, disk: 1000 }"
         />
         <Networks v-model:ipv4="ipv4" />
+        <FarmGatewayManager>
+          <input-tooltip
+            inline
+            tooltip="Click to know more about dedicated nodes."
+            href="https://manual.grid.tf/dashboard/portal/dashboard_portal_dedicated_nodes.html"
+          >
+            <v-switch color="primary" inset label="Dedicated" v-model="dedicated" hide-details />
+          </input-tooltip>
+          <input-tooltip inline tooltip="Renting capacity on certified nodes is charged 25% extra.">
+            <v-switch color="primary" inset label="Certified" v-model="certified" hide-details />
+          </input-tooltip>
+
+          <SelectFarmManager>
+            <SelectFarm
+              :filters="{
+                cpu: solution?.cpu,
+                memory: solution?.memory,
+                ssd: solution?.disk + rootFilesystemSize,
+                publicIp: ipv4,
+                rentedBy: dedicated ? profileManager.profile?.twinId : undefined,
+                certified: certified,
+              }"
+              v-model="farm"
+            />
+            <SelectNode
+              v-model="selectedNode"
+              :filters="{
+                farmId: farm?.farmID,
+                cpu: solution?.cpu,
+                memory: solution?.memory,
+                disks: [
+                  {
+                    size: solution?.disk,
+                    mountPoint: '/var/lib/docker',
+                  },
+                ],
+                rentedBy: dedicated ? profileManager.profile?.twinId : undefined,
+                certified: certified,
+              }"
+            />
+          </SelectFarmManager>
+          <DomainName :hasIPv4="ipv4" ref="domainNameCmp" />
+        </FarmGatewayManager>
+      </template>
+
+      <template #smtp>
+        <SmtpServer v-model="smtp" tls ssl email>
+          Configure these settings only If you have an smtp service and you know what you’re doing.
+        </SmtpServer>
       </template>
     </d-tabs>
 
@@ -59,9 +147,10 @@ import { computed, type Ref, ref } from "vue";
 
 import { useLayout } from "../components/weblet_layout.vue";
 import { useProfileManager } from "../stores";
-import type { Farm, Flist, solutionFlavor as SolutionFlavor } from "../types";
+import type { Farm, Flist, GatewayNode, solutionFlavor as SolutionFlavor } from "../types";
 import { ProjectName } from "../types";
 import { deployVM } from "../utils/deploy_vm";
+import { deployGatewayName, getSubdomain, rollbackDeployment } from "../utils/gateway";
 import { getGrid } from "../utils/grid";
 import { generateName, generatePassword } from "../utils/strings";
 
@@ -69,13 +158,13 @@ const layout = useLayout();
 const tabs = ref();
 const profileManager = useProfileManager();
 
-const name = ref(generateName(9, { prefix: "nc" }));
+const name = ref(generateName(9, { prefix: "oc" }));
 const username = ref("admin");
 const password = ref(generatePassword());
 const solution = ref() as Ref<SolutionFlavor>;
 const farm = ref() as Ref<Farm>;
 const flist: Flist = {
-  value: "https://hub.grid.tf/idrnd.3bot/logismosis-nextcloud-aio2-latest.flist",
+  value: "https://hub.grid.tf/tf-official-apps/owncloud-10.9.1.flist",
   entryPoint: "/sbin/zinit init",
 };
 const dedicated = ref(false);
@@ -84,33 +173,136 @@ const selectedNode = ref() as Ref<INode>;
 const ipv4 = ref(false);
 const domainNameCmp = ref();
 
-const rootFilesystemSize = 8;
+const smtp = ref(createSMTPServer());
+const rootFilesystemSize = computed(() => rootFs(solution.value?.cpu ?? 0, solution.value?.memory ?? 0));
 function finalize(deployment: any) {
   layout.value.reloadDeploymentsList();
-  layout.value.setStatus("success", "Successfully deployed a Nextcloud instance.");
-  layout.value.openDialog(deployment, deploymentListEnvironments.nextcloud);
+  layout.value.setStatus("success", "Successfully deployed a owncloud instance.");
+  layout.value.openDialog(deployment, deploymentListEnvironments.owncloud);
 }
 
+async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
+  layout.value.setStatus("deploy");
+
+  const projectName = ProjectName.Owncloud.toLowerCase();
+
+  const subdomain = getSubdomain({
+    deploymentName: name.value,
+    projectName,
+    twinId: profileManager.profile!.twinId,
+  });
+  const domain = customDomain ? gatewayName.domain : subdomain + "." + gatewayName.domain;
+
+  let grid: GridClient | null;
+  let vm: any;
+
+  try {
+    layout.value.validateSsh();
+    grid = await getGrid(profileManager.profile!, projectName);
+
+    await layout.value.validateBalance(grid!);
+
+    vm = await deployVM(grid!, {
+      name: name.value,
+      network: {
+        accessNodeId: gatewayName.id,
+        addAccess: !!gatewayName.id,
+      },
+      machines: [
+        {
+          name: name.value,
+          cpu: solution.value.cpu,
+          memory: solution.value.memory,
+          disks: [
+            {
+              size: solution.value.disk,
+              mountPoint: "/var/lib/docker",
+            },
+          ],
+          flist: flist.value,
+          entryPoint: flist.entryPoint,
+          rootFilesystemSize: rootFilesystemSize.value,
+          farmId: farm.value.farmID,
+          farmName: farm.value.name,
+          publicIpv4: ipv4.value,
+          country: farm.value.country,
+          planetary: true,
+          envs: [
+            { key: "SSH_KEY", value: profileManager.profile!.ssh },
+            { key: "OWNCLOUD_ADMIN_USERNAME", value: username.value },
+            { key: "OWNCLOUD_ADMIN_PASSWORD", value: password.value },
+            { key: "OWNCLOUD_DOMAIN", value: domain },
+            ...(smtp.value.enabled
+              ? [
+                  {
+                    key: "OWNCLOUD_MAIL_SMTP_SECURE",
+                    value: smtp.value.tls ? "tls" : smtp.value.ssl ? "ssl" : "none",
+                  },
+                  { key: "OWNCLOUD_MAIL_FROM_ADDRESS", value: smtp.value.email.split("@")[0] },
+                  { key: "OWNCLOUD_MAIL_DOMAIN", value: smtp.value.email.split("@")[1] },
+                  { key: "OWNCLOUD_MAIL_SMTP_HOST", value: smtp.value.hostname },
+                  { key: "OWNCLOUD_MAIL_SMTP_PORT", value: smtp.value.port.toString() },
+                  { key: "OWNCLOUD_MAIL_SMTP_NAME", value: smtp.value.username },
+                  { key: "OWNCLOUD_MAIL_SMTP_PASSWORD", value: smtp.value.password },
+                ]
+              : []),
+          ],
+          nodeId: selectedNode.value.nodeId,
+          rentedBy: dedicated.value ? grid!.twinId : undefined,
+          certified: certified.value,
+        },
+      ],
+    });
+  } catch (e) {
+    return layout.value.setStatus("failed", normalizeError(e, "Failed to deploy a owncloud instance."));
+  }
+  if (customDomain && ipv4.value) {
+    vm[0].customDomain = gatewayName.domain;
+    finalize(vm);
+    return;
+  }
+  try {
+    layout.value.setStatus("deploy", "Preparing to deploy gateway...");
+    await deployGatewayName(grid!, {
+      name: subdomain,
+      nodeId: gatewayName.id!,
+      ip: vm[0].interfaces[0].ip,
+      port: 80,
+      networkName: vm[0].interfaces[0].network,
+      fqdn: gatewayName?.useFQDN ? gatewayName.domain : undefined,
+    });
+
+    finalize(vm);
+  } catch (e) {
+    layout.value.setStatus("deploy", "Rollbacking back due to fail to deploy gateway...");
+    await rollbackDeployment(grid!, name.value);
+    layout.value.setStatus("failed", normalizeError(e, "Failed to deploy a owncloud instance."));
+  }
+}
 </script>
 
 <script lang="ts">
 import DomainName from "../components/domain_name.vue";
+import FarmGatewayManager from "../components/farm_gateway_manager.vue";
 import Networks from "../components/networks.vue";
 import SelectFarm from "../components/select_farm.vue";
 import SelectFarmManager from "../components/select_farm_manager.vue";
 import SelectNode from "../components/select_node.vue";
 import SelectSolutionFlavor from "../components/select_solution_flavor.vue";
+import SmtpServer, { createSMTPServer } from "../components/smtp_server.vue";
 import { deploymentListEnvironments } from "../constants";
 import type { INode } from "../utils/filter_nodes";
 import { normalizeError } from "../utils/helpers";
 import rootFs from "../utils/root_fs";
 
 export default {
-  name: "TfNextcloud",
+  name: "TfOwncloud",
   components: {
+    SmtpServer,
     SelectSolutionFlavor,
     Networks,
     DomainName,
+    FarmGatewayManager,
     SelectFarm,
     SelectNode,
     SelectFarmManager,
