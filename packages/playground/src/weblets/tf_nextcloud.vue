1c079d6ed4200a7f2ba77e2d80fdce6961828930
mik-tf <logismos@protonmail.ch>
2023-08-08 02:34:18 -0400

    nextcloud.vue update

--- packages/playground/src/weblets/tf_nextcloud.vue
+++ packages/playground/src/weblets/tf_nextcloud.vue
@@ -1,30 +1,32 @@
 <template>
   <weblet-layout
     ref="layout"
+    @mount="layoutMount"
     :cpu="solution?.cpu"
     :memory="solution?.memory"
-    :disk="solution?.disk + rootFilesystemSize"
+    :disk="disks.reduce((total, disk) => total + disk.size, rootFilesystemSize)"
     :ipv4="ipv4"
     :certified="certified"
     :dedicated="dedicated"
-    title-image="images/icons/owncloud.png"
+    title-image="images/icons/nextcloud.png"
   >
-    <template #title>Deploy an OwnCloud Instance </template>
+    <template #title>Deploy a Nextcloud All-in-One Instance </template>
 
     <d-tabs
       :tabs="[
-        { title: 'Base', value: 'base' },
-        { title: 'SMTP Server', value: 'smtp' },
+        { title: 'Config', value: 'config' },
+        { title: 'Environment Variables', value: 'env' },
+        { title: 'Disks', value: 'disks' },
       ]"
       ref="tabs"
     >
-      <template #base>
+      <template #config>
         <input-validator
           :value="name"
           :rules="[
             validators.required('Name is required.'),
             validators.isLowercase('Name should consist of lowercase letters only.'),
-            validators.isAlphanumeric('Name should consist of letters and numbers only.'),
+            validators.isAlphanumeric('Name should consist of alphabets & numbers only.'),
             name => validators.isAlpha('Name must start with alphabet char.')(name[0]),
             validators.minLength('Name must be at least 2 characters.', 2),
             validators.maxLength('Name cannot exceed 15 characters.', 15),
@@ -36,275 +38,268 @@
           </input-tooltip>
         </input-validator>
 
-        <input-validator
-          :value="username"
-          :rules="[
-            validators.required('Username is required.'),
-            validators.isLowercase('Username should consist of lowercase letters only.'),
-            validators.isAlphanumeric('Username should consist of letters and numbers only.'),
-            username => validators.isAlpha('Username must start with alphabet char.')(username[0]),
-            validators.minLength('Username must be at least 2 characters.', 2),
-            validators.maxLength('Username cannot exceed 15 characters.', 15),
-          ]"
-          #="{ props }"
-        >
-          <input-tooltip tooltip="OwnCloud admin username.">
-            <v-text-field label="Username" v-model="username" v-bind="props" />
-          </input-tooltip>
-        </input-validator>
-
-        <password-input-wrapper #="{ props }">
-          <input-validator
-            :value="password"
-            :rules="[
-              validators.required('Password is required.'),
-              validators.minLength('Password must be at least 6 characters.', 6),
-              validators.maxLength('Password cannot exceed 15 characters.', 15),
-            ]"
-            #="{ props: validatorProps }"
-          >
-            <input-tooltip tooltip="OwnCloud admin password.">
-              <v-text-field label="Password" v-model="password" v-bind="{ ...props, ...validatorProps }" />
-            </input-tooltip>
-          </input-validator>
-        </password-input-wrapper>
-
+        <SelectVmImage :images="images" v-model="flist" />
         <SelectSolutionFlavor
-          v-model="solution"
-          :minimum="{ cpu: 2, memory: 1024 * 16, disk: 250 }"
-          :standard="{ cpu: 2, memory: 1024 * 16, disk: 500 }"
+          :minimum="{ cpu: 2, memory: 1024 * 4, disk: 50 }"
+          :standard="{ cpu: 4, memory: 1024 * 8, disk: 500 }"
           :recommended="{ cpu: 4, memory: 1024 * 16, disk: 1000 }"
+          v-model="solution"
         />
-        <Networks v-model:ipv4="ipv4" />
-        <FarmGatewayManager>
-          <input-tooltip
-            inline
-            tooltip="Click to know more about dedicated nodes."
-            href="https://manual.grid.tf/dashboard/portal/dashboard_portal_dedicated_nodes.html"
-          >
-            <v-switch color="primary" inset label="Dedicated" v-model="dedicated" hide-details />
-          </input-tooltip>
-          <input-tooltip inline tooltip="Renting capacity on certified nodes is charged 25% extra.">
-            <v-switch color="primary" inset label="Certified" v-model="certified" hide-details />
-          </input-tooltip>
 
-          <SelectFarmManager>
-            <SelectFarm
-              :filters="{
-                cpu: solution?.cpu,
-                memory: solution?.memory,
-                ssd: solution?.disk + rootFilesystemSize,
-                publicIp: ipv4,
-                rentedBy: dedicated ? profileManager.profile?.twinId : undefined,
-                certified: certified,
-              }"
-              v-model="farm"
-            />
-            <SelectNode
-              v-model="selectedNode"
-              :filters="{
-                farmId: farm?.farmID,
-                cpu: solution?.cpu,
-                memory: solution?.memory,
-                disks: [
-                  {
-                    size: solution?.disk,
-                    mountPoint: '/var/lib/docker',
-                  },
-                ],
-                rentedBy: dedicated ? profileManager.profile?.twinId : undefined,
-                certified: certified,
-              }"
-            />
-          </SelectFarmManager>
-          <DomainName :hasIPv4="ipv4" ref="domainNameCmp" />
-        </FarmGatewayManager>
+        <Network
+          required
+          v-model:ipv4="ipv4"
+          v-model:planetary="planetary"
+          v-model:wireguard="wireguard"
+          ref="network"
+        />
+        <input-tooltip
+          inline
+          tooltip="Click to know more about dedicated nodes."
+          href="https://manual.grid.tf/dashboard/portal/dashboard_portal_dedicated_nodes.html"
+        >
+          <v-switch color="primary" inset label="Dedicated" v-model="dedicated" hide-details />
+        </input-tooltip>
+
+        <input-tooltip inline tooltip="Renting capacity on certified nodes is charged 25% extra.">
+          <v-switch color="primary" inset label="Certified" v-model="certified" hide-details />
+        </input-tooltip>
+
+        <SelectFarmManager>
+          <SelectFarm
+            :filters="{
+              cpu: solution?.cpu,
+              memory: solution?.memory,
+              publicIp: ipv4,
+              ssd: disks.reduce((total, disk) => total + disk.size, rootFilesystemSize),
+              rentedBy: dedicated ? profileManager.profile?.twinId : undefined,
+              certified: certified,
+            }"
+            v-model="farm"
+          />
+          <SelectNode
+            v-model="selectedNode"
+            :filters="{
+              farmId: farm?.farmID,
+              cpu: solution?.cpu,
+              memory: solution?.memory,
+              ipv4: ipv4,
+              ipv6: ipv4,
+              disks: disks,
+              rentedBy: dedicated ? profileManager.profile?.twinId : undefined,
+              certified: certified,
+            }"
+          />
+        </SelectFarmManager>
       </template>
 
-      <template #smtp>
-        <SmtpServer v-model="smtp" tls ssl email>
-          Configure these settings only If you have an smtp service and you know what you’re doing.
-        </SmtpServer>
+      <template #env>
+        <ExpandableLayout
+          v-model="envs"
+          @add="envs.push({ key: '', value: '' })"
+          #="{ index, isRequired }"
+          :required="[0]"
+        >
+          <input-validator
+            :value="envs[index].key"
+            :rules="[
+              validators.required('Key name is required.'),
+              key => validators.isAlpha('Key must start with alphabet char.')(key[0]),
+              validators.pattern('Invalid key format.', { pattern: /^[^0-9_\s][a-zA-Z0-9_]+$/ }),
+              validators.maxLength('Key max length is 128 chars.', 128),
+            ]"
+            #="{ props }"
+          >
+            <input-tooltip tooltip="Environment key.">
+              <v-text-field label="Name" v-model="envs[index].key" :disabled="isRequired" v-bind="props" />
+            </input-tooltip>
+          </input-validator>
+
+          <input-validator
+            :value="envs[index].value"
+            :rules="[validators.required('Value is required.')]"
+            #="{ props }"
+          >
+            <input-tooltip tooltip="Environment Value.">
+              <v-textarea label="Value" v-model="envs[index].value" no-resize :spellcheck="false" />
+            </input-tooltip>
+          </input-validator>
+        </ExpandableLayout>
+      </template>
+
+      <template #disks>
+        <ExpandableLayout
+          v-model="disks"
+          @add="addDisk"
+          title="Add additional disk space to your micro virtual machine"
+          #="{ index }"
+        >
+          <p class="text-h6 mb-4">Disk #{{ index + 1 }}</p>
+          <input-validator
+            :value="disks[index].name"
+            :rules="[
+              validators.required('Disk name is required.'),
+              validators.pattern('Disk name can\'t start with a number, a non-alphanumeric character or a whitespace', {
+                pattern: /^[A-Za-z]/,
+              }),
+              validators.minLength('Disk minLength is 2 chars.', 2),
+              validators.isAlphanumeric('Disk name only accepts alphanumeric chars.'),
+              validators.maxLength('Disk maxLength is 15 chars.', 15),
+            ]"
+            #="{ props }"
+          >
+            <input-tooltip tooltip="Disk name.">
+              <v-text-field label="Name" v-model="disks[index].name" v-bind="props" />
+            </input-tooltip>
+          </input-validator>
+          <input-validator
+            :value="disks[index].size"
+            :rules="[
+              validators.required('Disk size is required.'),
+              validators.isInt('Disk size must be a valid integer.'),
+              validators.min('Minimum allowed disk size is 1 GB.', 1),
+              validators.max('Maximum allowed disk size is 10000 GB.', 10000),
+            ]"
+            #="{ props }"
+          >
+            <input-tooltip tooltip="Disk Size.">
+              <v-text-field label="Size (GB)" type="number" v-model.number="disks[index].size" v-bind="props" />
+            </input-tooltip>
+          </input-validator>
+        </ExpandableLayout>
       </template>
     </d-tabs>
 
     <template #footer-actions>
-      <v-btn
-        color="primary"
-        variant="tonal"
-        @click="deploy(domainNameCmp?.domain, domainNameCmp?.customDomain)"
-        :disabled="tabs?.invalid"
-      >
-        Deploy
-      </v-btn>
+      <v-btn color="primary" variant="tonal" :disabled="tabs?.invalid || network?.error" @click="deploy">Deploy</v-btn>
     </template>
   </weblet-layout>
 </template>
 
 <script lang="ts" setup>
-import type { GridClient } from "@threefold/grid_client";
 import { computed, type Ref, ref } from "vue";
 
+import Network from "../components/networks.vue";
+import SelectSolutionFlavor from "../components/select_solution_flavor.vue";
 import { useLayout } from "../components/weblet_layout.vue";
 import { useProfileManager } from "../stores";
-import type { Farm, Flist, GatewayNode, solutionFlavor as SolutionFlavor } from "../types";
-import { ProjectName } from "../types";
-import { deployVM } from "../utils/deploy_vm";
-import { deployGatewayName, getSubdomain, rollbackDeployment } from "../utils/gateway";
+import { type Farm, type Flist, ProjectName } from "../types";
+import { deployVM, type Disk, type Env } from "../utils/deploy_vm";
 import { getGrid } from "../utils/grid";
-import { generateName, generatePassword } from "../utils/strings";
+import { generateName } from "../utils/strings";
 
 const layout = useLayout();
 const tabs = ref();
 const profileManager = useProfileManager();
 
-const name = ref(generateName(9, { prefix: "oc" }));
-const username = ref("admin");
-const password = ref(generatePassword());
-const solution = ref() as Ref<SolutionFlavor>;
+const images = [
+  {
+    name: "Nextcloud All-in-One",
+    flist: "https://hub.grid.tf/idrnd.3bot/logismosis-nextcloud-aio2-latest.flist",
+    entryPoint: "/sbin/zinit init",
+  },
+];
+
+const name = ref(generateName(8, { prefix: "nc" }));
+const flist = ref<Flist>();
+const ipv4 = true;
+const planetary = ref(true);
+const wireguard = ref(false);
 const farm = ref() as Ref<Farm>;
-const flist: Flist = {
-  value: "https://hub.grid.tf/tf-official-apps/owncloud-10.9.1.flist",
-  entryPoint: "/sbin/zinit init",
-};
+const envs = ref<Env[]>([]);
+const disks = ref<Disk[]>([]);
+const network = ref();
 const dedicated = ref(false);
 const certified = ref(false);
 const selectedNode = ref() as Ref<INode>;
-const ipv4 = ref(false);
-const domainNameCmp = ref();
+const rootFilesystemSize = 8;
+function layoutMount() {
+  if (envs.value.length > 0) {
+    envs.value.splice(0, 1);
+  }
 
-const smtp = ref(createSMTPServer());
-const rootFilesystemSize = computed(() => rootFs(solution.value?.cpu ?? 0, solution.value?.memory ?? 0));
-function finalize(deployment: any) {
-  layout.value.reloadDeploymentsList();
-  layout.value.setStatus("success", "Successfully deployed a owncloud instance.");
-  layout.value.openDialog(deployment, deploymentListEnvironments.owncloud);
+  envs.value.unshift({
+    key: "SSH_KEY",
+    value: profileManager.profile!.ssh,
+  });
 }
 
-async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
+function addDisk() {
+  const name = generateName(5);
+  disks.value.push({
+    name: "disk" + name,
+    size: 50,
+    mountPoint: "/mnt/" + name,
+  });
+}
+
+async function deploy() {
   layout.value.setStatus("deploy");
 
-  const projectName = ProjectName.Owncloud.toLowerCase();
-
-  const subdomain = getSubdomain({
-    deploymentName: name.value,
-    projectName,
-    twinId: profileManager.profile!.twinId,
-  });
-  const domain = customDomain ? gatewayName.domain : subdomain + "." + gatewayName.domain;
-
-  let grid: GridClient | null;
-  let vm: any;
+  const projectName = ProjectName.Nextcloud.toLowerCase();
 
   try {
-    layout.value.validateSsh();
-    grid = await getGrid(profileManager.profile!, projectName);
+    const grid = await getGrid(profileManager.profile!, projectName);
 
     await layout.value.validateBalance(grid!);
 
-    vm = await deployVM(grid!, {
+    const vm = await deployVM(grid!, {
       name: name.value,
       network: {
-        accessNodeId: gatewayName.id,
-        addAccess: !!gatewayName.id,
+        addAccess: wireguard.value,
       },
       machines: [
         {
           name: name.value,
           cpu: solution.value.cpu,
           memory: solution.value.memory,
-          disks: [
-            {
-              size: solution.value.disk,
-              mountPoint: "/var/lib/docker",
-            },
-          ],
-          flist: flist.value,
-          entryPoint: flist.entryPoint,
-          rootFilesystemSize: rootFilesystemSize.value,
+          flist: flist.value!.value,
+          entryPoint: flist.value!.entryPoint,
           farmId: farm.value.farmID,
           farmName: farm.value.name,
-          publicIpv4: ipv4.value,
           country: farm.value.country,
-          planetary: true,
-          envs: [
-            { key: "SSH_KEY", value: profileManager.profile!.ssh },
-            { key: "OWNCLOUD_ADMIN_USERNAME", value: username.value },
-            { key: "OWNCLOUD_ADMIN_PASSWORD", value: password.value },
-            { key: "OWNCLOUD_DOMAIN", value: domain },
-            ...(smtp.value.enabled
-              ? [
-                  {
-                    key: "OWNCLOUD_MAIL_SMTP_SECURE",
-                    value: smtp.value.tls ? "tls" : smtp.value.ssl ? "ssl" : "none",
-                  },
-                  { key: "OWNCLOUD_MAIL_FROM_ADDRESS", value: smtp.value.email.split("@")[0] },
-                  { key: "OWNCLOUD_MAIL_DOMAIN", value: smtp.value.email.split("@")[1] },
-                  { key: "OWNCLOUD_MAIL_SMTP_HOST", value: smtp.value.hostname },
-                  { key: "OWNCLOUD_MAIL_SMTP_PORT", value: smtp.value.port.toString() },
-                  { key: "OWNCLOUD_MAIL_SMTP_NAME", value: smtp.value.username },
-                  { key: "OWNCLOUD_MAIL_SMTP_PASSWORD", value: smtp.value.password },
-                ]
-              : []),
-          ],
+          disks: disks.value,
+          envs: envs.value,
+          planetary: planetary.value,
+          publicIpv4: ipv4.value,
+          rootFilesystemSize: rootFilesystemSize.value,
           nodeId: selectedNode.value.nodeId,
           rentedBy: dedicated.value ? grid!.twinId : undefined,
           certified: certified.value,
         },
       ],
     });
-  } catch (e) {
-    return layout.value.setStatus("failed", normalizeError(e, "Failed to deploy a owncloud instance."));
-  }
-  if (customDomain && ipv4.value) {
-    vm[0].customDomain = gatewayName.domain;
-    finalize(vm);
-    return;
-  }
-  try {
-    layout.value.setStatus("deploy", "Preparing to deploy gateway...");
-    await deployGatewayName(grid!, {
-      name: subdomain,
-      nodeId: gatewayName.id!,
-      ip: vm[0].interfaces[0].ip,
-      port: 80,
-      networkName: vm[0].interfaces[0].network,
-      fqdn: gatewayName?.useFQDN ? gatewayName.domain : undefined,
-    });
 
-    finalize(vm);
+    layout.value.reloadDeploymentsList();
+    layout.value.setStatus("success", "Successfully deployed a Nextcloud AIO instance.");
+    layout.value.openDialog(vm, deploymentListEnvironments.vm);
   } catch (e) {
-    layout.value.setStatus("deploy", "Rollbacking back due to fail to deploy gateway...");
-    await rollbackDeployment(grid!, name.value);
-    layout.value.setStatus("failed", normalizeError(e, "Failed to deploy a owncloud instance."));
+    layout.value.setStatus("failed", normalizeError(e, "Failed to deploy Nextcloud."));
   }
 }
 </script>
 
 <script lang="ts">
-import DomainName from "../components/domain_name.vue";
-import FarmGatewayManager from "../components/farm_gateway_manager.vue";
-import Networks from "../components/networks.vue";
+import ExpandableLayout from "../components/expandable_layout.vue";
 import SelectFarm from "../components/select_farm.vue";
 import SelectFarmManager from "../components/select_farm_manager.vue";
 import SelectNode from "../components/select_node.vue";
-import SelectSolutionFlavor from "../components/select_solution_flavor.vue";
-import SmtpServer, { createSMTPServer } from "../components/smtp_server.vue";
+import SelectVmImage from "../components/select_vm_image.vue";
 import { deploymentListEnvironments } from "../constants";
+import type { solutionFlavor as SolutionFlavor } from "../types";
 import type { INode } from "../utils/filter_nodes";
 import { normalizeError } from "../utils/helpers";
-import rootFs from "../utils/root_fs";
+
+const solution = ref() as Ref<SolutionFlavor>;
 
 export default {
-  name: "TfOwncloud",
+  name: "MicroVm",
   components: {
-    SmtpServer,
+    SelectVmImage,
     SelectSolutionFlavor,
-    Networks,
-    DomainName,
-    FarmGatewayManager,
     SelectFarm,
     SelectNode,
+    ExpandableLayout,
     SelectFarmManager,
   },
 };
