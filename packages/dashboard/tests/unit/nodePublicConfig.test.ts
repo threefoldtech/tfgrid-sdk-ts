0740cd1cc059a533c91f512cd2218c7c2eff003d
kassem <omarksm09@gmail.com>
2023-08-10 17:26:06 +0300

    add ipv4 class unit tests

--- packages/dashboard/tests/unit/nodePublicConfig.test.ts
+++ packages/dashboard/tests/unit/nodePublicConfig.test.ts
@@ -1,10 +1,17 @@
-import { IP6 } from "../../src/portal/lib/ips";
+import { IP4, IP6 } from "../../src/portal/lib/ips";
+
+test("IP4 contains", () => {
+  const ip4Range = new IP4("192.168.0.0/24");
+  expect(ip4Range.contains("192.168.0.1")).toBe(true);
+  expect(ip4Range.contains("192.168.1.1")).toBe(false);
+});
 
 test("IP6 contains", () => {
   const ip6Range = new IP6("2001:db8::/64");
   expect(ip6Range.contains("2001:db8::1")).toBe(true);
   expect(ip6Range.contains("2001:db8:1234::1")).toBe(false);
 });
+
 test("IP expand", () => {
   const ip6Range = new IP6("2001:db8::/64"); // Creating an IP range with a /64 mask
   expect(ip6Range.expandIPv6("2001:db8::")).toBe("2001:0db8:0000:0000:0000:0000:0000:0000");
@@ -12,5 +19,5 @@ test("IP expand", () => {
   expect(ip6Range.expandIPv6("2001:db8:abcd::")).toBe("2001:0db8:abcd:0000:0000:0000:0000:0000");
   expect(ip6Range.expandIPv6("1::")).toBe("0001:0000:0000:0000:0000:0000:0000:0000");
   expect(ip6Range.expandIPv6("::1")).toBe("0000:0000:0000:0000:0000:0000:0000:0001");
-  expect(ip6Range.expandIPv6("12::1::")).toBe("0012:0000:0001:0000:0000:0000:0000");
+  expect(ip6Range.expandIPv6("12::1::")).toBe("0012:0000:0001:0000:0000:0000:0000:0000");
 });
