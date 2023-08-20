e0529b68261f01a7b496f48c1597f7f39788445d
alaamahmoud.1223@gmail.com <alaamahmoud.1223@gmail.com>
2023-08-20 14:32:45 +0300

    remove unnecessary logic

--- packages/playground/src/utils/strings.ts
+++ packages/playground/src/utils/strings.ts
@@ -9,8 +9,7 @@ export interface GenerateNameOptions {
   suffix?: string;
 }
 export function generateName(length = 12, options?: GenerateNameOptions): string {
-  const vowels = "aeiou";
-  const consonants = "bcdfghjklmnpqrstvwxyz";
+  const chars = "abcdefghijklmnopqrstuvwxyz";
   const nums = "0123456789";
 
   const prefix = options?.prefix ?? "";
@@ -19,10 +18,8 @@ export function generateName(length = 12, options?: GenerateNameOptions): string
 
   return (
     (options?.prefix ?? "") +
-    randomChoice(consonants) +
-    generateString(vowels + consonants, Math.ceil((remainingLength - 2) / 2)) +
-    generateString(consonants, Math.floor(remainingLength / 3)) +
-    generateString(nums, Math.floor(remainingLength / 6)) +
+    randomChoice(chars) +
+    generateString(chars + nums, remainingLength - 1) +
     (options?.suffix ?? "")
   );
 }
