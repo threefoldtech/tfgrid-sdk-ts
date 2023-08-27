0aa08679a785d7285da8690cbdc5dcb3395e71ae
zaelgohary <zainabelgohary19@gmail.com>
2023-08-27 18:42:13 +0300

    Hide freeflow card

--- packages/playground/src/views/solutions_view.vue
+++ packages/playground/src/views/solutions_view.vue
@@ -150,13 +150,13 @@ export default {
         icon: "umbrel.png",
         route: "/umbrel",
       },
-      {
-        title: "Freeflow",
-        excerpt: `Freeflow is a convenient ecosystem on top of a resilient internet grid. We bring you a new internet with a set of
-productivity tools so you can enhance collaboration within your country, your company, your community.`,
-        icon: "freeflow.png",
-        route: "/freeflow",
-      },
+      //       {
+      //         title: "Freeflow",
+      //         excerpt: `Freeflow is a convenient ecosystem on top of a resilient internet grid. We bring you a new internet with a set of
+      // productivity tools so you can enhance collaboration within your country, your company, your community.`,
+      //         icon: "freeflow.png",
+      //         route: "/freeflow",
+      //       },
       {
         title: "Wordpress",
         excerpt:
