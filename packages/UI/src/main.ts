c2f5bb28534ea5c092df881c7d211b604aab9266
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2023-08-30 12:02:31 +0300

    Removed vuetify

--- packages/UI/src/main.ts
+++ packages/UI/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 
 import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
 import router from "./router";
 
-createApp(App).use(router).use(vuetify).mount("#app");
+createApp(App).use(router).mount("#app");
