0e729bdbae6cf63e8d08162e290110bd078799bc
mayar osama <mayarosama18@gmail.com>
2023-09-03 17:21:21 +0300

    updating cards

--- packages/playground/src/components/statistics_card.vue
+++ packages/playground/src/components/statistics_card.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="mt-8">
-    <v-card color="primary" class="my-3 pa-3 text-center">
-      <div>
-        <v-icon size="30">{{ item.icon }}</v-icon>
-        <h4 class="d-inline">{{ item.title }}</h4>
-        <v-divider class="mt-1" />
-        <p class="text-center">{{ item.data }}</p>
-      </div>
+    <v-card color="primary" class="pa-3 pt-6" height="200">
+      <v-icon class="ml-3">{{ item.icon }} </v-icon>
+      <v-card-title> {{ item.title }} </v-card-title>
+      <v-divider class="mt-1" />
+
+      <v-card-text> {{ item.data }} </v-card-text>
     </v-card>
   </div>
 </template>
