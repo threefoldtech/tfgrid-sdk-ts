016d4d9ae78000d641030dfee61682b90144a3a1
ehab-hassan <hassane@codescalers.com>
2023-09-04 13:00:34 +0300

    update stats ui

--- packages/playground/src/components/statistics_card.vue
+++ packages/playground/src/components/statistics_card.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="mt-8">
-    <v-card color="primary" class="pa-3 pt-6" height="200">
-      <v-icon class="ml-3">{{ item.icon }} </v-icon>
-      <v-card-title> {{ item.title }} </v-card-title>
+  <div class="mt-2 text-center">
+    <v-card color="primary" class="pa-3 pt-4">
+      <div class="d-flex justify-center items-center pa-2">
+        <v-icon class="mr-3">{{ item.icon }} </v-icon>
+        {{ item.title }}
+      </div>
+
       <v-divider class="mt-1" />
 
-      <v-card-text> {{ item.data }} </v-card-text>
+      <v-card-title> {{ item.data }} </v-card-title>
     </v-card>
   </div>
 </template>
