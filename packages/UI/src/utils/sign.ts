09b1df57d0209ee014a9d5ff06be31219c7c96eb
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2023-09-10 17:53:03 +0300

    Implemeneted an interface with two types of the PDF provider, 1. with ThreefoldConnector, 2. NormalProvider with MNEMONIC.

--- packages/UI/src/utils/sign.ts
+++ packages/UI/src/utils/sign.ts
@@ -8,16 +8,23 @@ export enum KeypairType {
   ed25519 = "ed25519",
 }
 
-export const sign = async (content: string, mnemonic: string, keypairType: KeypairType): Promise<string> => {
-  const hash = MD5(content.toString());
-  const message_bytes = Uint8Array.from(Buffer.from(hash.toString(), "hex"));
+export interface SignReturn {
+  publicKey: string;
+  signature: string;
+}
+
+export const sign = async (content: string, mnemonic: string, keypairType: KeypairType): Promise<SignReturn> => {
+  const hash = MD5(content.toString()).toString();
+  const messageBytes = Uint8Array.from(Buffer.from(hash.toString(), "hex"));
   const keyr = new Keyring({ type: keypairType });
 
   await waitReady();
 
   const key = keyr.addFromMnemonic(mnemonic);
-  const signed = key.sign(message_bytes);
+  const publicKey = key.address;
+
+  const signed = key.sign(messageBytes);
   const signature = Buffer.from(signed).toString("hex");
 
-  return signature;
+  return { signature, publicKey };
 };
