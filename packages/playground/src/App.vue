a6fb315995de4bb8989c3fb9160d26b2771af8a2
Alaa <alaamahmoud.1223@gmail.com>
2023-09-12 11:12:21 +0300

    update position of button

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -88,9 +88,12 @@
         <v-toolbar-title>
           <v-img :src="baseUrl + 'images/logoTF.png'" width="160px" />
         </v-toolbar-title>
-        <FundsCard v-if="hasActiveProfile"></FundsCard>
 
-        <v-spacer></v-spacer>
+        <v-spacer>
+          <div class="d-flex align-center justify-start">
+            <FundsCard v-if="hasActiveProfile"></FundsCard>
+          </div>
+        </v-spacer>
         <v-btn class="capitalize" :style="{ pointerEvents: 'none' }" variant="text"> {{ network }}net </v-btn>
         <v-divider vertical class="mx-2" />
         <AppTheme />
