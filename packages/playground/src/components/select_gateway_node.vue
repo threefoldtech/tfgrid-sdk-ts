6cd7edb3297d43df2403e57484a90ea4975e639e
zaelgohary <zainabelgohary19@gmail.com>
2023-09-14 15:14:24 +0300

    Fix select node validation

--- packages/playground/src/components/select_gateway_node.vue
+++ packages/playground/src/components/select_gateway_node.vue
@@ -110,7 +110,7 @@ async function loadNextPage() {
   loading.value = false;
 
   if (props.modelValue && !nodeExists) emits("update:model-value", undefined);
-  if (nodeExists) validator.value?.validate();
+  if (nodeExists) await validator.value?.validate();
 }
 defineExpose({ loading });
 function normalizeGatewayNode(item: any): GatewayNode {
