10a8803aeef3fbffa5acf52d2b09a474cc42b4ee
amiraabouhadid <amiraabouhadid@gmail.com>
2023-09-21 12:45:50 +0300

    remove unnecessary import

--- packages/grid_client/src/clients/tf-grid/index.ts
+++ packages/grid_client/src/clients/tf-grid/index.ts
@@ -3,6 +3,5 @@ export * from "./contracts";
 export * from "./twins";
 export * from "./kvstore";
 export * from "./balances";
-export * from "./dao";
 export * from "./tftPrice";
 export * from "./terms_and_conditions";
