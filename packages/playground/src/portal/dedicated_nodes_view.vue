bf08498f3c80dabe8104b8a3f941d450b14d18e3
AlaaElattar <alaamahmoud.1223@gmail.com>
2023-09-26 17:21:33 +0300

    add table tabs

--- packages/playground/src/portal/dedicated_nodes_view.vue
+++ packages/playground/src/portal/dedicated_nodes_view.vue
@@ -8,11 +8,23 @@
 
       <!-- Filters -->
       <dedicated-node-filters :filters="filters" />
+      <div class="pt-5">
+        <v-card>
+          <v-tabs v-model="activeTab" align-tabs="center">
+            <v-tab :value="0" color="primary" class="pr-8">Rentable</v-tab>
+            <v-tab :value="1" color="primary" class="pr-8">Rented</v-tab>
+            <v-tab :value="2" color="primary" class="pr-8">Mine</v-tab>
+          </v-tabs>
+        </v-card>
+      </div>
     </v-container>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+const activeTab = ref(0);
+
 const filters = [
   {
     label: "Total SRU (GB)",
