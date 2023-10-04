f76087975385901786a0ccf554ab9dca4b8e24ad
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2023-10-04 12:27:29 +0300

    Removed unused console log.

--- packages/playground/src/explorer/stats.vue
+++ packages/playground/src/explorer/stats.vue
@@ -80,7 +80,6 @@ const fetchData = async () => {
   try {
     stats = await fetchStats();
     if (!loading.value || stats != null) {
-      console.log(stats!.nodesDistribution);
       nodesDistribution.value = JSON.stringify(stats!.nodesDistribution);
       Istats.value = [
         { data: stats!.nodes, title: "Nodes Online", icon: "mdi-laptop" },
