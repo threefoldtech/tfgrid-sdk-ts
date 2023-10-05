4423be62527f30c0ee57211f546b38fe99bb64da
AlaaElattar <alaamahmoud.1223@gmail.com>
2023-10-05 15:39:06 +0300

    update title to dedicated nodes

--- packages/playground/src/portal/dedicated_nodes_view.vue
+++ packages/playground/src/portal/dedicated_nodes_view.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-card color="primary" class="d-flex justify-center items-center mt-3 pa-3 text-center">
         <v-icon size="30" class="pr-3">mdi-resistor-nodes</v-icon>
-        <v-card-title class="pa-0" color="white">Dedicated Node</v-card-title>
+        <v-card-title class="pa-0" color="white">Dedicated Nodes</v-card-title>
       </v-card>
 
       <!-- Table -->
