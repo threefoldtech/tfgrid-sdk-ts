f4be2f3d920338955b5c6d82b4828b5617228101
kassem <omarksm09@gmail.com>
2023-10-05 12:28:37 +0300

    init project

---
+++ packages/new_stats/src/components/MapCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>testing stats</div>
+</template>
+<script setup lang="ts">
+import { Network } from "@threefold/gridproxy_client";
+import { onMounted } from "vue";
+
+import { NetworkStats } from "../utils/fetchData";
+onMounted(() => console.log(NetworkStats([Network.Dev])));
+</script>
+<script lang="ts">
+export default {
+  name: "MapCard",
+};
+</script>
---
+++ packages/new_stats/src/utils/fetchData.ts
@@ -0,0 +1,48 @@
+import GridProxyClient, { Network, NodeStatus, type Stats } from "@threefold/gridproxy_client";
+export type NetworkStats = {
+  [network in Network]?: Stats;
+};
+
+/**
+ * Retrieves statistics for multiple networks.
+ *
+ * @param {Network[]} networks - An array of network types for which to retrieve statistics.
+ *
+ * @returns {Promise<NetworkStats>} A promise that resolves to an object containing statistics data for the specified networks.
+ *
+ * @throws {Error} If there is an issue with the network connection or the request for any of the specified networks.
+ */
+export async function NetworkStats(networks: Network[]): Promise<NetworkStats> {
+  const res: NetworkStats = {};
+
+  if (networks.includes(Network.Dev)) {
+    res[Network.Dev] = await getStats(new GridProxyClient(Network.Dev));
+  }
+
+  if (networks.includes(Network.Test)) {
+    res[Network.Test] = await getStats(new GridProxyClient(Network.Test));
+  }
+
+  if (networks.includes(Network.Main)) {
+    res[Network.Main] = await getStats(new GridProxyClient(Network.Main));
+  }
+
+  return res;
+}
+
+/**
+ * Retrieves statistics for a specific network using the provided GridProxyClient.
+ *
+ * @param {GridProxyClient} client - The GridProxyClient instance for the network.
+ *
+ * @returns {Promise<StatsResponse>} A promise that resolves to an object containing statistics data.
+ *
+ * @throws {Error} If there is an issue with the network connection or the request.
+ */
+async function getStats(client: GridProxyClient) {
+  try {
+    return await client.stats.get({ status: NodeStatus.Up });
+  } catch (error) {
+    throw new Error(`Failed to retrieve statistics on ${client.uri}: ${error}`);
+  }
+}
