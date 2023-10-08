855d5369ae0b89bd0e71bd3b52eb9a6f8a54e5c3
kassem <omarksm09@gmail.com>
2023-10-08 15:15:49 +0300

    set themes

--- packages/new_stats/src/main.ts
+++ packages/new_stats/src/main.ts
@@ -2,14 +2,8 @@ import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 
 import { createApp } from "vue";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 
 import App from "./App.vue";
-const vuetify = createVuetify({
-  components,
-  directives,
-});
+import vuetify from "./plugins/vuetify";
 
 createApp(App).use(vuetify).mount("#app");
