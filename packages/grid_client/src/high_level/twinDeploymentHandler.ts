45c7986eff4e9616db3c508c07ea065b2cc9db78
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2023-10-08 19:29:00 +0300

    Update the type of the farmIps, and changed the error message.

--- packages/grid_client/src/high_level/twinDeploymentHandler.ts
+++ packages/grid_client/src/high_level/twinDeploymentHandler.ts
@@ -266,25 +266,29 @@ class TwinDeploymentHandler {
   }
 
   async checkFarmIps(twinDeployments: TwinDeployment[]) {
-    const farms: { [key: string]: { ips: number } } = {};
+    const farmIPs: Map<number, number> = new Map();
     for (const twinDeployment of twinDeployments) {
-      const node = await this.nodes.getNode(twinDeployment.nodeId);
-      if (node) {
-        if (!(node.farmId in farms)) {
-          farms[node.farmId] = { ips: twinDeployment.publicIps };
-        } else {
-          farms[node.farmId] = { ips: farms[node.farmId].ips + twinDeployment.publicIps };
+      if (twinDeployment.operation === Operations.deploy) {
+        const node = await this.nodes.getNode(twinDeployment.nodeId);
+        if (node) {
+          if (!(node.farmId in farmIPs)) {
+            farmIPs.set(node.farmId, twinDeployment.publicIps);
+          } else {
+            farmIPs.set(node.farmId, farmIPs[node.farmId] + twinDeployment.publicIps);
+          }
         }
       }
     }
 
-    for (const farm of Object.keys(farms)) {
+    for (const farm of Object.keys(farmIPs)) {
       const _farm = await this.nodes.filterFarms({
         farmId: +farm,
       });
       const freeIps = _farm[0].publicIps.filter(res => res.contract_id === 0).length;
-      if (freeIps < farms[farm].ips) {
-        throw Error(`Farm ${farm} doesn't have enough ips: requested Ips=${farms[farm].ips}, free Ips=${freeIps}`);
+      if (freeIps < farmIPs[farm]) {
+        throw Error(
+          `Farm ${farm} doesn't have enough public IPs: requested IPs=${farmIPs[farm]}, avaiable IPs=${freeIps}`,
+        );
       }
     }
   }
