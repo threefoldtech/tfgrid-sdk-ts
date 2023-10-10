e62ae1ac4c43cc423cbf395f53077a3771b3a71f
ahmedhanafy725 <hanafya@incubaid.com>
2023-10-10 10:37:50 +0300

    Add network high level

---
+++ packages/grid_client/src/high_level/network.ts
@@ -0,0 +1,53 @@
+import { Addr } from "netaddr";
+
+import { DeploymentFactory, Network } from "../primitives";
+import { WorkloadTypes } from "../zos";
+import { HighLevelBase } from "./base";
+import { Operations, TwinDeployment } from "./models";
+
+class NetworkHL extends HighLevelBase {
+  async addNode(networkName: string, ipRange: string, nodeId: number, solutionProviderId: number, description = "") {
+    const network = new Network(networkName, ipRange, this.config);
+    await network.load();
+    const networkMetadata = JSON.stringify({
+      type: "network",
+      name: networkName,
+      projectName: this.config.projectName,
+    });
+
+    const workload = await network.addNode(nodeId, networkMetadata, description);
+    if (!workload) {
+      throw Error(`Node ${nodeId} is already exist on network ${networkName}`);
+    }
+
+    const twinDeployments: TwinDeployment[] = [];
+    const deploymentFactory = new DeploymentFactory(this.config);
+    const deployment = deploymentFactory.create([workload], 0, networkMetadata, description, 0);
+    twinDeployments.push(
+      new TwinDeployment(deployment, Operations.deploy, 0, nodeId, network, solutionProviderId, true),
+    );
+
+    if (!(await network.exists())) {
+      return twinDeployments;
+    }
+    // update network if it's already exist
+    for (const deployment of network.deployments) {
+      const d = await deploymentFactory.fromObj(deployment);
+      for (const workload of d["workloads"]) {
+        if (
+          workload["type"] !== WorkloadTypes.network ||
+          !Addr(network.ipRange).contains(Addr(workload["data"]["subnet"]))
+        ) {
+          continue;
+        }
+        workload.data = network.updateNetwork(workload["data"]);
+        workload.version += 1;
+        break;
+      }
+      twinDeployments.push(new TwinDeployment(d, Operations.update, 0, 0, network, solutionProviderId, true));
+    }
+    return twinDeployments;
+  }
+}
+
+export { NetworkHL };
