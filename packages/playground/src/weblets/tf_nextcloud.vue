54e35a53c1e57163913749523134d85725eb9aaa
Scott Yeager <yeagersm@gmail.com>
2023-10-16 18:18:09 -0700

    Update tf_nextcloud.vue
    Co-authored-by: Mohamed Rabie <31689104+MohamedElmdary@users.noreply.github.com>

--- packages/playground/src/weblets/tf_nextcloud.vue
+++ packages/playground/src/weblets/tf_nextcloud.vue
@@ -137,7 +137,7 @@ function finalize(deployment: any) {
 async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
   layout.value.setStatus("deploy");
 
-  const projectName = ProjectName.Nextcloud.toLowerCase();
+  const projectName = ProjectName.Nextcloud.toLowerCase() + "/" + name.value;
 
   const subdomain = getSubdomain({
     deploymentName: name.value,
