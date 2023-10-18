5df2dc7005cef0063c2d0ad5917e29329db1345e
kassem <omarksm09@gmail.com>
2023-10-18 12:11:48 +0300

    update network filter

--- packages/new_stats/src/components/network_filter.vue
+++ packages/new_stats/src/components/network_filter.vue
@@ -1,8 +1,14 @@
 <template>
   <v-row class="d-flex pt-4">
     <v-col class="d-flex justify-start py-0 align-center">
-      <div v-for="(network, index) in networks" :key="index" class="px-2">
-        <v-switch hide-details color="primary" inset :label="network + ' Nodes'" />
+      <div v-for="network in networks" :key="network.value" class="px-2">
+        <v-switch
+          hide-details
+          color="primary"
+          @update:model-value="updateNetworks($event, network.value)"
+          inset
+          :label="network.label + ' Nodes'"
+        />
       </div>
     </v-col>
     <v-col class="d-flex justify-end pb-0">
@@ -13,7 +19,32 @@
 </template>
 
 <script lang="ts" setup>
-const networks = ["Dev", "Main", "Test"];
+import { Network } from "@threefold/gridproxy_client";
+import { type Ref, ref } from "vue";
+
+const networks = [
+  { label: "Dev", value: Network.Dev },
+  { label: "Main", value: Network.Main },
+  { label: "Test", value: Network.Test },
+];
+const emits = defineEmits<{
+  (events: "update:modelValue", value?: Network[]): void;
+}>();
+const selectedNetworks = ref([]) as Ref<Network[]>;
+function updateNetworks(event: Event | undefined, network: Network) {
+  if (event) {
+    if (!selectedNetworks.value.includes(network)) {
+      selectedNetworks.value.push(network);
+    }
+  } else {
+    if (selectedNetworks.value.includes(network)) {
+      selectedNetworks.value.forEach((element, index) => {
+        if (network == element) selectedNetworks.value.splice(index, 1);
+      });
+    }
+  }
+  emits("update:modelValue", selectedNetworks.value);
+}
 </script>
 <script lang="ts">
 export default {
