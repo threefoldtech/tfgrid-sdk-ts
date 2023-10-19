42518ac62c15ddfe4bbf85202121cb375c12baf5
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2023-10-19 15:39:54 +0300

    Fix unsetting the color for another childs, Change the colors of the icons/contents of all opened toast to be  as vuetify.

--- packages/playground/src/utils/custom_toast.ts
+++ packages/playground/src/utils/custom_toast.ts
@@ -40,8 +40,18 @@ export function createCustomToast(content: string, type: ToastType) {
   createToast(content, toastOptions);
 
   setTimeout(() => {
-    document.querySelector(".mosha__icon")?.children[0].classList.add(`mosha__icon__${theme}__${type}`);
-    document.querySelector(".mosha__toast__close-icon")?.classList.add(`${theme}__${type}`);
-    document.querySelector(".mosha__toast__content")?.classList.add(`${theme}__${type}`);
+    // Change the colors of the icons/contents of all opened toast to be `tonal` as vuetify.
+    const leftIcons = document.getElementsByClassName("mosha__icon");
+    const closIcons = document.getElementsByClassName("mosha__toast__close-icon");
+    const contents = document.getElementsByClassName("mosha__toast__content");
+
+    if (leftIcons && closIcons && contents) {
+      for (let index = 0; index < leftIcons.length; index++) {
+        const svgIcon = leftIcons[index].children[0];
+        svgIcon.classList.add(`mosha__icon__${theme}__${type}`);
+        closIcons[index].classList.add(`${theme}__${type}`);
+        contents[index].classList.add(`${theme}__${type}`);
+      }
+    }
   }, 1);
 }
