3def1e8eec6a436c14820b9a52ee3a009c1895ae
Thunder <mahmmoud.hassanein@gmail.com>
2023-10-24 14:34:48 +0300

    Development 313 offline nodes notifacation (#1239)
    * Created 2 methods 'all' and 'filter' on the nodes module to list and filter the nodes, and added one more field named numberOfPublicIPs returns the number of public ips registered on the deployment.
    
    * Added a notification helper that will notify the user every 2 hours if there are deployments on an offline node.
    
    * Fix unsetting the color for another childs, Change the colors of the icons/contents of all opened toast to be  as vuetify.
    
    * Update the contract types.
    
    * Apply commits and merge the development_313_gql_contracts_interface branch
    
    * Update the ds of saving the deployment nodeIds to be Set instead of Array to avoied the dublication.
    
    * Revert the changes.
    
    * Added the  option to responed with an array instead when call it.

--- packages/playground/src/utils/custom_toast.ts
+++ packages/playground/src/utils/custom_toast.ts
@@ -8,15 +8,26 @@ export enum ToastType {
   warning = "warning",
 }
 
-const colors = {
+const darkModeColors = {
   danger: "#FF5252",
   default: "#313131",
   info: "#2196F3",
   success: "#1AA18F",
-  warning: "#FFCC00",
+  warning: "#3C351D",
+};
+
+const lightModeColors = {
+  danger: "#FF5252",
+  default: "#313131",
+  info: "#2196F3",
+  success: "#1AA18F",
+  warning: "#FEF1E0",
 };
 
 export function createCustomToast(content: string, type: ToastType) {
+  const theme = localStorage.getItem("APP_CURRENT_THEME");
+  const colors = theme === "dark" ? darkModeColors : lightModeColors;
+
   const toastOptions: ToastOptions = {
     hideProgressBar: true,
     position: "top-right",
@@ -27,4 +38,20 @@ export function createCustomToast(content: string, type: ToastType) {
   };
 
   createToast(content, toastOptions);
+
+  setTimeout(() => {
+    // Change the colors of the icons/contents of all opened toast to be `tonal` as vuetify.
+    const leftIcons = document.getElementsByClassName("mosha__icon");
+    const closeIcons = document.getElementsByClassName("mosha__toast__close-icon");
+    const contents = document.getElementsByClassName("mosha__toast__content");
+
+    if (leftIcons && closeIcons && contents) {
+      for (let index = 0; index < leftIcons.length; index++) {
+        const svgIcon = leftIcons[index].children[0];
+        svgIcon.classList.add(`mosha__icon__${theme}__${type}`);
+        closeIcons[index].classList.add(`${theme}__${type}`);
+        contents[index].classList.add(`${theme}__${type}`);
+      }
+    }
+  }, 1);
 }
