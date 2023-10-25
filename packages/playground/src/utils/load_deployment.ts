34b0f290fa397887d57fe1b211e284e9cc386c92
AlaaElattar <alaamahmoud.1223@gmail.com>
2023-10-25 12:18:23 +0300

    add promise timeout when loading deployments

--- packages/playground/src/utils/load_deployment.ts
+++ packages/playground/src/utils/load_deployment.ts
@@ -21,14 +21,29 @@ export async function loadVms(grid: GridClient, options: LoadVMsOptions = {}) {
     machines.map(n => getGrid(grid.clientOptions, `${projectName}/${n}`)),
   )) as GridClient[];
 
-  const promises = machines.map((name, index) => {
-    return grids[index].machines.getObj(name).catch(e => {
+  const promises = machines.map(async (name, index) => {
+    const machinePromise = grids[index].machines.getObj(name);
+    const timeoutPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error("Timeout"));
+      }, 5000);
+    });
+
+    try {
+      const result = await Promise.race([machinePromise, timeoutPromise]);
+
+      if (result instanceof Error && result.message === "Timeout") {
+        console.log(`%c[Error] Timeout loading deployment with name ${name}`, "color: rgb(207, 102, 121)");
+        return null;
+      } else {
+        return result;
+      }
+    } catch (e) {
       console.log(
         `%c[Error] failed to load deployment with name ${name}:\n${normalizeError(e, "No errors were provided.")}`,
         "color: rgb(207, 102, 121)",
       );
-      return null;
-    });
+    }
   });
   const items = await Promise.all(promises);
   const vms = items
@@ -96,14 +111,28 @@ export async function loadK8s(grid: GridClient) {
     clusters.map(n => getGrid(grid.clientOptions, `${projectName}/${n}`)),
   )) as GridClient[];
 
-  const promises = clusters.map((name, index) => {
-    return grids[index].k8s.getObj(name).catch(e => {
+  const promises = clusters.map(async (name, index) => {
+    const clusterPromise = grids[index].k8s.getObj(name);
+    const timeoutPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error("Timeout"));
+      }, 5000);
+    });
+
+    try {
+      const result = await Promise.race([clusterPromise, timeoutPromise]);
+      if (result instanceof Error && result.message === "Timeout") {
+        console.log(`%c[Error] Timeout loading deployment with name ${name}`, "color: rgb(207, 102, 121)");
+        return null;
+      } else {
+        return result;
+      }
+    } catch (e) {
       console.log(
         `%c[Error] failed to load deployment with name ${name}:\n${normalizeError(e, "No errors were provided.")}`,
         "color: rgb(207, 102, 121)",
       );
-      return null;
-    });
+    }
   });
   const items = (await Promise.all(promises)) as any[];
   const k8s = items
