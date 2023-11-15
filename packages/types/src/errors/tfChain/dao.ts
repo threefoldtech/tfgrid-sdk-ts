c093449ff35fbb90807eebf144fc65e323f91007
kassem <omarksm09@gmail.com>
2023-11-15 11:22:42 +0200

    change types to erros

--- packages/types/src/errors/tfChain/dao.ts
+++ packages/types/src/errors/tfChain/dao.ts
@@ -1,6 +1,6 @@
 import { BaseError } from "../index";
 
-export enum Types {
+export enum Errors {
   NoneValue = 1,
   StorageOverflow,
   FarmNotExists,
@@ -22,102 +22,102 @@ export enum Types {
 
 export class NoneValue extends BaseError {
   constructor(message: string) {
-    super(Types.NoneValue, message);
+    super(Errors.NoneValue, message);
   }
 }
 
 export class StorageOverflow extends BaseError {
   constructor(message: string) {
-    super(Types.StorageOverflow, message);
+    super(Errors.StorageOverflow, message);
   }
 }
 
 export class FarmNotExists extends BaseError {
   constructor(message: string) {
-    super(Types.FarmNotExists, message);
+    super(Errors.FarmNotExists, message);
   }
 }
 
 export class NotCouncilMember extends BaseError {
   constructor(message: string) {
-    super(Types.NotCouncilMember, message);
+    super(Errors.NotCouncilMember, message);
   }
 }
 
 export class WrongProposalLength extends BaseError {
   constructor(message: string) {
-    super(Types.WrongProposalLength, message);
+    super(Errors.WrongProposalLength, message);
   }
 }
 
 export class DuplicateProposal extends BaseError {
   constructor(message: string) {
-    super(Types.DuplicateProposal, message);
+    super(Errors.DuplicateProposal, message);
   }
 }
 
 export class NotAuthorizedToVote extends BaseError {
   constructor(message: string) {
-    super(Types.NotAuthorizedToVote, message);
+    super(Errors.NotAuthorizedToVote, message);
   }
 }
 
 export class ProposalMissing extends BaseError {
   constructor(message: string) {
-    super(Types.ProposalMissing, message);
+    super(Errors.ProposalMissing, message);
   }
 }
 
 export class WrongIndex extends BaseError {
   constructor(message: string) {
-    super(Types.WrongIndex, message);
+    super(Errors.WrongIndex, message);
   }
 }
 
 export class DuplicateVote extends BaseError {
   constructor(message: string) {
-    super(Types.DuplicateVote, message);
+    super(Errors.DuplicateVote, message);
   }
 }
 
 export class DuplicateVeto extends BaseError {
   constructor(message: string) {
-    super(Types.DuplicateVeto, message);
+    super(Errors.DuplicateVeto, message);
   }
 }
 
 export class WrongProposalWeight extends BaseError {
   constructor(message: string) {
-    super(Types.WrongProposalWeight, message);
+    super(Errors.WrongProposalWeight, message);
   }
 }
 
 export class TooEarly extends BaseError {
   constructor(message: string) {
-    super(Types.TooEarly, message);
+    super(Errors.TooEarly, message);
   }
 }
 
 export class TimeLimitReached extends BaseError {
   constructor(message: string) {
-    super(Types.TimeLimitReached, message);
+    super(Errors.TimeLimitReached, message);
   }
 }
 
 export class OngoingVoteAndTresholdStillNotMet extends BaseError {
   constructor(message: string) {
-    super(Types.OngoingVoteAndTresholdStillNotMet, message);
+    super(Errors.OngoingVoteAndTresholdStillNotMet, message);
   }
 }
 
 export class FarmHasNoNodes extends BaseError {
   constructor(message: string) {
-    super(Types.FarmHasNoNodes, message);
+    super(Errors.FarmHasNoNodes, message);
   }
 }
 
 export class InvalidProposalDuration extends BaseError {
   constructor(message: string) {
-    super(Types.InvalidProposalDuration, message);
+    super(Errors.InvalidProposalDuration, message);
   }
 }
