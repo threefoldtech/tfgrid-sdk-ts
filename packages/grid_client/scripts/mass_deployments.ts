2764b66c8743b2d6fffdf9c16a15bbf069c76d95
AlaaElattar <alaamahmoud.1223@gmail.com>
2023-11-15 12:04:41 +0200

    add batches to mass deployment

--- packages/grid_client/scripts/mass_deployments.ts
+++ packages/grid_client/scripts/mass_deployments.ts
@@ -17,92 +17,111 @@ async function main() {
   const errors: any = [];
   let failedCount = 0;
   let successCount = 0;
+  const batchSize = 100;
+  const totalVMs = 1000;
+  const batches = totalVMs / batchSize;
 
-  for (let i = 0; i < 100; i++) {
-    //Generating the resources
-    const cru = 1;
-    const mru = 256;
-    const diskSize = 5;
-    const rootFs = 1;
-    const vmName = "vm" + generateString(8);
-    const deploymentName = "dep" + generateString(8);
-    const publicIp = false;
+  console.time("Total Deployment Time");
 
-    // create network Object
+  for (let i = 0; i < batches; i++) {
+    console.time("Batch " + (i + 1));
+
+    const batchVMs: MachineModel[] = [];
+
+    for (let i = 0; i < batchSize; i++) {
+      const cru = 1;
+      const mru = 256;
+      const diskSize = 5;
+      const rootFs = 1;
+      const vmName = "vm" + generateString(8);
+      const deploymentName = "dep" + generateString(8);
+      const publicIp = false;
+
+      const n = new NetworkModel();
+      n.name = "nw" + generateString(5);
+      n.ip_range = "10.238.0.0/16";
+
+      // create disk Object
+      const disk1 = new DiskModel();
+      disk1.name = "d" + generateString(5);
+      disk1.size = diskSize;
+      disk1.mountpoint = "/newDisk1";
+
+      //Farm Selection
+      const farms = await grid3.capacity.filterFarms({
+        nodeMRU: mru / 1024,
+        nodeSRU: diskSize + rootFs,
+        publicIp: publicIp,
+        availableFor: await grid3.twins.get_my_twin_id(),
+        randomize: false,
+      } as FarmFilterOptions);
+
+      if (farms.length < 1) {
+        throw new Error("No farms found");
+      }
+      //Node Selection
+      const nodes = await grid3.capacity.filterNodes({
+        cru: cru,
+        mru: mru / 1024,
+        sru: rootFs + diskSize,
+        availableFor: await grid3.twins.get_my_twin_id(),
+        farmId: +randomChoice(farms).farmId,
+        randomize: false,
+      } as FilterOptions);
+
+      if (nodes.length < 1) {
+        errors.push("Node not found");
+        failedCount++;
+        continue;
+      }
+      const id = +randomChoice(nodes).nodeId;
+
+      // create vm node Object
+      const vm = new MachineModel();
+      vm.name = vmName;
+      vm.node_id = id;
+      vm.disks = [disk1];
+      vm.public_ip = publicIp;
+      vm.planetary = true;
+      vm.cpu = cru;
+      vm.memory = mru;
+      vm.rootfs_size = rootFs;
+      vm.flist = "https://hub.grid.tf/tf-official-apps/base:latest.flist";
+      vm.entrypoint = "/sbin/zinit init";
+      vm.env = {
+        SSH_KEY: config.ssh_key,
+      };
+
+      batchVMs.push(vm);
+    }
+    // create network model for whole batch
     const n = new NetworkModel();
     n.name = "nw" + generateString(5);
     n.ip_range = "10.238.0.0/16";
 
-    // create disk Object
-    const disk1 = new DiskModel();
-    disk1.name = "d" + generateString(5);
-    disk1.size = diskSize;
-    disk1.mountpoint = "/newDisk1";
-
-    //Farm Selection
-    const farms = await grid3.capacity.filterFarms({
-      nodeMRU: mru / 1024,
-      nodeSRU: diskSize + rootFs,
-      publicIp: publicIp,
-      availableFor: await grid3.twins.get_my_twin_id(),
-      randomize: true,
-    } as FarmFilterOptions);
-
-    if (farms.length < 1) {
-      throw new Error("No farms found");
-    }
-
-    //Node Selection
-    const nodes = await grid3.capacity.filterNodes({
-      cru: cru,
-      mru: mru / 1024,
-      sru: rootFs + diskSize,
-      availableFor: await grid3.twins.get_my_twin_id(),
-      farmId: +randomChoice(farms).farmId,
-      randomize: true,
-    } as FilterOptions);
-
-    if (nodes.length < 1) {
-      errors.push("Node not found");
-      failedCount++;
-      continue;
-    }
-
-    // create vm node Object
-    const vm = new MachineModel();
-    vm.name = vmName;
-    vm.node_id = nodes[0].nodeId;
-    vm.disks = [disk1];
-    vm.public_ip = publicIp;
-    vm.planetary = true;
-    vm.cpu = cru;
-    vm.memory = mru;
-    vm.rootfs_size = rootFs;
-    vm.flist = "https://hub.grid.tf/tf-official-apps/base:latest.flist";
-    vm.entrypoint = "/sbin/zinit init";
-    vm.env = {
-      SSH_KEY: config.ssh_key,
-    };
-
     // create VMs Object
     const vms = new MachinesModel();
-    vms.name = deploymentName;
+    vms.name = "batch" + (i + 1);
     vms.network = n;
-    vms.machines = [vm];
+    vms.machines = batchVMs;
     vms.metadata = "";
-    vms.description = "test deploying VMs via ts grid3 client";
+    vms.description = "Test deploying VMs via ts grid3 client - Batch " + (i + 1);
 
     // deploy vm
     try {
       await grid3.machines.deploy(vms);
-      successCount++;
+      successCount += batchSize;
     } catch (error) {
       log(error);
       errors.push(error);
-      failedCount++;
+      failedCount += batchSize;
       continue;
+    } finally {
+      console.timeEnd("Batch " + (i + 1));
     }
   }
+
+  console.timeEnd("Total Deployment Time");
 
   log("Successful Deployments: " + successCount);
   log("Failed Deployments: " + failedCount);
