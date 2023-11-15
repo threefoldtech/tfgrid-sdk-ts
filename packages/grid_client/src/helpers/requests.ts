820024ff0e605650711c7f5b57b37acb45227453
kassem <omarksm09@gmail.com>
2023-11-15 13:40:37 +0200

    use sendFullResponse inside send

--- packages/grid_client/src/helpers/requests.ts
+++ packages/grid_client/src/helpers/requests.ts
@@ -1,18 +1,5 @@
 import { default as axios, Method } from "axios";
 
-async function send(method: Method, url: string, body: string, headers: Record<string, string>) {
-  const options = {
-    method: method,
-    url: url,
-    data: body,
-    headers: headers,
-  };
-  const response = await axios(options);
-  if (response.status >= 400) {
-    throw Error(`HTTP request failed with status code: ${response.status} due to: ${response.data}`);
-  }
-  return response.data;
-}
 async function sendWithFullResponse(method: Method, url: string, body: string, headers: Record<string, string>) {
   const options = {
     method: method,
@@ -26,4 +13,10 @@ async function sendWithFullResponse(method: Method, url: string, body: string, h
   }
   return response;
 }
+
+async function send(method: Method, url: string, body: string, headers: Record<string, string>) {
+  const response = await sendWithFullResponse(method, url, body, headers);
+  return response.data;
+}
+
 export { send, sendWithFullResponse };
