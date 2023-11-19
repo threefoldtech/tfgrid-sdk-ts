cf911b77f6787bebaa8c0bbc3f841fc2d264325b
kassem <omarksm09@gmail.com>
2023-11-19 12:20:49 +0200

    rename tfChain

--- packages/types/src/errors/index.ts
+++ packages/types/src/errors/index.ts
@@ -1,4 +1,4 @@
-export { ValidationError, TimeoutError, BaseError } from "./base_error";
-export { TFChainError } from "./tfChain/tfchain_error";
+export * from "./base_error";
+export { TFChainError } from "./tfchain/tfchain_error";
 export { ErrorModules } from "./modules";
-export * as TFChainErrors from "./tfChain/index";
+export * as TFChainErrors from "./tfchain/index";
