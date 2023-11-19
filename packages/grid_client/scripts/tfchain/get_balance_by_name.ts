a0bb62551b5081f18ffef6fb428fed6737ac2dec
mohamedamer453 <amerm@incubaid.com>
2023-11-19 11:57:46 +0200

    update tfchain scripts

--- packages/grid_client/scripts/tfchain/get_balance_by_name.ts
+++ packages/grid_client/scripts/tfchain/get_balance_by_name.ts
@@ -1,9 +1,27 @@
+import { BlockchainGetModel } from "../../src";
 import { getClient } from "../client_loader";
+import { log } from "../utils";
+
+async function getBalanceByName(client, account) {
+  try {
+    const res = await client.tfchain.assets(account);
+    log("================= Getting balance =================");
+    log(res);
+    log("================= Getting balance =================");
+  } catch (error) {
+    log("Error while getting balance" + error);
+  }
+}
 
 async function main() {
   const grid3 = await getClient();
-  const balance = await grid3.tfchain.assets({ name: "mariobassem" });
-  console.log(balance);
+  const account: BlockchainGetModel = {
+    name: "newacc",
+  };
+
+  //Get balance by account name
+  await getBalanceByName(grid3, account);
+
   await grid3.disconnect();
 }
 
