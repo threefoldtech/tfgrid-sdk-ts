43d86389798cc0a48b36dcec5f2fba715fd948a8
kassem <omarksm09@gmail.com>
2023-11-20 12:55:43 +0200

    Use validation error class instead of panic will leave painc in case it needed anywhere else

--- packages/graphql_client/src/utils/index.ts
+++ packages/graphql_client/src/utils/index.ts
@@ -1,3 +1,4 @@
+import { ValidationError } from "@threefold/types";
 type AssertReturn = void | never;
 function panic(message: string): never {
   throw new Error(message);
@@ -8,15 +9,16 @@ function panic(message: string): never {
  * Assertion
  */
 export function assertHasField(fields: any): AssertReturn {
-  if (Object.keys(fields).length === 0) panic(`[Object] must contain at least 1 field.`);
+  if (Object.keys(fields).length === 0) throw new ValidationError(`[Object] must contain at least 1 field.`);
   for (const key in fields) {
     if (fields[key] instanceof Object) assertHasField(fields[key]);
-    else if (fields[key] !== true) panic(`Expected "${key}" of type [true | string] but got [${typeof fields[key]}]`);
+    else if (fields[key] !== true)
+      throw new ValidationError(`Expected "${key}" of type [true | string] but got [${typeof fields[key]}]`);
   }
 }
 
 export function assertID(value: any): AssertReturn {
   if (value === "" || value === undefined || value === null) {
-    panic(`Expected a valid [ID] but got [${value}].`);
+    throw new ValidationError(`Expected a valid [ID] but got [${value}].`);
   }
 }
