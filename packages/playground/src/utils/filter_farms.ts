16108ca377eed71323d4c2b66e345d7a96131f67
amiraabouhadid <amiraabouhadid@gmail.com>
2023-11-20 13:27:58 +0200

    add certification filter

--- packages/playground/src/utils/filter_farms.ts
+++ packages/playground/src/utils/filter_farms.ts
@@ -1,9 +1,10 @@
 import type { InputFilterType } from "../types";
-import { isAlphanumeric, isInt, isNumeric } from "./validators";
+import { isAlphanumeric, isInt, isNumeric, isString } from "./validators";
 
 export type FilterFarmInputs = {
   farmId: InputFilterType;
   name: InputFilterType;
+  certification: InputFilterType;
 };
 export const inputsInitializer: FilterFarmInputs = {
   farmId: {
@@ -16,6 +17,12 @@ export const inputsInitializer: FilterFarmInputs = {
     label: "Farm Name",
     placeholder: "e.g myfarm",
     rules: [[isAlphanumeric("Farm name should be made of either numbers or letters")]],
+    type: "text",
+  },
+  certification: {
+    label: "Certification Type",
+    placeholder: "e.g certified",
+    rules: [[isString("Certification type should be written in only letters")]],
     type: "text",
   },
 };
