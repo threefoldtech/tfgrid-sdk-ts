b8f616b05218995c61beac6f4b80cca77fd00c4f
kassem <omarksm09@gmail.com>
2023-11-26 15:02:56 +0200

    sort errors in errors module

--- packages/types/src/errors/modules.ts
+++ packages/types/src/errors/modules.ts
@@ -1,15 +1,12 @@
 export enum ErrorModules {
   Generic = "Generic",
-  //tfchain modules
+  Farm = "tfGridFarm",
+  Node = "tfGridNode",
+  Workloads = "tfGridWorkloads",
   Dao = "dao",
   TFKVStore = "tfkvStore",
   SmartContract = "smartContractModule",
   TFGrid = "tfgridModule",
   TFTBridge = "tfgridModule",
   TFTPrice = "tftPriceModule",
-  //grid client modules
-  Farm = "tfGridFarm",
-  Network = "tfGridNetwork",
-  Node = "tfGridNode",
-  Workloads = "tfGridWorkloads",
 }
