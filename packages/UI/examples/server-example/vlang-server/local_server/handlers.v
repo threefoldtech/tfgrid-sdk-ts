cbe14bdd01b1892f4f3946e4daa155253bf9db97
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2023-11-30 13:10:03 +0200

    Update the v-server, enable cors

--- packages/UI/examples/server-example/vlang-server/local_server/handlers.v
+++ packages/UI/examples/server-example/vlang-server/local_server/handlers.v
@@ -1,29 +1,40 @@
 module local_server
-
 import vweb
 import time
 import x.json2
 
 
-pub fn (app &VServerApp) before_request() {
+pub fn (mut app VServerApp) before_request() {
 	eprintln('${time.now()}: ${app.req.method} ${app.req.url}')
 }
 
-@['/api/verify'; post]
+@[options;'/:path...']
+pub fn (mut app VServerApp) preflight(path string) vweb.Result {
+	app.add_header('Vary', 'Access-Control-Request-Headers')
+	app.add_header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+	app.add_header('Access-Control-Allow-Headers', 'Content-Type') // Add this line
+	app.add_header('Access-Control-Allow-Origin', '*')
+
+	return app.text('options are ok')
+}
+
+@['/api/verify'; POST]
 pub fn (mut app VServerApp) verify() vweb.Result {
+
+	app.add_header('Access-Control-Allow-Origin', '*')
 	data := app.req.data
-	payload := json2.decode[Payload](data) or { 
+	json2.decode[Payload](data) or { 
 		app.set_status(400, "")
 		return app.json({
 			"Error": "true",
-			"message": "Please make sure that you sent a valid date."
-			"required_fields": Payload{}.str()
+			"message": "Please make sure that you sent a valid date.",
+			"required_fields": Payload{}.str(),
 			"status": '400'
 		})
 	}
 
-	println(payload)
 	return app.json({
-		"data": "id"
+		"data": data
 	})
+		
 }
