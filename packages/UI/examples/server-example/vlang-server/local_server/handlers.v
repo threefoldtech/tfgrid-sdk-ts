ae6e41999d0a5563a8a6860ad57e11ce2aecf183
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2023-12-03 12:37:28 +0200

    Removed unused comment.

--- packages/UI/examples/server-example/vlang-server/local_server/handlers.v
+++ packages/UI/examples/server-example/vlang-server/local_server/handlers.v
@@ -11,7 +11,7 @@ pub fn (mut app VServerApp) before_request() {
 pub fn (mut app VServerApp) preflight(path string) vweb.Result {
 	app.add_header('Vary', 'Access-Control-Request-Headers')
 	app.add_header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-	app.add_header('Access-Control-Allow-Headers', 'Content-Type') // Add this line
+	app.add_header('Access-Control-Allow-Headers', 'Content-Type')
 	app.add_header('Access-Control-Allow-Origin', '*')
 
 	return app.text('options are ok')
