1bfc96d8edf302746e571f3616906e39264ce35f
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2023-12-03 11:53:05 +0200

    Removed the validation and printed the payload.

--- packages/UI/examples/server-example/vlang-server/local_server/handlers.v
+++ packages/UI/examples/server-example/vlang-server/local_server/handlers.v
@@ -1,7 +1,6 @@
 module local_server
 import vweb
 import time
-import x.json2
 
 
 pub fn (mut app VServerApp) before_request() {
@@ -22,17 +21,9 @@ pub fn (mut app VServerApp) preflight(path string) vweb.Result {
 pub fn (mut app VServerApp) verify() vweb.Result {
 
 	app.add_header('Access-Control-Allow-Origin', '*')
+	app.set_status(200, "")
 	data := app.req.data
-	json2.decode[Payload](data) or { 
-		app.set_status(400, "")
-		return app.json({
-			"Error": "true",
-			"message": "Please make sure that you sent a valid date.",
-			"required_fields": Payload{}.str(),
-			"status": '400'
-		})
-	}
-
+	println("Payload: ${data}")
 	return app.json({
 		"data": data
 	})
