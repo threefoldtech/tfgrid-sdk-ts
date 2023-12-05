bf3f8c7ee76081735b86b0574da04bb1c3989e11
kassem <omarksm09@gmail.com>
2023-12-05 11:01:20 +0200

    fix reload issue and uptime calculation

--- packages/playground/src/dashboard/components/user_nodes.vue
+++ packages/playground/src/dashboard/components/user_nodes.vue
@@ -126,12 +126,7 @@ import { useProfileManager } from "@/stores";
 import type { NodeDetailsCard } from "@/types";
 import { createCustomToast, ToastType } from "@/utils/custom_toast";
 import { getNodeStatusColor } from "@/utils/get_nodes";
-import {
-  getNodeAvailability,
-  getNodeMintingFixupReceipts,
-  getNodeUptimePercentage,
-  type NodeInterface,
-} from "@/utils/node";
+import { calculateUptime, getNodeAvailability, getNodeMintingFixupReceipts, type NodeInterface } from "@/utils/node";
 
 import NodeMintingDetails from "./NodeMintingDetails.vue";
 import PublicConfig from "./public_config.vue";
@@ -197,17 +192,7 @@ export default {
     const uptime = ref();
     const nodesCount = ref();
 
-    onMounted(async () => {
-      const nodes = await getUserNodes();
-      if (nodes) {
-        await Promise.all(
-          nodes.map(async (n, _) => {
-            n.uptime = +(await getNodeUptimePercentage(n.nodeId));
-            return n.uptime;
-          }),
-        );
-      }
-    });
+    onMounted(async () => await getUserNodes());
 
     const reloadNodes = setTimeout(async () => {
       await getUserNodes();
@@ -238,6 +223,7 @@ export default {
             node.receipts = [];
             if (network == "main") node.receipts = await getNodeMintingFixupReceipts(node.nodeId);
             node.availability = await getNodeAvailability(node.nodeId);
+            node.uptime = +calculateUptime(node.availability.currentPeriod, node.availability.downtime);
           } catch (error) {
             node.receipts = [];
             node.used_resources = {
@@ -247,6 +233,7 @@ export default {
               cru: 0,
             };
             node.availability = { downtime: 0, currentPeriod: 0 };
+            node.uptime = 0;
           }
 
           return node;
