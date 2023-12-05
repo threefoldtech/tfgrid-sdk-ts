84f99cd88cb8f353eaf10a7e08f596cc3cc118c7
amiraabouhadid <amiraabouhadid@gmail.com>
2023-12-05 13:06:46 +0200

    revert filter expansion

--- packages/playground/src/components/filter.vue
+++ packages/playground/src/components/filter.vue
@@ -9,7 +9,6 @@
           }
         }
       "
-      v-model="panel"
     >
       <v-expansion-panel>
         <v-expansion-panel-title>
@@ -82,7 +81,7 @@ export default defineComponent({
   setup(props, { emit }) {
     const formRef = useFormRef();
     const isFiltersTouched = ref<boolean>(false);
-    const panel = ref(0);
+
     watch(
       () => props.modelValue,
       (newValue: PropType<{ [key: string]: InputFilterType }>) => {
@@ -109,7 +108,6 @@ export default defineComponent({
       resetFilters,
       isFiltersTouched,
       formRef,
-      panel,
     };
   },
 });
