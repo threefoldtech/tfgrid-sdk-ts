6f971807f9ce341f512d9639ff1746b67ee01199
zaelgohary <zainabelgohary19@gmail.com>
2023-12-18 12:53:21 +0200

    Rearrage validations

--- packages/playground/src/dashboard/transfer_view.vue
+++ packages/playground/src/dashboard/transfer_view.vue
@@ -38,8 +38,8 @@
                   validators.required('Transfer amount is required '),
                   validators.isNumeric('Amount should be a number.'),
                   validators.min('Amount must be greater than 0.', 0.001),
-                  validators.max('Insufficient funds.', freeBalance),
                   validators.isDecimal('Amount can have 3 decimals only.', { decimal_digits: '0,3' }),
+                  validators.max('Insufficient funds.', freeBalance),
                 ]"
                 #="{ props }"
               >
@@ -89,8 +89,8 @@
                   validators.required('Transfer amount is required '),
                   validators.isNumeric('Amount should be a number.'),
                   validators.min('Amount must be greater than 0.', 0.001),
-                  validators.max('Insufficient funds.', freeBalance),
                   validators.isDecimal('Amount can have 3 decimals only.', { decimal_digits: '0,3' }),
+                  validators.max('Insufficient funds.', freeBalance),
                 ]"
                 #="{ props }"
               >
