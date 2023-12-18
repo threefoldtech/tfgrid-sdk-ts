8b883bff2d6a7bf4863e8db18c2d38745fa07c94
zaelgohary <zainabelgohary19@gmail.com>
2023-12-18 11:09:31 +0200

    Replace capacity URL w stats route

--- packages/playground/src/utils/getCapacityUrl.ts
+++
@@ -1,8 +0,0 @@
-import { NetworkEnv } from "@threefold/grid_client";
-
-export function getCapacityURL(network: NetworkEnv) {
-  if (network === NetworkEnv.main) {
-    return "https://newstats.grid.tf/";
-  }
-  return `https://newstats.${network}.grid.tf/`;
-}
