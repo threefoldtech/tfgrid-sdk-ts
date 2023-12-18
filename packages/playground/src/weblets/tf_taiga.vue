60a027e4bf636a6c11819417b8902a149852ae3d
MohamedElmdary <engm5081@gmail.com>
2023-12-18 15:40:56 +0200

    feat: integrate selection details in tf_taiga

--- packages/playground/src/weblets/tf_taiga.vue
+++ packages/playground/src/weblets/tf_taiga.vue
@@ -5,9 +5,8 @@
     :memory="solution?.memory"
     :disk="solution?.disk + rootFilesystemSize"
     :ipv4="ipv4"
-    :certified="certified"
     :dedicated="dedicated"
-    :SelectedNode="selectedNode"
+    :SelectedNode="selectionDetails?.node"
     title-image="images/icons/taiga.png"
   >
     <template #title>Deploy a Taiga Instance</template>
@@ -89,70 +88,33 @@
           v-model="solution"
           :small="{ cpu: 2, memory: 4, disk: 100 }"
           :medium="{ cpu: 4, memory: 8, disk: 150 }"
-          :disabled="loadingFarm"
         />
-        <!-- <Networks v-model:ipv4="ipv4" /> -->
-        <FarmGatewayManager>
-          <input-tooltip
-            inline
-            tooltip="Click to know more about dedicated nodes."
-            href="https://manual.grid.tf/dashboard/portal/dashboard_portal_dedicated_nodes.html"
-          >
-            <v-switch
-              color="primary"
-              inset
-              label="Dedicated"
-              v-model="dedicated"
-              :disabled="loadingFarm"
-              hide-details
-            />
-          </input-tooltip>
-          <input-tooltip inline tooltip="Renting capacity on certified nodes is charged 25% extra.">
-            <v-switch
-              color="primary"
-              inset
-              label="Certified"
-              v-model="certified"
-              :disabled="loadingFarm"
-              hide-details
-            />
-          </input-tooltip>
-          <SelectFarmManager>
-            <NodeSelector v-model="selection" />
-            <SelectFarm
-              v-if="selection == Selection.AUTOMATED"
-              :filters="{
-                cpu: solution?.cpu,
-                memory: solution?.memory,
-                ssd: (solution?.disk ?? 0) + rootFilesystemSize,
-                publicIp: ipv4,
-                rentedBy: dedicated ? profileManager.profile?.twinId : undefined,
-                certified: certified,
-              }"
-              v-model="farm"
-              v-model:loading="loadingFarm"
-              v-model:search="farmName"
-            />
 
-            <SelectNode
-              v-model="selectedNode"
-              :selection="selection"
-              :filters="{
-                farmId: farm?.farmID,
-                cpu: solution?.cpu,
-                memory: solution?.memory,
-                diskSizes: [solution?.disk],
-                rentedBy: dedicated ? profileManager.profile?.twinId : undefined,
-                certified: certified,
-                country: farm?.country,
-                region: farm?.region,
-              }"
-              :loading-farm="loadingFarm"
-              :root-file-system-size="rootFilesystemSize"
-            />
-          </SelectFarmManager>
-          <DomainName :hasIPv4="ipv4" ref="domainNameCmp" />
-        </FarmGatewayManager>
+        <input-tooltip
+          inline
+          tooltip="Click to know more about dedicated nodes."
+          href="https://manual.grid.tf/dashboard/portal/dashboard_portal_dedicated_nodes.html"
+        >
+          <v-switch color="primary" inset label="Dedicated" v-model="dedicated" hide-details />
+        </input-tooltip>
+
+        <input-tooltip inline tooltip="Renting capacity on certified nodes is charged 25% extra.">
+          <v-switch color="primary" inset label="Certified" v-model="certified" hide-details />
+        </input-tooltip>
+
+        <TfSelectionDetails
+          :filters="{
+            ipv4,
+            certified,
+            dedicated,
+            cpu: solution?.cpu,
+            solutionDisk: solution?.disk,
+            memory: solution?.memory,
+            rootFilesystemSize,
+          }"
+          require-domain
+          v-model="selectionDetails"
+        />
       </template>
 
       <template #smtp>
@@ -163,36 +125,25 @@
     </d-tabs>
 
     <template #footer-actions>
-      <v-btn
-        color="primary"
-        variant="tonal"
-        @click="deploy(domainNameCmp?.domain, domainNameCmp?.customDomain)"
-        :disabled="tabs?.invalid"
-      >
-        Deploy
-      </v-btn>
+      <v-btn color="primary" variant="tonal" @click="deploy()" :disabled="tabs?.invalid"> Deploy </v-btn>
     </template>
   </weblet-layout>
 </template>
 
 <script lang="ts" setup>
 import type { GridClient } from "@threefold/grid_client";
-import { computed, type Ref, ref, watch } from "vue";
+import { computed, type Ref, ref } from "vue";
 
-import { Selection } from "@/utils/types";
-
-import NodeSelector from "../components/node_selection.vue";
 import { useLayout } from "../components/weblet_layout.vue";
 import { useProfileManager } from "../stores";
-import type { FarmInterface, Flist, GatewayNode, solutionFlavor as SolutionFlavor } from "../types";
+import type { Flist, solutionFlavor as SolutionFlavor } from "../types";
 import { ProjectName } from "../types";
 import { deployVM } from "../utils/deploy_vm";
-import { deployGatewayName, getSubdomain, rollbackDeployment } from "../utils/gateway";
+import { deployGatewayName2, getSubdomain, rollbackDeployment } from "../utils/gateway";
 import { getGrid } from "../utils/grid";
 import { generateName, generatePassword } from "../utils/strings";
 
 const layout = useLayout();
-const selection = ref();
 const tabs = ref();
 const profileManager = useProfileManager();
 
@@ -201,30 +152,17 @@ const username = ref("admin");
 const password = ref(generatePassword());
 const email = ref("");
 const solution = ref() as Ref<SolutionFlavor>;
-const farm = ref() as Ref<FarmInterface>;
-const farmName = ref();
-const loadingFarm = ref(false);
 const flist: Flist = {
   value: "https://hub.grid.tf/tf-official-apps/grid3_taiga_docker-latest.flist",
   entryPoint: "/sbin/zinit init",
 };
 const dedicated = ref(false);
 const certified = ref(false);
-const selectedNode = ref() as Ref<INode>;
 const ipv4 = ref(false);
-const domainNameCmp = ref();
 
 const smtp = ref(createSMTPServer());
 const rootFilesystemSize = computed(() => rootFs(solution.value?.cpu ?? 0, solution.value?.memory ?? 0));
-watch(
-  () => selection.value,
-  (value, oldValue) => {
-    if (value !== oldValue) {
-      loadingFarm.value = false;
-    }
-  },
-  { deep: false },
-);
+const selectionDetails = ref<SelectionDetails>();
 
 function finalize(deployment: any) {
   layout.value.reloadDeploymentsList();
@@ -232,7 +170,7 @@ function finalize(deployment: any) {
   layout.value.openDialog(deployment, deploymentListEnvironments.taiga);
 }
 
-async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
+async function deploy() {
   layout.value.setStatus("deploy");
 
   const projectName = ProjectName.Taiga.toLowerCase() + "/" + name.value;
@@ -242,7 +180,10 @@ async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
     projectName,
     twinId: profileManager.profile!.twinId,
   });
-  const domain = customDomain ? gatewayName.domain : subdomain + "." + gatewayName.domain;
+
+  const domain = selectionDetails.value?.domain?.enabledCustomDomain
+    ? selectionDetails.value.domain.customDomain
+    : subdomain + "." + selectionDetails.value?.domain?.selectedDomain?.publicConfig.domain;
 
   let grid: GridClient | null;
   let vm: any;
@@ -256,8 +197,8 @@ async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
     vm = await deployVM(grid!, {
       name: name.value,
       network: {
-        accessNodeId: gatewayName.id,
-        addAccess: !!gatewayName,
+        addAccess: selectionDetails.value!.domain!.enableSelectedDomain,
+        accessNodeId: selectionDetails.value?.domain?.selectedDomain?.nodeId,
       },
       machines: [
         {
@@ -273,10 +214,7 @@ async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
           flist: flist.value,
           entryPoint: flist.entryPoint,
           rootFilesystemSize: rootFilesystemSize.value,
-          farmId: farm.value.farmID,
-          farmName: farm.value.name,
           publicIpv4: ipv4.value,
-          country: farm.value.country,
           planetary: true,
           envs: [
             { key: "SSH_KEY", value: profileManager.profile!.ssh },
@@ -295,7 +233,7 @@ async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
                 ]
               : []),
           ],
-          nodeId: selectedNode.value.nodeId,
+          nodeId: selectionDetails.value!.node!.nodeId,
           rentedBy: dedicated.value ? grid!.twinId : undefined,
           certified: certified.value,
         },
@@ -304,21 +242,21 @@ async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
   } catch (e) {
     return layout.value.setStatus("failed", normalizeError(e, "Failed to deploy a taiga instance."));
   }
-  if (customDomain && ipv4.value) {
-    vm[0].customDomain = gatewayName.domain;
+
+  if (!selectionDetails.value?.domain?.enableSelectedDomain) {
+    vm[0].customDomain = selectionDetails.value?.domain?.customDomain;
     finalize(vm);
     return;
   }
 
   try {
     layout.value.setStatus("deploy", "Preparing to deploy gateway...");
-    await deployGatewayName(grid!, {
-      name: subdomain,
-      nodeId: gatewayName.id!,
+
+    await deployGatewayName2(grid, selectionDetails.value.domain, {
+      subdomain,
       ip: vm[0].interfaces[0].ip,
-      port: 9000,
-      networkName: vm[0].interfaces[0].network,
-      fqdn: gatewayName?.useFQDN ? gatewayName.domain : undefined,
+      port: 80,
+      network: vm[0].interfaces[0].network,
     });
 
     finalize(vm);
@@ -331,30 +269,15 @@ async function deploy(gatewayName: GatewayNode, customDomain: boolean) {
 </script>
 
 <script lang="ts">
-import DomainName from "../components/domain_name.vue";
-import FarmGatewayManager from "../components/farm_gateway_manager.vue";
-// import Networks from "../components/networks.vue";
-import SelectFarm from "../components/select_farm.vue";
-import SelectFarmManager from "../components/select_farm_manager.vue";
-import SelectNode from "../components/select_node.vue";
 import SelectSolutionFlavor from "../components/select_solution_flavor.vue";
 import SmtpServer, { createSMTPServer } from "../components/smtp_server.vue";
 import { deploymentListEnvironments } from "../constants";
-import type { INode } from "../utils/filter_nodes";
+import type { SelectionDetails } from "../types/nodeSelector";
 import { normalizeError } from "../utils/helpers";
 import rootFs from "../utils/root_fs";
 
 export default {
   name: "TfTaiga",
-  components: {
-    SmtpServer,
-    SelectSolutionFlavor,
-    // Networks,
-    DomainName,
-    FarmGatewayManager,
-    SelectFarm,
-    SelectNode,
-    SelectFarmManager,
-  },
+  components: { SmtpServer, SelectSolutionFlavor },
 };
 </script>
