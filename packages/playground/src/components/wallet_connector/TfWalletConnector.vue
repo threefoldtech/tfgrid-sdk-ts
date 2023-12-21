d3445e187748f0e3cd353e42269fceedeb3550ee
MohamedElmdary <engm5081@gmail.com>
2023-12-21 02:17:25 +0200

    feat: add style of logged in wallet

--- packages/playground/src/components/wallet_connector/TfWalletConnector.vue
+++ packages/playground/src/components/wallet_connector/TfWalletConnector.vue
@@ -3,9 +3,32 @@
     <template #activator="{ props }">
       <VBtn variant="flat" v-bind="props">
         <template #prepend>
-          <VIcon icon="mdi-check" />
+          <VIcon icon="mdi-account" size="x-large" />
         </template>
-        <span>Connect your TFChain Wallet</span>
+        <span class="font-weight-bold">Connect your TFChain Wallet</span>
+
+        <!-- Login -->
+        <!-- <div class="text-left">
+          <p class="mb-1">Balance: <span class="font-weight-bold text-secondary" v-text="'26649985954.701 TFT'" /></p>
+          <p class="d-flex align-center">
+            Locked: <span class="font-weight-bold text-secondary ml-2" v-text="'119.342 TFT'" />
+            <VBtn
+              :style="{ height: '24px', width: '24px' }"
+              icon="mdi-information-outline"
+              href="https://manual.grid.tf/tfchain/tfchain.html?highlight=locked#contract-locking"
+              target="_blank"
+              size="xs"
+              color="none"
+              class="ml-2"
+              @click.stop
+            />
+          </p>
+        </div> -->
+
+        <!-- Login -->
+        <!-- <template #append>
+          <VBtn :style="{ height: '48px' }" icon="mdi-logout" color="error" variant="tonal" @click.stop />
+        </template> -->
       </VBtn>
     </template>
 
