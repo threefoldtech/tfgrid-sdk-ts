5718930534422199abad98a149ccfb1aa5a0fa38
kassem <omarksm09@gmail.com>
2024-01-18 12:16:05 +0200

    fix: hide the low balance alert if the balance is still loading

--- packages/playground/src/views/sshkey_view.vue
+++ packages/playground/src/views/sshkey_view.vue
@@ -3,7 +3,7 @@
     <v-alert v-if="hasEnoughBalance" type="info" variant="tonal" class="ma-4">
       Updating or generating SSH key will cost you up to 0.01 TFT
     </v-alert>
-    <v-alert v-else type="warning" variant="tonal" class="ma-4">
+    <v-alert v-else-if="balance" type="warning" variant="tonal" class="ma-4">
       Your balance is not enough to {{ profileManager.profile?.ssh ? "update the" : `generate a` }} SSH key; please make
       sure you have at least 0.01 TFT.
     </v-alert>
