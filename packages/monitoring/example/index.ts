e5326a75a868cd62d02c8ff987591ce578c1783d
kassem <omarksm09@gmail.com>
2024-01-22 16:16:16 +0200

    update example

--- packages/monitoring/example/index.ts
+++ packages/monitoring/example/index.ts
@@ -1,19 +1,17 @@
-import { TFservicesLiveChecker } from "../src/index";
-import { TFServices } from "../src/types";
+import { checkServiceAliveness, GridProxyMonitor, monitorServiceAliveness } from "../src/";
+import { RMBMonitor, TFChainMonitor } from "../src/serviceMonitor";
+import { IServiceAliveness } from "../src/types";
 async function HealthCheck() {
-  /* urls.rmbProxy = "https://gridproxy.dev.grid.tf";
-  urls.relay = "wss://relay.dev.grid.tf";
-  urls.substrate = "wss://tfchain.dev.grid.tf/ws";
-  urls.graphql = "https://graphql.dev.grid.tf/graphql";
-   */
   try {
-    const s: TFServices = {
-      graphQL: { LivenessURL: "https://graphql.dev.grid.tf/graphql" },
-      gridProxy: { LivenessURL: "https://gridproxy.dev.grid.tf" },
-      tfChain: { LivenessURL: "wss://tfchain.dev.grid.tf/ws" },
-      rmb: { LivenessURL: "wss://relay.dev.grid.tf" },
-    };
-    console.log(await TFservicesLiveChecker(s));
+    const services: IServiceAliveness[] = [];
+    services.push(new GridProxyMonitor("<FakeURL>"));
+    services.push(new TFChainMonitor("wss://tfchain.dev.grid.tf/ws"));
+    services.push(new RMBMonitor("wss://relay.dev.grid.tf", "wss://tfchain.dev.grid.tf/ws", "", "sr25519"));
+    // monitor some services once
+    await checkServiceAliveness(services);
+    // keep monitoring services with Interval
+    await monitorServiceAliveness(services, 5);
+
     process.exit(0);
   } catch (err) {
     console.log(err);
