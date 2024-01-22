242a55c00a4023e58b773e043268a83faf87dbac
kassem <omarksm09@gmail.com>
2024-01-22 14:51:56 +0200

    update alivenessCheckers

--- packages/monitoring/src/serviceMonitor/alivenessChecker.ts
+++ packages/monitoring/src/serviceMonitor/alivenessChecker.ts
@@ -1,4 +1,5 @@
-import { IServiceAliveness } from "src/types";
+import { events } from "../helpers/events";
+import { IServiceAliveness } from "../types";
 
 const HEALTH_CHECK_INTERVAL = 5000; // ms
 const MAX_RETRIES = 2;
@@ -9,18 +10,25 @@ export async function monitorServiceAliveness(
 ): Promise<void> {
   // making sure we have at least one service to monitor
   while (services.length) {
-    for (const service of services) {
-      await checkServiceAliveness(service);
-    }
+    events.emit("logs", `Checking services status...`);
+    await checkServiceAliveness(services);
     await new Promise(resolve => setTimeout(resolve, interval));
   }
 }
 
-async function checkServiceAliveness(service: IServiceAliveness, retries = MAX_RETRIES, retryInterval = 1000) {
-  while (retries > 0) {
-    if (await service.isAlive()) return;
-    retries--;
-    if (retries > 0) await new Promise(resolve => setTimeout(resolve, retryInterval));
-    //else emit event
+export async function checkServiceAliveness(
+  services: IServiceAliveness[],
+  retries = MAX_RETRIES,
+  retryInterval = 1000,
+) {
+  for (const service of services) {
+    for (let retryCount = 1; retryCount <= retries; retryCount++) {
+      const { alive, error } = await service.isAlive();
+      if (alive) break;
+      if (retryCount < retries) {
+        events.emit("logs", `${service.ServiceName} seems to be down; Retrying (${retryCount}/${retries})...`);
+        await new Promise(resolve => setTimeout(resolve, retryInterval));
+      } else events.emit("serviceIsDown", service.ServiceName, error);
+    }
   }
 }
