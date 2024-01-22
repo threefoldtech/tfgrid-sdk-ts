2fa72b9d6eab27f3a9d1bcb37ae3ca3099d35ca2
kassem <omarksm09@gmail.com>
2024-01-22 15:06:08 +0200

    update alivenessCheckers and add js docs

--- packages/monitoring/src/serviceMonitor/alivenessChecker.ts
+++ packages/monitoring/src/serviceMonitor/alivenessChecker.ts
@@ -1,33 +1,38 @@
 import { events } from "../helpers/events";
 import { IServiceAliveness } from "../types";
 
-const HEALTH_CHECK_INTERVAL = 5000; // ms
-const MAX_RETRIES = 2;
-
-export async function monitorServiceAliveness(
-  services: IServiceAliveness[],
-  interval = HEALTH_CHECK_INTERVAL,
-): Promise<void> {
+/**
+ * Monitors the aliveness of one or multiple services at a specified interval.
+ *
+ * @param services - An array of services to monitor.
+ * @param interval - The interval, in minutes, between consecutive checks (default is 2 minutes).
+ *
+ */
+export async function monitorServiceAliveness(services: IServiceAliveness[], interval = 2): Promise<void> {
   // making sure we have at least one service to monitor
   while (services.length) {
     events.emit("logs", `Checking services status...`);
     await checkServiceAliveness(services);
-    await new Promise(resolve => setTimeout(resolve, interval));
+    await new Promise(resolve => setTimeout(resolve, interval * 60 * 1000));
   }
 }
 
-export async function checkServiceAliveness(
-  services: IServiceAliveness[],
-  retries = MAX_RETRIES,
-  retryInterval = 1000,
-) {
+/**
+ * Checks the aliveness of multiple services with optional retries.
+ *
+ * @param services - An array of services to check.
+ * @param retries - The number of retry attempts for each service (default is 2).
+ * @param retryInterval - The interval, in minutes, between consecutive retry attempts (default is 2 minutes).
+ * @returns A Promise that resolves when all services are checked.
+ */
+export async function checkServiceAliveness(services: IServiceAliveness[], retries = 2, retryInterval = 2) {
   for (const service of services) {
     for (let retryCount = 1; retryCount <= retries; retryCount++) {
       const { alive, error } = await service.isAlive();
       if (alive) break;
       if (retryCount < retries) {
         events.emit("logs", `${service.ServiceName} seems to be down; Retrying (${retryCount}/${retries})...`);
-        await new Promise(resolve => setTimeout(resolve, retryInterval));
+        await new Promise(resolve => setTimeout(resolve, retryInterval * 60));
       } else events.emit("serviceIsDown", service.ServiceName, error);
     }
   }
