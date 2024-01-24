e32e76aa80d7cac2043f192074d12e25a8312345
kassem <omarksm09@gmail.com>
2024-01-24 12:46:07 +0200

    update example

--- packages/monitoring/example/index.ts
+++ packages/monitoring/example/index.ts
@@ -5,26 +5,18 @@ async function HealthCheck() {
   try {
     const services: IServiceAliveness[] = [];
     services.push(new GridProxyMonitor("<FakeURL>"));
-    services.push(
-      new TFChainMonitor(
-        "wss://tfhain.dev.grid.tf/ws",
-        "energy garage female trend guard pipe skill dumb drill defy crush genuine",
-        "sr25519",
-      ),
-    );
-    services.push(
-      new RMBMonitor(
-        "wss://relay.dev.grid.tf",
-        "wss://tfchain.dev.grid.tf/ws",
-        "energy garage female trend guard pipe skill dumb drill defy crush genuine",
-        "sr25519",
-      ),
-    );
+    services.push(new TFChainMonitor("wss://tfhain.dev.grid.tf/ws", "<MNEMONIC>", "sr25519"));
+    services.push(new RMBMonitor("wss://relay.dev.grid.tf", "wss://tfchain.dev.grid.tf/ws", "<MNEMONIC>", "sr25519"));
     // monitor some services once
     await checkServiceAliveness(services);
-    // keep monitoring services with Interval
-    await monitorServiceAliveness(services, 5);
+
+    // disconnect
     await disconnectServices(services);
+
+    // keep monitoring services with Interval
+    const monitor = monitorServiceAliveness(services, 0.25);
+    await new Promise(resolve => setTimeout(resolve, 0.5 * 60 * 1000));
+    await monitor.exitAndDisconnect();
     process.exit(0);
   } catch (err) {
     console.log(err);
