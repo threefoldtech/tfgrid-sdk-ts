b95330d54da5ac44c9bc2c809421e2f8bc13773e
MohamedElmdary <engm5081@gmail.com>
2024-01-29 14:13:10 +0200

    feat: Add global error handling

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -9,8 +9,16 @@ import vuetify from "@/plugins/vuetify";
 import App from "./App.vue";
 import { defineGlobals } from "./config";
 import router from "./router";
+import { normalizeError } from "./utils/helpers";
 
 const app = createApp(App);
+
+app.config.errorHandler = error => {
+  console.error(
+    "[Uncatched Error]",
+    normalizeError(error, "Something went wrong in dashboard app but we couldn't fetch it."),
+  );
+};
 
 app.use(createPinia());
 app.use(router);
