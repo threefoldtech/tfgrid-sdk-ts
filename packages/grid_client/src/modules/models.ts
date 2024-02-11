ee7324f1844b67d8a81d6dce9484e8c748a85117
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-02-11 16:20:07 +0200

    rename mycelium_network_seed to myceliumNetworkSeed

--- packages/grid_client/src/modules/models.ts
+++ packages/grid_client/src/modules/models.ts
@@ -124,7 +124,7 @@ class MachinesModel {
 
 class AddMachineModel extends MachineModel {
   @Expose() @IsString() @IsNotEmpty() @IsAlphanumeric() @MaxLength(NameLength) deployment_name: string;
-  @Expose() @IsString() @IsOptional() mycelium_network_seed?: string;
+  @Expose() @IsString() @IsOptional() myceliumNetworkSeed?: string;
 }
 
 class DeleteMachineModel {
@@ -173,7 +173,7 @@ class K8SDeleteModel extends BaseGetDeleteModel {}
 
 class AddWorkerModel extends KubernetesNodeModel {
   @Expose() @IsString() @IsNotEmpty() @IsAlphanumeric() @MaxLength(NameLength) deployment_name: string;
-  @Expose() @IsString() @IsOptional() mycelium_network_seed: string;
+  @Expose() @IsString() @IsOptional() myceliumNetworkSeed: string;
 }
 
 class DeleteWorkerModel {
