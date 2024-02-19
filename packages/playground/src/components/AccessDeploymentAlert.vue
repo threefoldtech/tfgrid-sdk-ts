e59db434abf780003f2efec0c6a8bb6fa722b8fb
AlaaElattar <alaamahmoud.1223@gmail.com>
2024-02-19 12:32:03 +0200

    remove how to stop ygg, just metion both can't run together

--- packages/playground/src/components/AccessDeploymentAlert.vue
+++ packages/playground/src/components/AccessDeploymentAlert.vue
@@ -1,5 +1,8 @@
 <template>
   <VAlert type="info" variant="tonal" class="my-4">
+    Please note that Mycelium and Yggdrasil cannot run simultaneously. Ensure only one is active at a time.
+  </VAlert>
+  <VAlert type="info" variant="tonal" class="my-4">
     Access your deployments via SSH, with default username
     <span class="font-weight-bold text-grey-darken-1" v-text="'root'" />.
 
@@ -17,33 +20,15 @@
       </li>
     </ul>
   </VAlert>
-  <VAlert type="info" variant="tonal" class="my-4">
-    To Access mycelium IP, you should disable yggdrasil.
-    <ul :style="{ listStyle: 'square' }">
-      <li
-        v-for="method in myceliumMethods"
-        :key="method"
-        class="my-1"
-        :style="{ listStyle: 'square' }"
-        :class="{
-          'text-white': $vuetify.theme.global.name === 'dark',
-          'text-grey-darken-3': $vuetify.theme.global.name === 'light',
-        }"
-      >
-        &blacksquare; {{ method }}
-      </li>
-    </ul>
-  </VAlert>
 </template>
 
 <script lang="ts">
 const methods = ["public-ipv4", "planetary-network-ip", "public-ipv6", "wireguard-ip"];
-const myceliumMethods = ["systemctl stop yggdrasil (linux)", "launchctl kill yggdrasil (macos)"];
 
 export default {
   name: "AccessDeploymentAlert",
   setup() {
-    return { methods, myceliumMethods };
+    return { methods };
   },
 };
 </script>
