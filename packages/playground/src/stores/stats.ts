4550b502b4c7d829199cee508c3cef87bfb7538d
kassem <omarksm09@gmail.com>
2024-02-19 08:21:40 +0200

    update stats url

--- packages/playground/src/stores/stats.ts
+++ packages/playground/src/stores/stats.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 import { computed } from "vue";
 
 import { useAsync } from "@/hooks";
-const url = window.env.STATS_URL || "https://stats.grid.tf/";
+const url = window.env.STATS_URL || "https://stats.grid.tf";
 
 export const useRequestStore = defineStore("request-store", () => {
   const res = useAsync(() => fetch(url + "/api/stats-summary").then(resp => resp.json()), {
