b8ba13199f6ca2cd23cabbf33dc26906b9c7796d
zaelgohary <zainabelgohary19@gmail.com>
2024-03-14 15:02:04 +0200

    Remove Dashboard

--- packages/dashboard/src/portal/lib/activation.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-import config from "../config";
-
-export async function activateThroughActivationService(substrateAccountID: string) {
-  return axios.post(`${config.activationServiceUrl}/activation/activate`, {
-    substrateAccountID,
-  });
-}
