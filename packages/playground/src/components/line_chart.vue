37ec62591e2136cd1865cf207319371bd20c9244
zaelgohary <zainabelgohary19@gmail.com>
2024-03-19 15:15:38 +0200

    Fix vue/no-dupe-keys err in lint workflow

--- packages/playground/src/components/line_chart.vue
+++ packages/playground/src/components/line_chart.vue
@@ -11,11 +11,11 @@ const props = defineProps<{
   isProfit: boolean;
 }>();
 const X = (20 - 0.15) / 19;
-const xs = [...Array.from({ length: 20 }).map((_, i) => 0.15 + X * i)];
+const XS = [...Array.from({ length: 20 }).map((_, i) => 0.15 + X * i)];
 const data = computed(() => {
   if (props.isProfit) {
     return {
-      labels: props.xs.map(i => i.toFixed(2)) || xs.map(i => i.toFixed(2)),
+      labels: props.xs.map(i => i.toFixed(2)) || XS.map(i => i.toFixed(2)),
       datasets: [
         {
           label: "ROI",
@@ -28,11 +28,11 @@ const data = computed(() => {
     };
   }
   return {
-    labels: props.xs.map(i => i.toFixed(2)) || xs.map(i => i.toFixed(2)),
+    labels: props.xs.map(i => i.toFixed(2)) || XS.map(i => i.toFixed(2)),
     datasets: [
       {
         label: "Margin",
-        data: props.xs.map(x => props.getTotalReward(x)) || xs.map(x => props.getTotalReward(x)),
+        data: props.xs.map(x => props.getTotalReward(x)) || XS.map(x => props.getTotalReward(x)),
         backgroundColor: "rgb(26, 161, 143)",
         borderColor: "rgba(26, 161, 143, 0.5)",
         pointRadius: 3,
