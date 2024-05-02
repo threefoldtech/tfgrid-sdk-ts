98bc63e6fc2f56df5681c366e8fbc85e3c1ffbc2
zaelgohary <zainabelgohary19@gmail.com>
2024-05-02 13:25:54 +0300

    Add volume workload and use it instead of zmount

--- packages/grid_client/src/primitives/disk.ts
+++ packages/grid_client/src/primitives/disk.ts
@@ -1,6 +1,6 @@
+import { Volume } from "../zos/volume";
 import { Workload, WorkloadTypes } from "../zos/workload";
 import { Mount } from "../zos/zmachine";
-import { Zmount } from "../zos/zmount";
 
 class DiskPrimitive {
   createMount(name: string, mountpoint: string): Mount {
@@ -10,17 +10,17 @@ class DiskPrimitive {
     return mount;
   }
   create(size: number, name: string, metadata = "", description = "", version = 0): Workload {
-    const zmount = new Zmount();
-    zmount.size = size * 1024 ** 3;
+    const volume = new Volume();
+    volume.size = size * 1024 ** 3;
 
-    const zmount_workload = new Workload();
-    zmount_workload.version = version;
-    zmount_workload.name = name;
-    zmount_workload.type = WorkloadTypes.zmount;
-    zmount_workload.data = zmount;
-    zmount_workload.metadata = metadata;
-    zmount_workload.description = description;
-    return zmount_workload;
+    const volume_workload = new Workload();
+    volume_workload.version = version;
+    volume_workload.name = name;
+    volume_workload.type = WorkloadTypes.volume;
+    volume_workload.data = volume;
+    volume_workload.metadata = metadata;
+    volume_workload.description = description;
+    return volume_workload;
   }
   update(size: number, name: string, metadata = "", description = "", old_version = 1): Workload {
     return this.create(size, name, metadata, description, old_version + 1);
