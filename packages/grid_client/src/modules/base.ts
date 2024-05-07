99d2353299b060b58b22b3e533f408022f3473ee
zaelgohary <zainabelgohary19@gmail.com>
2024-05-07 10:15:19 +0300

    Seperate volume type in a standalone condition in _getDiskData

--- packages/grid_client/src/modules/base.ts
+++ packages/grid_client/src/modules/base.ts
@@ -16,7 +16,7 @@ import { DeploymentFactory } from "../primitives/deployment";
 import { Network } from "../primitives/network";
 import { Nodes } from "../primitives/nodes";
 import { BackendStorage, BackendStorageType } from "../storage/backend";
-import { Volume } from "../zos";
+import { Volume, Zmount } from "../zos";
 import { Deployment } from "../zos/deployment";
 import { PublicIPResult } from "../zos/public_ip";
 import { QuantumSafeFS, QuantumSafeFSResult } from "../zos/qsfs";
@@ -335,16 +335,20 @@ class BaseModule {
     for (const deployment of deployments) {
       if (deployment.contract_id !== contractId) continue;
       for (const workload of deployment.workloads) {
-        if (
-          (workload.type === WorkloadTypes.zmount || workload.type === WorkloadTypes.volume) &&
-          workload.name === name
-        ) {
+        if (workload.name !== name) continue;
+        if (workload.type === WorkloadTypes.zmount) {
+          return {
+            size: (workload.data as Zmount).size,
+            state: workload.result.state,
+            message: workload.result.message,
+          };
+        } else if (workload.type === WorkloadTypes.volume) {
           return {
             size: (workload.data as Volume).size,
             state: workload.result.state,
             message: workload.result.message,
           };
-        } else if (workload.type === WorkloadTypes.qsfs && workload.name === name) {
+        } else if (workload.type === WorkloadTypes.qsfs) {
           const data = workload.data as QuantumSafeFS;
           const metadata = JSON.parse(workload.metadata);
           return {
