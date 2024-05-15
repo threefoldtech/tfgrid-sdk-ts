0baa89359d2047e17995dc0233112b6c8e94c412
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-05-15 13:24:16 +0300

    fix: Removed unused 'console.log' in 'base.ts'

--- packages/grid_client/src/modules/base.ts
+++ packages/grid_client/src/modules/base.ts
@@ -177,8 +177,6 @@ class BaseModule {
 
   private async getMyContracts(fetch = false) {
     if (fetch || !this.contracts) {
-      console.log("modulesNames:", modulesNames[this.moduleName]);
-      console.log("moduleName: ", this.moduleName);
       let contracts = await this.tfClient.contracts.listMyNodeContracts({
         graphqlURL: this.config.graphqlURL,
         type: modulesNames[this.moduleName] ?? this.moduleName,
