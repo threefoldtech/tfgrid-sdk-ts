95f4c58bb449f3838e14fdad039ce68bf1bf5d2b
amiraabouhadid <amiraabouhadid@gmail.com>
2024-06-13 15:48:55 +0300

    add validation that checks if ip exists in any farm, not just selected farm

--- packages/playground/src/dashboard/components/add_ip.vue
+++ packages/playground/src/dashboard/components/add_ip.vue
@@ -17,7 +17,8 @@
               ></v-select>
               <input-validator
                 :value="publicIP"
-                :rules="[validators.required('IP is required.'), validators.isIPRange('Not a valid IP'), ipcheck]"
+                :rules="[validators.required('IP is required.'), validators.isIPRange('Not a valid IP')]"
+                :async-rules="[ipcheck]"
                 #="{ props }"
               >
                 <input-tooltip tooltip="IP address in CIDR format xxx.xxx.xxx.xxx/xx">
@@ -35,7 +36,8 @@
               <input-validator
                 v-if="type === IPType.range"
                 :value="toPublicIP"
-                :rules="[validators.required('IP is required.'), validators.isIPRange('Not a valid IP'), toIpCheck]"
+                :rules="[validators.required('IP is required.'), validators.isIPRange('Not a valid IP')]"
+                :async-rules="[toIpCheck]"
                 #="{ props }"
               >
                 <input-tooltip tooltip="IP address in CIDR format xxx.xxx.xxx.xxx/xx">
@@ -106,6 +108,7 @@ import { getIPRange } from "get-ip-range";
 import { default as PrivateIp } from "private-ip";
 import { ref, watch } from "vue";
 
+import { gqlClient } from "@/clients";
 import { IPType } from "@/utils/types";
 
 import { useGrid } from "../../stores";
@@ -146,12 +149,20 @@ export default {
       { deep: true },
     );
 
-    function ipcheck() {
+    async function ipcheck() {
       if (PrivateIp(publicIP.value.split("/")[0])) {
         return {
           message: "IP is not public",
         };
       }
+
+      const ips = await gqlClient.publicIps({ ip: true }, { where: { ip_eq: publicIP.value } });
+      if (ips.length > 0) {
+        return {
+          message: "IP exists",
+        };
+      }
+
       return undefined;
     }
 
@@ -163,7 +174,7 @@ export default {
       { deep: true },
     );
 
-    function toIpCheck() {
+    async function toIpCheck() {
       if (toPublicIP.value.split("/")[1] !== publicIP.value.split("/")[1]) {
         return {
           message: "Subnet is different.",
@@ -199,6 +210,12 @@ export default {
       if (PrivateIp(publicIP.value.split("/")[0])) {
         return {
           message: "IP is not public.",
+        };
+      }
+      const ips = await gqlClient.publicIps({ ip: true }, { where: { ip_eq: publicIP.value } });
+      if (ips.length > 0) {
+        return {
+          message: "IP exists",
         };
       }
       return undefined;
