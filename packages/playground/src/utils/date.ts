5eafcc14524e451bb5d51f9505943e81d9684a75
amiraabouhadid <amiraabouhadid@gmail.com>
2024-06-19 10:31:33 +0300

    update date time format

--- packages/playground/src/utils/date.ts
+++ packages/playground/src/utils/date.ts
@@ -1,5 +1,5 @@
 import moment from "moment";
 
 export default function toHumanDate(timeInSeconds: number): string {
-  return moment(timeInSeconds * 1000).format("M/D/YY, h:m A");
+  return moment(timeInSeconds * 1000).format("MM/DD/YY, hh:mm A");
 }
