2c872599d68a3280b6dfdc5c68e0651c28f84fdc
amiraabouhadid <amiraabouhadid@gmail.com>
2024-06-19 12:26:50 +0300

    update month to month name instead of number, make year 4 digits

--- packages/playground/src/utils/date.ts
+++ packages/playground/src/utils/date.ts
@@ -1,5 +1,5 @@
 import moment from "moment";
 
 export default function toHumanDate(timeInSeconds: number): string {
-  return moment(timeInSeconds * 1000).format("DD/MM/YY, hh:mm A");
+  return moment(timeInSeconds * 1000).format("DD MMMM YYYY, hh:mm A");
 }
