f245e0a2b1ea40e12cd4250640bbd946ef71741d
MohamedElmdary <engm5081@gmail.com>
2024-06-25 18:19:27 +0300

    feat: Add a class decrator with options to validate any needed prop/method

--- packages/grid_client/src/helpers/validator.ts
+++ packages/grid_client/src/helpers/validator.ts
@@ -31,4 +31,106 @@ function validateHexSeed(seed: string, length: number): boolean {
   return true;
 }
 
-export { validateObject, validateInput, validateHexSeed };
+interface ValidationOptions {
+  props?: boolean | string | string[];
+  methods?: boolean | string | string[];
+}
+
+function Validate(options?: ValidationOptions): ClassDecorator {
+  const _options = _normalizeValidationOptions(options);
+  return (target: any): any => {
+    const methods = _getMethods(target, _options);
+    for (const method of methods) {
+      const fn = target.prototype[method].bind(target.prototype);
+      target.prototype[method] = function (...args: any[]): any {
+        const errors = validateSync(this);
+        if (errors.length) {
+          throw errors;
+        }
+        return fn(...args);
+      };
+    }
+
+    return class extends target {
+      constructor(...args: any[]) {
+        super(args);
+
+        const props = _getProps(this, _options);
+        for (const prop of props) {
+          let _value = this[prop];
+
+          Object.defineProperty(this, prop, {
+            configurable: false,
+            enumerable: true,
+            get: () => _value,
+            set(value) {
+              const errors = validateSync(this);
+              for (const error of errors) {
+                if (error.property === prop) {
+                  throw error;
+                }
+              }
+              _value = value;
+            },
+          });
+        }
+      }
+    };
+  };
+}
+
+function _normalizeValidationOptions(options?: ValidationOptions): Required<ValidationOptions> {
+  return {
+    props: options?.props ?? true,
+    methods: options?.methods ?? true,
+  };
+}
+
+function _getProps(ctor: any, options: Required<ValidationOptions>): string[] {
+  /* This env variable should be used while testing to prevent throw error while setting values */
+  if (process.env.SKIP_PROPS_VALIDATION) {
+    return [];
+  }
+
+  if (options.props === true) {
+    return Object.getOwnPropertyNames(ctor);
+  }
+
+  if (typeof options.props === "string") {
+    return [options.props];
+  }
+
+  if (Array.isArray(options.props)) {
+    return options.props;
+  }
+
+  return [];
+}
+
+function _getMethods(ctor: any, options: Required<ValidationOptions>): string[] {
+  /* This env variable should be used to prevent throw error while calling methods if needed */
+  if (process.env.SKIP_METHODS_VALIDATION) {
+    return [];
+  }
+
+  if (options.methods === true) {
+    const methods = Object.getOwnPropertyNames(ctor.prototype);
+    const constructorIndex = methods.indexOf("constructor");
+    if (constructorIndex !== -1) {
+      methods.splice(constructorIndex, 1);
+    }
+    return methods;
+  }
+
+  if (typeof options.methods === "string") {
+    return [options.methods];
+  }
+
+  if (Array.isArray(options.methods)) {
+    return options.methods;
+  }
+
+  return [];
+}
+
+export { validateObject, validateInput, validateHexSeed, type ValidationOptions, Validate };
