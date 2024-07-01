1185ffec112806fe0782f85ca747207598680134
samaradel <samar@incubaid.com>
2024-07-01 16:48:13 +0300

    Fix password inner icon across all inputs

--- packages/playground/src/hooks/use_password_input.ts
+++ packages/playground/src/hooks/use_password_input.ts
@@ -7,5 +7,9 @@ export function usePasswordInput(defaultShow = false) {
     type: computed(() => (show.value ? "text" : "password")),
     "append-inner-icon": computed(() => (show.value ? "mdi-eye-outline" : "mdi-eye-off-outline")),
     "onClick:append-inner": () => (show.value = !show.value),
+    ref: () => {
+      const elements = document.querySelectorAll(".v-field__append-inner i");
+      elements.forEach((el: any) => el.setAttribute("tabindex", "-1"));
+    },
   });
 }
