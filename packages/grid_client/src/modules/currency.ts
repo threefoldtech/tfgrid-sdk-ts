5261a23347d4a8ba02134aa8732da778c6714157
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-07-01 14:24:08 +0300

    docs: Documented the Currency module.

--- packages/grid_client/src/modules/currency.ts
+++ packages/grid_client/src/modules/currency.ts
@@ -5,18 +5,40 @@ import { CurrencyModel } from "./models";
 
 class TFTUSDConversionService {
   // TFT rate: 1 TFT = x USD
+  /**
+   * Class representing a service for converting between TFT (ThreeFold Token) and USD (United States Dollar).
+   * The service provides methods for normalizing currency amounts, converting between TFT and USD, and calculating daily, monthly, and yearly amounts in both currencies.
+   */
   constructor(private _rate: number, private decimals = 2) {}
 
   get rate() {
     return this._rate;
   }
 
+  /**
+   * Normalize the currency amount to a string with a fixed number of decimals.
+   *
+   * @param options - The currency model containing the amount to normalize.
+   * @returns A string representation of the normalized currency amount.
+   * @decorators
+   * - `@expose`: Exposes the method for external use.
+   * - `@validateInput`: Validates the input options.
+   */
   @expose
   @validateInput
   normalizeCurrency(options: CurrencyModel): string {
     return new Decimal(options.amount).toFixed(this.decimals);
   }
 
+  /**
+   * Convert the given amount from `USD` to `TFT` using the current conversion rate.
+   *
+   * @param options - The currency model containing the amount in `USD` to convert.
+   * @returns A string representation of the converted amount in `TFT`.
+   * @decorators
+   * - `@expose`: Exposes the method for external use.
+   * - `@validateInput`: Validates the input options.
+   */
   @expose
   @validateInput
   convertUSDtoTFT(options: CurrencyModel): string {
@@ -24,6 +46,15 @@ class TFTUSDConversionService {
     return this.normalizeCurrency({ amount });
   }
 
+  /**
+   * Convert the given amount from `TFT` to `USD` using the current conversion rate.
+   *
+   * @param options - The currency model containing the amount in `TFT` to convert.
+   * @returns A string representation of the converted amount in `USD`.
+   * @decorators
+   * - `@expose`: Exposes the method for external use.
+   * - `@validateInput`: Validates the input options.
+   */
   @expose
   @validateInput
   convertTFTtoUSD(options: CurrencyModel): string {
@@ -31,6 +62,15 @@ class TFTUSDConversionService {
     return this.normalizeCurrency({ amount });
   }
 
+  /**
+   * Convert the given amount from `TFT` to `TFT` by multiplying with 24 hours.
+   *
+   * @param options - The currency model containing the amount in `TFT` to convert.
+   * @returns A string representation of the converted amount in `TFT`.
+   * @decorators
+   * - `@expose`: Exposes the method for external use.
+   * - `@validateInput`: Validates the input options.
+   */
   @expose
   @validateInput
   dailyTFT(options: CurrencyModel): string {
@@ -38,6 +78,15 @@ class TFTUSDConversionService {
     return this.normalizeCurrency({ amount: hours });
   }
 
+  /**
+   * Convert the given amount from `TFT` to `TFT` by multiplying with 30 days.
+   *
+   * @param options - The currency model containing the amount in `TFT` to convert.
+   * @returns A string representation of the converted amount in `TFT`.
+   * @decorators
+   * - `@expose`: Exposes the method for external use.
+   * - `@validateInput`: Validates the input options.
+   */
   @expose
   @validateInput
   monthlyTFT(options: CurrencyModel): string {
@@ -45,6 +94,15 @@ class TFTUSDConversionService {
     return this.normalizeCurrency({ amount: months });
   }
 
+  /**
+   * Convert the given amount from TFT to TFT by multiplying with 12 months.
+   *
+   * @param options - The currency model containing the amount in TFT to convert.
+   * @returns A string representation of the converted amount in TFT.
+   * @decorators
+   * - `@expose`: Exposes the method for external use.
+   * - `@validateInput`: Validates the input options.
+   */
   @expose
   @validateInput
   yearlyTFT(options: CurrencyModel): string {
@@ -52,6 +110,15 @@ class TFTUSDConversionService {
     return this.normalizeCurrency({ amount: years });
   }
 
+  /**
+   * Convert the given amount from USD to USD by multiplying with 24 hours.
+   *
+   * @param options - The currency model containing the amount in USD to convert.
+   * @returns A string representation of the converted amount in USD.
+   * @decorators
+   * - `@expose`: Exposes the method for external use.
+   * - `@validateInput`: Validates the input options.
+   */
   @expose
   @validateInput
   dailyUSD(options: CurrencyModel): string {
@@ -59,6 +126,15 @@ class TFTUSDConversionService {
     return this.normalizeCurrency({ amount: hours });
   }
 
+  /**
+   * Convert the given amount from `USD` to `USD` by multiplying with 30 days.
+   *
+   * @param options - The currency model containing the amount in `USD` to convert.
+   * @returns A string representation of the converted amount in `USD`.
+   * @decorators
+   * - `@expose`: Exposes the method for external use.
+   * - `@validateInput`: Validates the input options.
+   */
   @expose
   @validateInput
   monthlyUSD(options: CurrencyModel): string {
@@ -66,6 +142,15 @@ class TFTUSDConversionService {
     return this.normalizeCurrency({ amount: months });
   }
 
+  /**
+   * Convert the given amount from `USD` to `USD` by multiplying with `12 months`.
+   *
+   * @param options - The currency model containing the amount in `USD` to convert.
+   * @returns A string representation of the converted amount in `USD`.
+   * @decorators
+   * - `@expose`: Exposes the method for external use.
+   * - `@validateInput`: Validates the input options.
+   */
   @expose
   @validateInput
   yearlyUSD(options: CurrencyModel): string {
