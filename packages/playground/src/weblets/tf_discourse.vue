72510892627e2603fb202979f43d27b9f8f4dcba
mayar osama <mayarosama18@gmail.com>
2024-07-03 10:54:45 +0300

    Fixing typo

--- packages/playground/src/weblets/tf_discourse.vue
+++ packages/playground/src/weblets/tf_discourse.vue
@@ -23,7 +23,7 @@
           :value="name"
           :rules="[
             validators.required('Name is required.'),
-            validators.IsAlphanumericExpectUnderscore('Name should consist of letters ,numbers and undersocres only.'),
+            validators.IsAlphanumericExpectUnderscore('Name should consist of letters ,numbers and underscores only.'),
             name => validators.isAlpha('Name must start with alphabet char.')(name[0]),
             validators.minLength('Name must be at least 2 characters.', 2),
             validators.maxLength('Name cannot exceed 50 characters.', 50),
--- packages/playground/src/weblets/tf_kubernetes.vue
+++ packages/playground/src/weblets/tf_kubernetes.vue
@@ -28,7 +28,7 @@
           :rules="[
             validators.required('Name is required.'),
             name => validators.isAlpha('Name must start with alphabet char.')(name[0]),
-            validators.IsAlphanumericExpectUnderscore('Name should consist of letters ,numbers and undersocres only.'),
+            validators.IsAlphanumericExpectUnderscore('Name should consist of letters ,numbers and underscores only.'),
             validators.minLength('Name minimum length is 2 chars.', 2),
             validators.maxLength('Name max length is 50 chars.', 50),
           ]"
