83765095c0cc857a6b51a656b31bf9c3d188ee7e
samaradel <samar@incubaid.com>
2024-07-03 15:03:16 +0300

    fix build

--- packages/playground/src/hooks/use_password_input.ts
+++ packages/playground/src/hooks/use_password_input.ts
@@ -1,8 +1,8 @@
-import { computed, reactive, ref, VNodeRef } from "vue";
+import { computed, reactive, ref, type VNodeRef } from "vue";
 
 interface PasswordInputProps {
-  type?: "text" | "password";
-  "append-inner-icon"?: "mdi-eye-outline" | "mdi-eye-off-outline";
+  type?: string;
+  "append-inner-icon"?: string;
   "onClick:append-inner"?: () => boolean;
   ref?: (ref: VNodeRef | null) => void;
   disabled?: boolean;
@@ -18,15 +18,14 @@ export function usePasswordInput(defaultShow = false): PasswordInputProps {
     type: computed(() => (show.value ? "text" : "password")),
     "append-inner-icon": computed(() => (show.value ? "mdi-eye-outline" : "mdi-eye-off-outline")),
     "onClick:append-inner": () => (show.value = !show.value),
-    ref: (el: VNodeRef | null) => {
-      if (el && isComponentPublicInstance(el)) {
-        const innerIcons = el.$el.querySelectorAll(".v-field__append-inner i");
+    ref: (ref: VNodeRef | null) => {
+      if (ref && isComponentPublicInstance(ref)) {
+        const innerIcons = ref.$el.querySelectorAll(".v-field__append-inner i");
         innerIcons.forEach(icon => icon.setAttribute("tabindex", "-1"));
       }
     },
   });
 }
-
 function isComponentPublicInstance(el: VNodeRef): el is VNodeRef & { $el: HTMLElement } {
   return typeof el === "object" && "$el" in el;
 }
