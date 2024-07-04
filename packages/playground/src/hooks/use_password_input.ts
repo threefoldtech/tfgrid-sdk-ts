a8b275d9bf0d241ec48b16ec269727fdb17ce5dc
samaradel <samar@incubaid.com>
2024-07-04 14:49:02 +0300

    Fix build

--- packages/playground/src/hooks/use_password_input.ts
+++ packages/playground/src/hooks/use_password_input.ts
@@ -1,4 +1,4 @@
-import { type ComponentPublicInstance, computed, reactive, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 
 export function usePasswordInput(defaultShow = false) {
   const show = ref(defaultShow);
@@ -7,7 +7,7 @@ export function usePasswordInput(defaultShow = false) {
     type: computed(() => (show.value ? "text" : "password")),
     "append-inner-icon": computed(() => (show.value ? "mdi-eye-outline" : "mdi-eye-off-outline")),
     "onClick:append-inner": () => (show.value = !show.value),
-    ref: (el: ComponentPublicInstance<{}> | null) => {
+    ref: (el: any) => {
       if (el) {
         const innerIcons = el.$el.querySelectorAll(".v-field__append-inner i");
         innerIcons.forEach((icon: HTMLElement) => icon.setAttribute("tabindex", "-1"));
