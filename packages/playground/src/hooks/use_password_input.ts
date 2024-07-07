67d1fc1962865074d55b26a966c2fb3892964ac0
Samar Adel <samar@incubaid.com>
2024-07-07 10:33:52 +0300

    Fix inputs with show/hide password icon (#3045)
    * - Remove password input wrapper
    - Create prepend inner icon for inputs with type password
    - Fix show/hide icon selection when press tab
    - Fix the order of dialog actions tabindex
    
    * Remove password input wrapper exporting from hooks
    
    * Revert "Remove password input wrapper exporting from hooks"
    
    This reverts commit befc351b27326726b7b7557d0c8eb38d8ad1333d.
    
    * Revert "- Remove password input wrapper"
    
    This reverts commit d957ceef6d333567eb1e22dbb119c1c858e6375d.
    
    * Fix password inner icon
    
    * Revert "Fix password inner icon"
    
    This reverts commit 8fa16408ed512e15a01a6c00faf05b610677ba61.
    
    * Fix password inner icon across all inputs
    
    * rm el type
    
    * Access the icon via $el
    
    * Fix el types
    
    * fix build
    
    * Create an interface for password input props
    
    * Make input type optional
    
    * create function to check el & $el type
    
    * fix build
    
    * Fix types
    
    * Fix types
    
    * Fix build

--- packages/playground/src/hooks/use_password_input.ts
+++ packages/playground/src/hooks/use_password_input.ts
@@ -7,5 +7,11 @@ export function usePasswordInput(defaultShow = false) {
     type: computed(() => (show.value ? "text" : "password")),
     "append-inner-icon": computed(() => (show.value ? "mdi-eye-outline" : "mdi-eye-off-outline")),
     "onClick:append-inner": () => (show.value = !show.value),
+    ref: (el: any) => {
+      if (el) {
+        const innerIcons = el.$el.querySelectorAll(".v-field__append-inner i");
+        innerIcons.forEach((icon: HTMLElement) => icon.setAttribute("tabindex", "-1"));
+      }
+    },
   });
 }
