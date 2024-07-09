f8a861e030033669e8a25aba553ea2da87e9827c
kassem <omarksm09@gmail.com>
2024-07-09 18:07:37 +0300

    Docs: add example of useing SerivceUrlManager

---
+++ packages/monitoring/example/stackAvilablity.ts
@@ -0,0 +1,25 @@
+const gridproxy = ["test", "hello", "https://gridproxy.dev.grid.tf/"];
+const rmb = ["wsefews://tfin.dev.grid.tf/ws", "wss://relay.dev.grid.tf"];
+const graphql = ["https://graphql.dev.grid.tf/graphql"];
+const tfChain = ["wss://tfchain.dev.grid.tf/ws", "wsss://tfchain.dev.grid.tf/ws"];
+const mnemonic = "<MNEMONIC>";
+
+import { ServiceUrlManager, StackPickerOptions } from "../src";
+
+async function checkStacksAvailability(services: StackPickerOptions) {
+  try {
+    const pickedUrls = await new ServiceUrlManager(services).GetAvailableServices();
+    console.log(pickedUrls);
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+checkStacksAvailability({
+  tfChain,
+  rmb,
+  graphql,
+  gridproxy,
+  mnemonic,
+});
