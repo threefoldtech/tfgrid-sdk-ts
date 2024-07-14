2727b81cdc653832d81956b4e79506217c65b689
kassem <omarksm09@gmail.com>
2024-07-14 15:05:12 +0300

    Feat: Add Stats service aliveness checker

---
+++ packages/monitoring/src/serviceMonitor/stats.ts
@@ -0,0 +1,22 @@
+import { resolveServiceStatus, sendGetRequest } from "../helpers/utils";
+import { ILivenessChecker, ServiceStatus } from "../types";
+
+export class Stats implements ILivenessChecker {
+  private readonly name = "Stats";
+  private url: string;
+  constructor(statusUrl: string) {
+    this.url = statusUrl;
+  }
+  serviceName() {
+    return this.name;
+  }
+  serviceUrl() {
+    return this.url;
+  }
+  updateUrl(url: string) {
+    this.url = url;
+  }
+  async isAlive(): Promise<ServiceStatus> {
+    return resolveServiceStatus(sendGetRequest(this.url));
+  }
+}
