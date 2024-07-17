db0df5beac74451fa97e6952b9da3d45cc30a4a0
kassem <omarksm09@gmail.com>
2024-07-17 14:41:13 +0300

    refactor: RMB: make service url optional in conostracutor

--- packages/monitoring/src/serviceMonitor/rmb.ts
+++ packages/monitoring/src/serviceMonitor/rmb.ts
@@ -1,31 +1,43 @@
-import { KeypairType } from "@polkadot/util-crypto/types";
 import { Client as RMBClient } from "@threefold/rmb_direct_client";
 
 import { generateString, resolveServiceStatus } from "../helpers/utils";
-import { IDisconnectHandler, ILivenessChecker, ServiceStatus } from "../types";
+import { IDisconnectHandler, ILivenessChecker, RMBProps, ServiceStatus } from "../types";
 
 export class RMBMonitor implements ILivenessChecker, IDisconnectHandler {
   private name = "RMB";
-  private url: string;
   private rmbClient: RMBClient;
-  constructor(relayUrl: string, chainUrl: string, mnemonic: string, keypairType: KeypairType) {
-    this.url = relayUrl;
-    this.rmbClient = new RMBClient(chainUrl, relayUrl, mnemonic, generateString(10), keypairType, 0);
+  constructor(private RMBProps: RMBProps) {
+    if (RMBProps) this.factory();
   }
+
+  /*
+    Creates a new RMB Client instance
+  */
+  private factory() {
+    const { chainUrl, relayUrl, mnemonics, keypairType } = this.RMBProps;
+    if (relayUrl) {
+      this.rmbClient = new RMBClient(chainUrl, relayUrl, mnemonics, generateString(10), keypairType, 0);
+    }
+  }
+
   private async setUp() {
+    if (!this.rmbClient) throw new Error("Can't setUp before initialization");
     await this.rmbClient.connect();
   }
   public get Name() {
     return this.name;
   }
   public get URL() {
-    return this.url;
+    if (!this.RMBProps.relayUrl) throw new Error("Can't access before initialization");
+    return this.RMBProps.relayUrl;
   }
   public set URL(url: string) {
     async () => {
-      await this.disconnect();
-      this.url = url;
-      this.rmbClient.relayUrl = url;
+      this.RMBProps.relayUrl = url;
+      if (this.rmbClient) {
+        await this.disconnect();
+        this.rmbClient.relayUrl = this.RMBProps.relayUrl;
+      } else this.factory();
     };
   }
   public async isAlive(): Promise<ServiceStatus> {
