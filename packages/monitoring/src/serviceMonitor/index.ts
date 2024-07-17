75708fd8fc2d9b5e40148df8ebf46fb71ccb5f63
kassem <omarksm09@gmail.com>
2024-07-17 16:51:28 +0300

    chore: update exports

--- packages/monitoring/src/serviceMonitor/index.ts
+++ packages/monitoring/src/serviceMonitor/index.ts
@@ -1,5 +1,6 @@
 export { GridProxyMonitor } from "./gridproxy";
 export { TFChainMonitor } from "./tfChain";
 export { RMBMonitor } from "./rmb";
-export { ServiceMonitor, ServiceUrlManager } from "./alivenessChecker";
+export { ServiceMonitor } from "./alivenessChecker";
+export { ServiceUrlManager } from "./serviceURLManager";
 export { GraphQLMonitor } from "./graphql";
