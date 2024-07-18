c81f7144af21b31c96ba8f2e5bca9cb05e68ec11
kassem <omarksm09@gmail.com>
2024-07-18 07:52:42 +0300

    Example: add serviceURlManager Example

---
+++ packages/monitoring/example/serviceURLManager.ts
@@ -0,0 +1,38 @@
+import {
+  GraphQLMonitor,
+  GridProxyMonitor,
+  RMBMonitor,
+  ServiceUrlManager,
+  TFChainMonitor,
+  URLManagerOptions,
+} from "../src";
+
+const gridproxy = ["http://www.nonexistentgridProxy.com", "http://www.gooogleTF.com", "https://gridproxy.dev.grid.tf/"];
+const rmb = ["wss://www.nonExistentRelay.com", "wss://relay.dev.grid.tf"];
+const graphql = ["https://graphql.dev.grid.tf/graphql"];
+const tfChain = ["wss://tfchain.dev.grid.tf/ws", "wss://www.nonExistentChain.com"];
+const mnemonics = "<MNEMONIC>";
+
+async function checkStacksAvailability(services: URLManagerOptions) {
+  try {
+    const pickedUrls = await new ServiceUrlManager(services).getAvailableServicesStack();
+    console.log(pickedUrls);
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+checkStacksAvailability({
+  retries: 3,
+  silent: false,
+  services: [
+    { service: new GridProxyMonitor(), URLs: gridproxy },
+    { service: new GraphQLMonitor(), URLs: graphql },
+    {
+      service: new RMBMonitor({ mnemonics, chainUrl: "wss://tfchain.02.dev.grid.tf/ws", keypairType: "sr25519" }),
+      URLs: rmb,
+    },
+    { service: new TFChainMonitor(), URLs: tfChain },
+  ],
+});
