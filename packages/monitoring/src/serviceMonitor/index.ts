ff29b60904057d000f1b80ded1dc23f982d29c1c
kassem <omarksm09@gmail.com>
2024-07-23 14:35:50 +0300

    chore: export new services

--- packages/monitoring/src/serviceMonitor/index.ts
+++ packages/monitoring/src/serviceMonitor/index.ts
@@ -3,4 +3,6 @@ export { TFChainMonitor } from "./tfChain";
 export { RMBMonitor } from "./rmb";
 export { ServiceMonitor } from "./alivenessChecker";
 export { GraphQLMonitor } from "./graphql";
+export { StatsMonitor } from "./stats";
+export { ActivationMonitor } from "./activations";
 export { ServiceUrlManager } from "./serviceURLManager";
