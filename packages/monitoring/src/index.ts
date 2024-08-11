457da79ae38463ac87c9d6faca9922dc40eb5c98
Omar Kassem <omarksm09@gmail.com>
2024-08-11 14:02:10 +0300

    `Monitoring`:  add service URL manager (#3134)
    * Support es6 build
    
    * Refactor: update events make the error optional and support log with color
    
    * - Refactor
      - sendGetRequest to sendRequest and use fetch instead of axios.
      - replace serviceUrl and serviceName with getters
      - add update method that uses URL setter
      - Make all IServiceBase classes take an optional options on their contractor, add error handling for side effect
      -  Grouped all RMB properties to RMB to RMBProps
    - Feat:
     - add stats monitor class that monitors stats service
     - add activation monitor class  that monitor activation service
    
    * Feat: Add serviceURLManager
    it is class managing service URLs with liveness checking <pick the first reachable endpoint for each service>
    
    - Define retries and silent mode settings
    - Implement constructor to handle StackManagerOptions
    - Add private handleErrorsOnSilentMode method for error handling based on silent mode
    - Implement getAvailableStack method to find reachable service URL
    - Implement getAvailableServicesStack method to fetch and store available service URLs for all services
    
    * Revert: playground/config.js
    
    * Refactor: remove sesstion from RMBOptions it will be handled internally
    
    * Refactor: update service monitor example
    
    * Refactor: Rename service url manager options
    
    * Refactor: make tfchainMonitor consotractor takes an optional param
    
    * Example: add serviceURlManager Example
    
    * Fix: remove error on getting Url before intialization and make it return undefined or emptystring
    remove debug lines
    
    * Chore: RMB handle error event
    
    * Example: add template type to support silent
    
    * Chore:
    - change naming
    - remove error on get url and return empty string in case the url is not set yet
    
    * Chore:
    - add errror handling in isAlive, will throw in case the service url is not set
    
    * chore: export new services
    
    * Chore: make tfchain url optional
    
    * chore: change stats url to api/summary
    
    * chore: add checks to prevent accessing undefined functions in RMB connect and disconnect
    
    * chore: validate rmb connectivity though gridproxy health endpoint
    
    * chore: add error handling to isAlive
    
    * chore: support timeout, and log errors using events
    
    * chore: support timeout
    
    * refactor: support increasing timeout
    
    * fix: return status object instade of throwing error
    
    * refactor:
    - rename constractor param
    - use replace instade of split
    
    * Refactor: use /health endpoint in tfchain monitor
    
    * refactor: remove logs line
    
    * feat: isAlive can take new url
    
    * Feat: ping url in parallel
    
    * Docs: update example
    
    * Refactor: retry logic moved to all stacks, to avoid delay
    
    * refactor: enahnce logs
    
    * docs: update urls in example
    
    * Chore: remove disconnect logic
    
    * chore: Remove IDisconnectHandler interface
    
    * Chore: update monitoring example
    
    * chore: Update the service url on success
    
    * Refactor: remove update function and replace it with setter
    
    * Refactor: add ServiceBase class
    
    * chore: predefine the timeout error

--- packages/monitoring/src/index.ts
+++ packages/monitoring/src/index.ts
@@ -1,1 +1,2 @@
 export * from "./serviceMonitor/index";
+export * from "./types";
--- packages/monitoring/src/serviceMonitor/activations.ts
+++ packages/monitoring/src/serviceMonitor/activations.ts
@@ -1,15 +1,14 @@
 import { resolveServiceStatus, sendRequest } from "../helpers/utils";
 import { ILivenessChecker, ServiceStatus } from "../types";
 import { ServiceBase } from "./serviceBase";
-export class TFChainMonitor extends ServiceBase implements ILivenessChecker {
+
+export class ActivationMonitor extends ServiceBase implements ILivenessChecker {
   constructor(ServiceUrl?: string) {
-    super("TFChain");
+    super("Activation");
     if (ServiceUrl) this.url = ServiceUrl;
   }
   async isAlive(url = this.url): Promise<ServiceStatus> {
     if (!url) throw new Error("Can't access before initialization");
-    let _url = url.replace("wss", "https");
-    _url = _url.replace("/ws", "/health");
-    return resolveServiceStatus(sendRequest(_url, { method: "Get" }));
+    return resolveServiceStatus(sendRequest(url, { method: "Get" }));
   }
 }
