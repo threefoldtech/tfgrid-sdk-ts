de6e731dbf2c3df516219d307d9dabb71e8c3bbf
A-Harby <harbya@incubaid.com>
2024-08-13 12:18:05 +0300

    add time import

--- packages/playground/tests/frontend_selenium/pages/transfer.py
+++ packages/playground/tests/frontend_selenium/pages/transfer.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
+import time
 
 class TransferPage:
 
