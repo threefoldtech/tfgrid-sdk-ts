ca9ec967e5c0f7f6a1346830d865d3b3e6969752
Mahmoud Emad <mahmmoud.hassanein@gmail.com>
2024-08-14 09:23:52 +0300

    fix: dynamically defines the TFChainError key prop:
    - dynamically defines the TFChainError key property instead of making a condition on each key in the 'TFChainError' contractor.

--- packages/tfchain_client/src/errors.ts
+++ packages/tfchain_client/src/errors.ts
@@ -28,27 +28,18 @@ class TFChainError extends Error {
     super(options.message);
     this.name = "TFChainError";
 
-    if (options.keyError) {
-      Object.defineProperty(this, "keyError", { value: options.keyError, enumerable: true });
-    } else {
-      Object.defineProperty(this, "keyError", { value: "GenericError", enumerable: true });
+    if (!options.keyError) {
+      options.keyError = "GenericError";
     }
 
-    if (options.section) {
-      Object.defineProperty(this, "section", { value: options.section, enumerable: true });
-    }
-
-    if (options.args) {
-      Object.defineProperty(this, "args", { value: options.args, enumerable: true });
-    }
-
-    if (options.method) {
-      Object.defineProperty(this, "method", { value: options.method, enumerable: true });
-    }
-
-    if (options.docs) {
-      Object.defineProperty(this, "docs", { value: options.docs, enumerable: true });
-    }
+    Object.keys(options).forEach(key => {
+      if (options[key as keyof ITFChainError]) {
+        Object.defineProperty(this, key, {
+          value: options[key as keyof ITFChainError],
+          enumerable: true,
+        });
+      }
+    });
   }
 }
 
