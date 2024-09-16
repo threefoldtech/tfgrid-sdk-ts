1dc08132dd8a5b430f5a30d969a8e04352e0b154
amiraabouhadid <amiraabouhadid@gmail.com>
2024-09-16 14:09:36 +0300

    remove contract billing and discount type from grid proxy client

--- packages/gridproxy_client/src/modules/contracts.ts
+++ packages/gridproxy_client/src/modules/contracts.ts
@@ -2,25 +2,11 @@ import { ContractsBuilder, ContractsQuery, ContractState, ContractType } from ".
 import { resolvePaginator } from "../utils";
 import { AbstractClient } from "./abstract_client";
 
-export enum Discount {
-  Default = "Default",
-  None = "none",
-  Bronze = "Bronze",
-  Silver = "Silver",
-  Gold = "Gold",
-}
-
 export interface ContractDetails {
   nodeId: number;
   deployment_data?: string;
   deployment_hash?: string;
   number_of_public_ips?: number;
-}
-
-export interface ContractBilling {
-  amountBilled: number;
-  discountReceived: Discount;
-  timestamp: number;
 }
 
 export interface Contract {
@@ -30,7 +16,6 @@ export interface Contract {
   created_at: number;
   type: ContractType;
   details: ContractDetails;
-  billing: Array<ContractBilling>;
 }
 
 export class ContractsClient extends AbstractClient<ContractsBuilder, ContractsQuery> {
