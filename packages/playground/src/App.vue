44e8c1b96bdd699c36c6eb8a0f8db66b29653f4a
amiraabouhadid <amiraabouhadid@gmail.com>
2024-09-17 12:55:40 +0300

    Revert "Update packages/playground/src/App.vue"
    This reverts commit d9765bfc31bd92a61dedae3984b205f95f9b0789.

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -338,13 +338,26 @@ async function setTimeouts() {
   const client = gridStore.client as GridClient;
 
   const localStorageDeploymentTimeout = localStorage.getItem(LocalStorageSettingsKey.TIMEOUT_DEPLOYMENT_KEY);
-  if (client && !localStorageDeploymentTimeout) {
-    const deploymentTimeoutMinutes = client.clientOptions.deploymentTimeoutMinutes;
-    if (deploymentTimeoutMinutes) {
-      localStorage.setItem(LocalStorageSettingsKey.TIMEOUT_DEPLOYMENT_KEY, `${+deploymentTimeoutMinutes * 60}`);
-    } else {
-      client.clientOptions.deploymentTimeoutMinutes = +localStorageDeploymentTimeout! / 60;
-      await client.connect();
+  if (!localStorageDeploymentTimeout) {
+    if (client) {
+      const clientOptions = client.clientOptions;
+      if (clientOptions) {
+        const deploymentTimeoutMinutes = clientOptions.deploymentTimeoutMinutes;
+        if (deploymentTimeoutMinutes) {
+          localStorage.setItem(LocalStorageSettingsKey.TIMEOUT_DEPLOYMENT_KEY, `${+deploymentTimeoutMinutes * 60}`);
+        }
+      }
+    }
+  } else {
+    if (client) {
+      const clientOptions = client.clientOptions;
+      if (clientOptions) {
+        const deploymentTimeoutMinutes = clientOptions.deploymentTimeoutMinutes;
+        if (deploymentTimeoutMinutes) {
+          client.clientOptions.deploymentTimeoutMinutes = +localStorageDeploymentTimeout / 60;
+          await client.connect();
+        }
+      }
     }
   }
 }
