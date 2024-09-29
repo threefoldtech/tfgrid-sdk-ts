f8022ebc3307818a408dec6e781460a2ebee46f3
maayarosama <mayarosama18@gmail.com>
2024-09-29 10:47:35 +0300

    Adding workload type instances

--- packages/grid_client/tests/modules/workload.test.ts
+++ packages/grid_client/tests/modules/workload.test.ts
@@ -1,6 +1,6 @@
 import { plainToClass } from "class-transformer";
 
-import { DeploymentResult, ResultStates, Workload, WorkloadTypes } from "../../src";
+import { DeploymentResult, ResultStates, Workload, WorkloadTypes, ZdbModes } from "../../src";
 import {
   ComputeCapacity,
   GatewayFQDNProxy,
@@ -27,13 +27,37 @@ const createDataInstance = (type: WorkloadTypes) => {
   const disks = new Mount();
 
   const rootfs_size = 2;
+  const size = 100 * 1025 ** 2;
+  // const qsfsZdbName = "exampleZDBName";
+  // const qsfsCount = 5;
+  // const qsfsNodes = [];
+  // const qsfsPassword = "examplePassword123";
+  // const qsfsDiskSize = 10;
+  // const cpu = 2;
+  // const memory = 2048;
+  // const rootfsSize = 4;
+  // const networkName = "exampleNetwork";
+  // const vmName = "exampleVM";
+  // const disksQsfs = [];
+  // const publicIP = false;
+  // const ipRangeClassA = "10.1.0.0/16";
+  // const ipRangeClassB = "172.16.0.0/16";
+  // const ipRangeClassC = "192.168.0.0/16";
+  // const ipRange = ipRangeClassA;
+  // const metadata = "{'deploymentType': 'vm'}";
+  // const description = "test deploying VM via ts grid3 client";
+  // const qsfsName = "exampleQSFS";
+  // const qsfsEncryptionKey = "encryptionKey123";
+  // const qsfsPrefix = "prefix";
+  // const qsfsCache = 2;
+  // const qsfsMountPoint = "/exampleMountPoint";
+  // const envVarValue = "envVarValue";
 
   switch (type) {
     case WorkloadTypes.zmachine:
       instance = new Zmachine();
       computeCapacity.cpu = 1;
       computeCapacity.memory = 256 * 1024 ** 2;
-
       network.planetary = true;
       network.public_ip = "10.249.0.0/16";
       network.interfaces = [
@@ -63,39 +87,51 @@ const createDataInstance = (type: WorkloadTypes) => {
       break;
     case WorkloadTypes.zmount:
       instance = new Zmount();
-      // Initialize zmount variables
+      instance.size = size;
+
       break;
     case WorkloadTypes.volume:
       instance = new Volume();
-      // Initialize volume variables
+      instance.size = size;
       break;
-    case WorkloadTypes.network:
-      instance = new Znet();
-      // Initialize znet variables
-      break;
+
     case WorkloadTypes.zdb:
       instance = new Zdb();
-      // Initialize zdb variables
+      instance.size = size;
+      instance.mode = ZdbModes.user;
+      instance.password = "123456";
+      instance.public = false;
       break;
+
     case WorkloadTypes.ip:
       instance = new PublicIP();
-      // Initialize publicIP variables
+      instance.v4 = true;
+      instance.v6 = false;
+
       break;
     case WorkloadTypes.qsfs:
       instance = new QuantumSafeFS();
+
       // Initialize qsfs variables
       break;
     case WorkloadTypes.zlogs:
       instance = new Zlogs();
-      // Initialize zlogs variables
+      instance.zmachine = "zmachine";
+      instance.output = "zlog";
       break;
     case WorkloadTypes.gatewayfqdnproxy:
       instance = new GatewayFQDNProxy();
-      // Initialize gatewayFQDNProxy variables
+      instance.fqdn = "dmftv9qfff.gent02.dev.grid.tf";
+      instance.tls_passthrough = false;
+      instance.backends = ["http://185.206.122.43:80"];
+
       break;
     case WorkloadTypes.gatewaynameproxy:
       instance = new GatewayNameProxy();
-      // Initialize gatewayNameProxy variables
+      instance.name = "GatewayNameProxy";
+      instance.tls_passthrough = false;
+      instance.backends = ["http://185.206.122.43:80"];
+
       break;
     default:
       throw new Error(`Invalid WorkloadType: ${type}`);
@@ -104,24 +140,24 @@ const createDataInstance = (type: WorkloadTypes) => {
   return instance;
 };
 
-// Initialize workload before each test
-beforeEach(() => {
-  const dataInstance = createDataInstance(WorkloadTypes.zmachine); //The type you want to test can be changed
-  workload = new Workload();
-  workload.version = 1;
-  workload.name = "Test Workload";
-  workload.type = WorkloadTypes.zmachine; // Should reflect created data instance
-  workload.metadata = "Metadata";
-  workload.description = "A test for workload";
-  workload.data = dataInstance;
-  workload.result = new DeploymentResult();
-  workload.result.created = Date.now();
-  workload.result.state = ResultStates.ok;
-  workload.result.message = "Deployment successful";
-  workload.result.data = workload.data;
-});
+// Test for each WorkloadType
+describe.each(Object.values(WorkloadTypes))("Workload Tests for %s", type => {
+  beforeEach(() => {
+    const dataInstance = createDataInstance(type);
+    workload = new Workload();
+    workload.version = 1;
+    workload.name = "Test Workload";
+    workload.type = type;
+    workload.metadata = "Metadata";
+    workload.description = "A test for workload";
+    workload.data = dataInstance;
+    workload.result = new DeploymentResult();
+    workload.result.created = Date.now();
+    workload.result.state = ResultStates.ok;
+    workload.result.message = "Deployment successful";
+    workload.result.data = workload.data;
+  });
 
-describe("Workload Class Tests", () => {
   test("should create a valid Workload instance", () => {
     expect(workload).toBeInstanceOf(Workload);
   });
@@ -129,22 +165,55 @@ describe("Workload Class Tests", () => {
   test("should handle valid Workload properties", () => {
     expect(workload.version).toBe(1);
     expect(workload.name).toBe("Test Workload");
-    expect(workload.type).toBe(WorkloadTypes.zmachine);
+    expect(workload.type).toBe(type);
     expect(workload.metadata).toBe("Metadata");
     expect(workload.description).toBe("A test for workload");
-    expect(workload.data).toBeInstanceOf(Zmachine); // Should reflect the workload type
     expect(workload.result.created).toBeGreaterThan(0);
     expect(workload.result.state).toBe(ResultStates.ok);
     expect(workload.result.message).toBe("Deployment successful");
+
+    switch (type) {
+      case WorkloadTypes.zmachine:
+        expect(workload.data).toBeInstanceOf(Zmachine);
+        break;
+      case WorkloadTypes.zmount:
+        expect(workload.data).toBeInstanceOf(Zmount);
+        break;
+      case WorkloadTypes.volume:
+        expect(workload.data).toBeInstanceOf(Volume);
+        break;
+      case WorkloadTypes.network:
+        expect(workload.data).toBeInstanceOf(Znet);
+        break;
+      case WorkloadTypes.zdb:
+        expect(workload.data).toBeInstanceOf(Zdb);
+        break;
+      case WorkloadTypes.ip:
+        expect(workload.data).toBeInstanceOf(PublicIP);
+        break;
+      case WorkloadTypes.qsfs:
+        expect(workload.data).toBeInstanceOf(QuantumSafeFS);
+        break;
+      case WorkloadTypes.zlogs:
+        expect(workload.data).toBeInstanceOf(Zlogs);
+        break;
+      case WorkloadTypes.gatewayfqdnproxy:
+        expect(workload.data).toBeInstanceOf(GatewayFQDNProxy);
+        break;
+      case WorkloadTypes.gatewaynameproxy:
+        expect(workload.data).toBeInstanceOf(GatewayNameProxy);
+        break;
+      default:
+        throw new Error("Unhandled WorkloadType");
+    }
   });
 
   test("should correctly serialize and deserialize Workload", () => {
     const serialized = JSON.stringify(workload);
     const deserialized = plainToClass(Workload, JSON.parse(serialized));
 
-    // Change the deserialized data variable into an instance of the correct workload type
     if (deserialized.data) {
-      deserialized.data = plainToClass(Zmachine, deserialized.data); // Should reflect the workload type
+      deserialized.data = plainToClass(createDataInstance(type).constructor, deserialized.data);
     }
 
     expect(deserialized).toBeInstanceOf(Workload);
@@ -163,27 +232,10 @@ describe("Workload Class Tests", () => {
     expect(workload.challenge()).toBe(expectedChallenge);
   });
 
-  test("should handle invalid Workload type", () => {
-    const invalidWorkload = new Workload();
-    invalidWorkload.version = 1;
-    invalidWorkload.name = "Invalid Workload";
-    invalidWorkload.type = WorkloadTypes.zmachine; // Invalid workload type
-    invalidWorkload.metadata = "Metadata";
-    invalidWorkload.description = "A test for workload";
-    invalidWorkload.data = createDataInstance(WorkloadTypes.zmachine);
-    invalidWorkload.result = new DeploymentResult();
-    invalidWorkload.result.created = Date.now();
-    invalidWorkload.result.state = ResultStates.ok;
-    invalidWorkload.result.message = "Deployment successful";
-    invalidWorkload.result.data = invalidWorkload.data;
-
-    expect(() => invalidWorkload.challenge()).not.toThrow();
-  });
-
   test("should handle invalid DeploymentResult", () => {
     const invalidResult = new DeploymentResult();
     invalidResult.created = Date.now();
-    invalidResult.state = "invalid-state" as any; // Invalid state
+    invalidResult.state = "invalid-state" as any;
     invalidResult.message = "Deployment successful";
     invalidResult.data = workload.data;
 
