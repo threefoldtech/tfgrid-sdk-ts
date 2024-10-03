8e804300324b77b8c798dd4f01bece27e4636702
samaradel <samar@incubaid.com>
2024-10-03 15:08:03 +0300

    Remove isNumeric filter

--- packages/playground/src/dashboard/transfer_view.vue
+++ packages/playground/src/dashboard/transfer_view.vue
@@ -19,7 +19,6 @@
                 :rules="[
                   validators.required('Recipient Twin ID is required'),
                   validators.isNotEmpty('Invalid Twin ID'),
-                  validators.isNumeric('Twin ID should be a number'),
                   validators.min('Twin ID should be more than 0', 1),
                   validators.isInt('Twin ID should be a valid integer.'),
                   isSameTwinID,
