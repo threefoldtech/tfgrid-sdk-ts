649e579bbcd10f8a2e3dc779d0e3811597b2e01a
samaradel <samar@incubaid.com>
2024-10-03 15:03:19 +0300

    revert domain

--- packages/grid_client/scripts/orchestrators/caprover_leader.ts
+++ packages/grid_client/scripts/orchestrators/caprover_leader.ts
@@ -65,7 +65,7 @@ async function main() {
         env: {
           PUBLIC_KEY: config.ssh_key,
           SWM_NODE_MODE: "leader",
-          CAPROVER_ROOT_DOMAIN: "dmrcfk2.gent01.dev.grid.tf", // update me
+          CAPROVER_ROOT_DOMAIN: "rafy.grid.tf", // update me
           DEFAULT_PASSWORD: "captain42",
           CAPTAIN_IMAGE_VERSION: "latest",
         },
