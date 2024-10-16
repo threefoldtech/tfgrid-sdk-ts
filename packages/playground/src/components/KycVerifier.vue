2f8e5eba7e3189ff7d51e55042552f91d89f779d
0oM4R <omarkassem099@gmail.com>
2024-10-16 13:54:32 +0300

    Feat: WIP adding KYC UI

---
+++ packages/playground/src/components/KycVerifier.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-dialog
+    fullscreen
+    :model-value="moduleValue"
+    @update:model-value="handleUpdateDialog($event)"
+    width="100%"
+    class="w-100 h-100 d-flex justify-center align-center"
+  >
+    <iframe
+      id="iframe"
+      allowfullscreen
+      style="width: 100%; height: 100%; border: none"
+      src="https://ui.idenfy.com/?authToken=3FA5TFPA2ZE3LMPGGS1EGOJNJE"
+      allow="camera"
+    ></iframe>
+  </v-dialog>
+</template>
+<script lang="ts">
+export default {
+  name: "KycVerifier",
+  props: {
+    moduleValue: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  emits: ["update:moduleValue"],
+  setup(props, { emit }) {
+    const handleUpdateDialog = (event: boolean) => {
+      emit("update:moduleValue", event);
+    };
+    return {
+      handleUpdateDialog,
+    };
+  },
+};
+</script>
