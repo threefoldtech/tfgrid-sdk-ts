3965861a6e6c8e37ed8fccccc9901d6ab78ce3ef
mik-tf <logismos@protonmail.ch>
2025-02-11 12:07:13 -0500

    revert to previous configuration

--- packages/grid_client/src/modules/tfchain.ts
+++ packages/grid_client/src/modules/tfchain.ts
@@ -9,7 +9,8 @@ import { Buffer } from "buffer";
 import MD5 from "crypto-js/md5";
 import * as PATH from "path";
 
-import { manual } from "../../../playground/src/utils/manual";
+import { manual } from "@/utils/manual";
+
 import { TFClient } from "../clients/tf-grid/client";
 import { GridClientConfig } from "../config";
 import { formatErrorMessage } from "../helpers";
@@ -479,7 +480,7 @@ class TFChain implements blockchainInterface {
     }
     await (
       await client.termsAndConditions.accept({
-        documentLink: manual.manual_legal_page,
+        documentLink: "https://manual.grid.tf/knowledge_base/legal/terms_conditions_all3.html",
       })
     ).apply();
     const ret = await (await client.twins.create({ relay })).apply();
