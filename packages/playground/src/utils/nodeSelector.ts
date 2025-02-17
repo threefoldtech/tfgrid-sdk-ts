94eae2568780b2ac6a8910a157bd088969b12c68
Omar Kassem <omarkassem099@gmail.com>
2025-02-17 14:37:29 +0200

    Fix[NodeManaualFilter]: add is in dedicated farm to the condition as the dedicated nodes in shared farms can be used as shared node

--- packages/playground/src/utils/nodeSelector.ts
+++ packages/playground/src/utils/nodeSelector.ts
@@ -227,7 +227,7 @@ export async function validateRentContract(
   }
 
   try {
-    if (node.dedicated && node.rentedByTwinId === 0) {
+    if (node.dedicated && node.rentedByTwinId === 0 && node.inDedicatedFarm) {
       throw `Node ${node.nodeId} is not rented`;
     }
     if (node.rentContractId !== 0) {
