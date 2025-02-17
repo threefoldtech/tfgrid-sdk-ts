8dbf5a045713dd3ca39a8248ac1e6f08ebeb49a0
Omar Kassem <omarkassem099@gmail.com>
2025-02-17 11:52:32 +0200

    Feature[AutoNodeSelector]: reset the selected node after action this should select the rented node after rent action and check if the node is valid after unreserve action

--- packages/playground/src/components/node_selector/TfAutoNodeSelector.vue
+++ packages/playground/src/components/node_selector/TfAutoNodeSelector.vue
@@ -43,7 +43,6 @@
 
             <VAlert v-else type="error" text="No Nodes were found!" />
           </VContainer>
-
           <div
             ref="nodesContainer"
             :style="{
@@ -221,6 +220,7 @@ export default {
     });
     function updateNode(node: NodeInfo) {
       _loadedNodes.value = loadedNodes.value.map(n => (n.nodeId === node.nodeId ? node : n));
+      _setValidNode(node.nodeId);
     }
     async function _setValidNode(oldNodeId?: number) {
       const node = await selectValidNode(
