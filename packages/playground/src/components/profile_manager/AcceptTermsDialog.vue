6e1920f4127749750868a76d9cd44f9c622a48d7
Omar Kassem <omarkassem099@gmail.com>
2025-02-26 19:44:34 +0200

    chore(T&C): update to sync with development

--- packages/playground/src/components/profile_manager/AcceptTermsDialog.vue
+++ packages/playground/src/components/profile_manager/AcceptTermsDialog.vue
@@ -34,10 +34,12 @@
 </template>
 <script lang="ts" setup>
 import { marked } from "marked";
+import urlJoin from "url-join";
 import { capitalize, onMounted, ref } from "vue";
 import { useTheme } from "vuetify";
 
 import { AppThemeSelection } from "@/utils/app_theme";
+import { manual } from "@/utils/manual";
 
 const loading = ref(true);
 const acceptTermsContent = ref("");
@@ -54,19 +56,10 @@ const emit = defineEmits(["update:modelValue", "onAccept", "onError"]);
 
 onMounted(async () => {
   try {
-    const url = "https://library.threefold.me/info/legal/";
-    const response = await fetch(url + "readme.md");
+    const response = await fetch(manual.manual_raw_legal);
     const mdContent = await response.text();
-    const parsedContent = marked.parse(mdContent);
-
-    const tempDiv = document.createElement("div");
-    tempDiv.innerHTML = parsedContent;
-
-    parseAcceptTermsImage(tempDiv, url);
-    parseAcceptTermsLink(tempDiv);
-
-    const updatedHtmlContent = tempDiv.innerHTML;
-    acceptTermsContent.value = updatedHtmlContent;
+    const processedContent = processMarkdownContent(mdContent, manual.manual_legal_base);
+    acceptTermsContent.value = marked.parse(processedContent);
   } catch (error) {
     console.error(error);
     emit("onError", "Something went wrong while loading terms and conditions, please try again");
@@ -74,28 +67,22 @@ onMounted(async () => {
     loading.value = false;
   }
 });
+const processMarkdownContent = (content: string, baseUrl: string) => {
+  let processedContent = content.replace(
+    "./" + manual.legal_header_img,
+    `${manual.manual_raw_legal_img}" class="info-legal-image`,
+  );
 
-function parseAcceptTermsImage(tempDiv: HTMLDivElement, url: string) {
-  const imageElements = tempDiv.querySelectorAll("img");
-  imageElements.forEach(imgElement => {
-    imgElement.setAttribute("src", url + "legal__legal_header_.jpg");
-    // Update the style of the image.
-    imgElement.setAttribute("class", "info-legal-image");
+  const patterns = [/\[([^\]]+)\]\((\.\/[^)]+)\.md\)/g, /\[([^\]]+)\]\((\.\/[^)]+\/[^)]+)\.md\)/g];
+  for (const pattern of patterns) {
+    processedContent = replaceMarkdownLinks(processedContent, pattern, baseUrl);
+  }
+  return processedContent;
+};
+const replaceMarkdownLinks = (content: string, pattern: RegExp, baseUrl: string) => {
+  return content.replace(pattern, (_, linkText, path) => {
+    const relativePath = path.replace("./", "");
+    return `[${linkText}](${urlJoin(baseUrl, `${relativePath}.html`)})`;
   });
-}
-
-function parseAcceptTermsLink(tempDiv: HTMLDivElement) {
-  const url = "https://library.threefold.me/info/legal#";
-  const linkElements = tempDiv.querySelectorAll("a");
-  linkElements.forEach(linkElement => {
-    const currentDomainMatch = linkElement.href.match(/^(https?:\/\/[^\\/]+)/);
-    if (
-      (currentDomainMatch && linkElement.href.includes("localhost")) ||
-      (currentDomainMatch && linkElement.href.includes("dashboard")) // To update only internal links
-    ) {
-      const currentDomain = currentDomainMatch[1];
-      linkElement.href = linkElement.href.replace(currentDomain, url);
-    }
-  });
-}
+};
 </script>
