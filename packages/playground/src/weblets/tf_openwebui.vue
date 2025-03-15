87435fb12918a4f29f6b6ef85cd8c4b5d69df6cf
mik-tf <logismos@protonmail.ch>
2025-03-15 11:29:24 -0400

    removed comments

--- packages/playground/src/weblets/tf_openwebui.vue
+++ packages/playground/src/weblets/tf_openwebui.vue
@@ -55,11 +55,9 @@
         >
           <v-switch color="primary" inset label="GPU" v-model="hasGPU" hide-details />
         </input-tooltip>
-        <!-- <input-tooltip inline tooltip="" :href="manual"> -->
-        <v-switch color="primary" inset label="Rented By Me" v-model="rentedByMe" hide-details />
-        <!-- </input-tooltip> -->
+        <v-switch color="primary" inset label="Rented By Me" v-model="$props.modelValue.rentedByMe" hide-details />
         <input-tooltip inline tooltip="Click to know more about dedicated machines." :href="manual.dedicated_machines">
-          <v-switch color="primary" inset label="Rentable" v-model="dedicated" hide-details />
+        <v-switch color="primary" inset label="Rentable" v-model="$props.modelValue.dedicated" hide-details />
         </input-tooltip>
 
         <input-tooltip inline tooltip="Renting capacity on certified nodes is charged 25% extra.">
@@ -86,7 +84,6 @@
           v-model="selectionDetails"
         />
 
-        <!-- Manage the selected keys and send them to the deployment as env var -->
         <manage-ssh-deployemnt @selected-keys="updateSSHkeyEnv($event)" />
       </template>
     </d-tabs>
