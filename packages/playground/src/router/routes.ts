ef60f836f6717d7ede0cc58928f1174ccb028204
Mik <77026219+mik-tf@users.noreply.github.com>
2025-03-15 11:25:55 -0400

    Update packages/playground/src/router/routes.ts
    Co-authored-by: Amira <56790126+amiraabouhadid@users.noreply.github.com>

--- packages/playground/src/router/routes.ts
+++ packages/playground/src/router/routes.ts
@@ -62,7 +62,7 @@ enum ApplicationRoutes {
   Discourse = "/deploy/labs/discourse/",
   Taiga = "/deploy/labs/taiga/",
   Owncloud = "/deploy/labs/owncloud/",
-  Openwebui = "/deploy/laps/openwebui/",
+  Openwebui = "/deploy/labs/openwebui/",
   Nextcloud = "/deploy/labs/nextcloud/",
   Presearch = "/deploy/labs/presearch/",
   Subsquid = "/deploy/labs/subsquid/",
