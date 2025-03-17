055db810c3b931af3def9935671cdcfed82d6b31
amiraabouhadid <amiraabouhadid@gmail.com>
2025-03-17 23:18:29 +0200

    remove watchers in app_theme

--- packages/playground/src/components/app_theme.vue
+++ packages/playground/src/components/app_theme.vue
@@ -4,7 +4,7 @@
       <v-btn
         v-bind="props"
         color="anchor"
-        @click="light = !light"
+        @click="changeTheme()"
         :icon="light ? 'mdi-moon-waning-crescent' : 'mdi-brightness-4'"
       />
     </template>
@@ -12,24 +12,20 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref, watch } from "vue";
+import { computed } from "vue";
 import { useTheme } from "vuetify";
 
 import { LocalStorageSettingsKey } from "@/utils/settings";
 
 const theme = useTheme();
-const light = ref(false);
 
-watch(light, light => (theme.global.name.value = light ? "light" : "dark"));
-watch(theme.global.name, theme => {
-  localStorage.setItem(LocalStorageSettingsKey.THEME_KEY, theme);
-  light.value = theme === "light";
+const light = computed(() => {
+  return theme.global.name.value == "light";
 });
-
-onMounted(() => {
-  const theme = localStorage.getItem(LocalStorageSettingsKey.THEME_KEY);
-  light.value = theme === "light";
-});
+function changeTheme() {
+  theme.global.name.value == "dark" ? (theme.global.name.value = "light") : (theme.global.name.value = "dark");
+  localStorage.setItem(LocalStorageSettingsKey.THEME_KEY, theme.global.name.value);
+}
 </script>
 
 <script lang="ts">
