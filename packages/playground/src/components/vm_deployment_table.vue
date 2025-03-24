220169c04407f0d5b40e312e5f36e2c29a154062
fatmaebrahim <fatmaebrahim3131@gmail.com>
2025-03-24 13:54:41 +0200

    refactor: added project title in props for vmDeploymentTable component to be used in message display

--- packages/playground/src/components/vm_deployment_table.vue
+++ packages/playground/src/components/vm_deployment_table.vue
@@ -161,7 +161,7 @@
             @click="loadDeployments"
           />
         </div>
-        <p v-else v-text="'No ' + projectName + ' deployments found on this account.'" />
+        <p v-else v-text="'No ' + projectTitle + ' deployments found on this account.'" />
       </template>
     </ListTable>
   </div>
@@ -181,6 +181,7 @@ const profileManager = useProfileManager();
 
 const props = defineProps<{
   projectName: string;
+  projectTitle: string;
   modelValue: any[];
   deleting: boolean;
   hideSSH?: boolean;
@@ -222,6 +223,7 @@ async function loadDomains() {
 }
 
 async function loadDeployments() {
+  console.log(props.projectTitle);
   if (props.projectName.toLowerCase() === ProjectName.Domains.toLowerCase()) {
     return loadDomains();
   }
