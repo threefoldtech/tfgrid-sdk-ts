3afe37b38bb3bfd972e97cfd5746b960261d768a
amiraabouhadid <amiraabouhadid@gmail.com>
2025-03-24 21:27:18 +0200

    remove unused watch import

--- packages/playground/src/weblets/tfrobot.vue
+++ packages/playground/src/weblets/tfrobot.vue
@@ -185,7 +185,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, type Ref, ref, watch } from "vue";
+import { computed, type Ref, ref } from "vue";
 
 import { manual } from "@/utils/manual";
 
