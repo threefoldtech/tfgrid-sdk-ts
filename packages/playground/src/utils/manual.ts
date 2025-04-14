38c51103b210e5ee40f4a5bb7c9175ab7083fe7d
mik-tf <logismos@protonmail.ch>
2025-04-14 11:08:13 -0400

    merge dev manual.ts to current branch to resolve conflicts

--- packages/playground/src/utils/manual.ts
+++ packages/playground/src/utils/manual.ts
@@ -10,7 +10,6 @@ const GITHUB = {
 const LEGAL_HEADER_IMG = "img/legal_header.jpg";
 
 export const manual = {
-<<<<<<< HEAD
   dedicated_machines: urlJoin(BASE, "/labs/documentation/dashboard/deploy/node_finder#dedicated-nodes"),
   tft_bridges: urlJoin(BASE, "/labs/documentation/threefold_token/tft_bridges"),
   buy_sell_tft: urlJoin(BASE, "/labs/documentation/threefold_token/buy_sell_tft"),
@@ -28,26 +27,6 @@ export const manual = {
   minting_receipts: urlJoin(BASE, "/labs/documentation/farmers/farming_optimization/minting_receipts"),
   minting_process: urlJoin(BASE, "/labs/documentation/farmers/farming_optimization/minting_process"),
   minting_reports: urlJoin(BASE, "/labs/documentation/dashboard/tfchain/tf_minting_reports"),
-=======
-  dedicated_machines: urlJoin(BASE, "/documentation/dashboard/deploy/node_finder.html#dedicated-nodes"),
-  tft_bridges: urlJoin(BASE, "/documentation/threefold_token/tft_bridges/tft_bridges.html"),
-  buy_sell_tft: urlJoin(BASE, "/documentation/threefold_token/buy_sell_tft/buy_sell_tft.html"),
-  farmers: urlJoin(BASE, "/documentation/farmers/farmers.html"),
-  governance: urlJoin(BASE, "/documentation/dashboard/tfchain/tf_dao.html"),
-  pricing: urlJoin(BASE, "/knowledge_base/cloud/pricing/pricing.html"),
-  dao: urlJoin(BASE, "/documentation/dashboard/tfchain/tf_dao.html"),
-  caprover: urlJoin(BASE, "/documentation/dashboard/solutions/caprover.html"),
-  tf_connect_app: urlJoin(BASE, "/documentation/threefold_token/storing_tft/tf_connect_app.html"),
-  tf_connect_installation: urlJoin(BASE, "/documentation/tfconnect/tfconnect_installation.html#download-links"),
-  tf_connect_wallet: urlJoin(BASE, "/documentation/dashboard/wallet_connector.html"),
-  contract_locking: urlJoin(BASE, "/documentation/developers/tfchain/tfchain.html#contract-locking"),
-  billing_pricing: urlJoin(BASE, "/documentation/dashboard/deploy/node_finder.html#billing--pricing"),
-  discount_levels: urlJoin(BASE, "/knowledge_base/cloud/pricing/pricing.html#staking-discount"),
-  tfchain_stellar_bridge: urlJoin(BASE, "/documentation/threefold_token/tft_bridges/tfchain_stellar_bridge.html"),
-  minting_receipts: urlJoin(BASE, "/documentation/farmers/3node_building/minting_receipts.html"),
-  minting_process: urlJoin(BASE, "/documentation/farmers/farming_optimization/minting_process.html"),
-  minting_reports: urlJoin(BASE, "/documentation/dashboard/tfchain/tf_minting_reports.html"),
->>>>>>> development
   manual_raw_legal: urlJoin(GITHUB.RAW_BASE, GITHUB.LEGAL_PATH, "terms_conditions_all3.md"),
   manual_raw_legal_img: urlJoin(GITHUB.RAW_BASE, GITHUB.LEGAL_PATH, LEGAL_HEADER_IMG),
   manual_legal_base: urlJoin(BASE, GITHUB.LEGAL_PATH),
