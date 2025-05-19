cff38ef3a964f23b12880ed585c3741606eea2d5
mik-tf <logismos@protonmail.ch>
2025-05-19 10:23:21 -0400

    fix remaining conflicts

--- packages/playground/tests/frontend_selenium/tests/TFChain/test_homepage.py
+++ packages/playground/tests/frontend_selenium/tests/TFChain/test_homepage.py
@@ -163,10 +163,6 @@ def test_account_validation(browser):
     assert dashboard_page.wait_for('Passwords should match')
     dashboard_page.click_button(dashboard_page.connect_your_wallet(get_email(), '123456'))
     dashboard_page.open_profile()
-<<<<<<< HEAD
-    assert dashboard_page.manual_link() == 'https://manual.dev.grid.tf/labs/documentation/dashboard/wallet_connector/'
-=======
->>>>>>> development
     assert dashboard_page.get_mnemonic() == get_seed()
     assert dashboard_page.get_email() == get_email()
     assert grid_proxy.get_twin_address(dashboard_page.get_id()) == dashboard_page.get_address()
@@ -195,18 +191,13 @@ def test_login_links(browser):
     dashboard_page.import_account(get_seed())
     dashboard_page.click_button(dashboard_page.connect_your_wallet(get_email(), password))
     dashboard_page.open_profile()
-<<<<<<< HEAD
-    assert dashboard_page.manual_link() == 'https://manual.dev.grid.tf/labs/documentation/dashboard/wallet_connector/'
-    assert dashboard_page.connect_manual_link() == 'https://www.manual.dev.grid.tf/labs/documentation/threefold_token/storing_tft/tf_connect_app/'
-=======
     if Base.net in ['dev', 'local']:
-        wallet_manual_link = 'https://www.manual.grid.tf/documentation/dashboard/wallet_connector.html'
-        tf_connect_app_link = 'https://www.manual.grid.tf/documentation/threefold_token/storing_tft/tf_connect_app.html'
+        wallet_manual_link = 'https://manual.dev.grid.tf/labs/documentation/dashboard/wallet_connector/'
+        tf_connect_app_link = 'https://www.manual.dev.grid.tf/labs/documentation/threefold_token/storing_tft/tf_connect_app/'
     else:
         wallet_manual_link = 'https://manual.grid.tf/documentation/dashboard/wallet_connector.html'
         tf_connect_app_link = 'https://manual.grid.tf/documentation/threefold_token/storing_tft/tf_connect_app.html'
     assert dashboard_page.manual_link() == wallet_manual_link
     assert dashboard_page.connect_manual_link() == tf_connect_app_link
->>>>>>> development
     assert dashboard_page.get_connect_google_link() == 'https://play.google.com/store/apps/details?id=org.jimber.threebotlogin&hl=en&gl=US'
     assert dashboard_page.get_connect_apple_link() == 'https://apps.apple.com/us/app/threefold-connect/id1459845885'
