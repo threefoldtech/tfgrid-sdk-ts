3bc8b86fc851347dbaf8b22a9c34e695b09af6f9
Omar Kassem <omarksm09@gmail.com>
2025-06-30 17:29:36 +0300

    fix: update loading state and message for SSH key recovery

--- packages/playground/src/views/sshkey_view.vue
+++ packages/playground/src/views/sshkey_view.vue
@@ -140,7 +140,7 @@ const sshKeysManagement = new SSHKeysManagement();
 onMounted(async () => {
   loading.value = true;
   if (!sshKeysManagement.migrated() || sshKeysManagement.needsDefaultNameAssignment()) {
-    tableLoadingMessage.value = "Migrating your old key...";
+    tableLoadingMessage.value = "Recovering your old key...";
     const migrationInterval = setInterval(async () => {
       const migrated = sshKeysManagement.migrated();
       const nameUpdated = !sshKeysManagement.needsDefaultNameAssignment();
@@ -148,13 +148,14 @@ onMounted(async () => {
         clearInterval(migrationInterval);
         allKeys.value = sshKeysManagement.list();
         tableLoadingMessage.value = "";
+        loading.value = false;
       }
     }, 1000);
   } else {
     allKeys.value = sshKeysManagement.list();
     tableLoadingMessage.value = "";
+    loading.value = false;
   }
-  loading.value = false;
 });
 
 const openDialog = (type: SSHCreationMethod) => {
