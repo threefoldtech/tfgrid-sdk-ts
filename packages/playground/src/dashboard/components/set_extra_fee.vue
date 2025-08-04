c2c48d20eb6def7cf4dd0b66d34c38fe310127ba
Samar Adel <samar@incubaid.com>
2025-08-04 15:49:31 +0300

    Fix extra fees limitation (#4324)

--- packages/playground/src/dashboard/components/set_extra_fee.vue
+++ packages/playground/src/dashboard/components/set_extra_fee.vue
@@ -31,7 +31,7 @@
                   validators.required('Fee is required.'),
                   validators.isNumeric('Fee must be a valid number.'),
                   validators.min('Fee must be a 0 or more.', 0),
-                  validators.max('Maximum allowed fee is 1000000', 10000000),
+                  validators.max('Maximum allowed fee is 1000000', 1000000),
                 ]"
                 #="{ props }"
               >
