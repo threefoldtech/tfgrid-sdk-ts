caa96cfa7a66170d4469e895af051bddf9381987
samaradel <samar@incubaid.com>
2025-09-09 18:01:38 +0300

    Fix full and micro vms loads

--- packages/playground/src/components/vm_deployment_table.vue
+++ packages/playground/src/components/vm_deployment_table.vue
@@ -238,7 +238,6 @@ async function loadDeployments() {
   if (props.projectName.toLowerCase() === ProjectName.Domains.toLowerCase()) {
     return loadDomains();
   }
-
   const migrateGateways = props.projectName.toLowerCase() !== "fullvm" && props.projectName.toLowerCase() !== "vm";
 
   items.value = [];
@@ -269,7 +268,10 @@ async function loadDeployments() {
       chunk3.items = chunk3.items.map(markAsFromAnotherClient);
     }
 
-    const vms = mergeLoadedDeployments(chunk1, chunk2, chunk3 as any);
+    const vms =
+      props.projectName.toLowerCase() === "fullvm" || props.projectName.toLowerCase() == "vm"
+        ? mergeLoadedDeployments(chunk2, chunk3 as any)
+        : mergeLoadedDeployments(chunk1, chunk2, chunk3 as any);
     failedDeployments.value = vms.failedDeployments;
     count.value = vms.count;
     items.value = mergeCaproverDeployments(vms.items);
