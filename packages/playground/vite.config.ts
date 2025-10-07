1edffcec5257faaf06d9a8a7d61825b4a10bca39
Omar Kassem <omarksm09@gmail.com>
2025-10-07 12:31:49 +0300

    feat: add Vue DevTools plugin to playground environment (#4378)

--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
     outDir: "dist",
   },
   base: "/",
-  plugins: [vue(), nodePolyfills()],
+  plugins: [vue(), nodePolyfills(), vueDevTools()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
