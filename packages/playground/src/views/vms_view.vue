5b317338e0b56d85d5ac66b74601e6a950cd6aa7
Zainab Elgohary <40770501+zaelgohary@users.noreply.github.com>
2025-11-20 09:15:33 +0200

    Remove ubuntu image version (#4425)

--- packages/playground/src/views/vms_view.vue
+++ packages/playground/src/views/vms_view.vue
@@ -1,12 +1,8 @@
 <template>
   <view-layout>
     <v-card color="primary" class="d-flex justify-center items-center mb-4 pa-3 text-center">
-      <v-icon size="30" class="pr-3">
-        mdi-television
-      </v-icon>
-      <v-card-title class="pa-0">
-        Virtual Machines
-      </v-card-title>
+      <v-icon size="30" class="pr-3"> mdi-television </v-icon>
+      <v-card-title class="pa-0"> Virtual Machines </v-card-title>
     </v-card>
     <ApplicationCards :cards="cards" />
   </view-layout>
@@ -34,7 +30,7 @@ export default {
       {
         title: "Micro Virtual Machine",
         excerpt:
-          "Deploy a micro virtual machine on Threefold Grid. We provide few images managed by Threefold like Ubuntu 22.04, and NixOS, but you can still use a custom one.",
+          "Deploy a micro virtual machine on Threefold Grid. We provide few images managed by Threefold like Ubuntu, and NixOS, but you can still use a custom one.",
         icon: "vm.png",
         route: DashboardRoutes.VirtualMachines.MicroVirtualMachine,
       },
